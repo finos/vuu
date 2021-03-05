064184605ac26f0418ea42e5dcdf06661a7bff79
Chris Stevenson <chris.stevenson@gmail.com>
2021-03-05 16:00:39 +0000

    #38 Resolved test issuus due to changing the interface when opening nodes.

--- vuu/src/test/scala/io/venuu/vuu/viewport/GroupByAndAggregate2Test.scala
+++ vuu/src/test/scala/io/venuu/vuu/viewport/GroupByAndAggregate2Test.scala
@@ -86,7 +86,9 @@ class GroupByAndAggregate2Test extends AnyFeatureSpec with Matchers with GivenWh
     viewPortContainer.openNode(viewport.id, "$root/chris/VOD.L")
     viewPortContainer.openNode(viewport.id, "$root/steve")
     viewPortContainer.closeNode(viewport.id, "$root/steve/BT.L")
-    
+
+    emptyQueues(viewport)
+
     viewPortContainer.runOnce()
     viewPortContainer.runGroupByOnce()
 
@@ -97,15 +99,12 @@ class GroupByAndAggregate2Test extends AnyFeatureSpec with Matchers with GivenWh
     viewPortContainer.runOnce()
     viewPortContainer.runGroupByOnce()
 
-    //viewport.combinedQueueLength should be(14) //as have no keys
-
     val updates = combineQs(viewport)
 
     assertVpEq(updates) {
 
       Table(
         ("_isOpen" ,"_depth"  ,"_treeKey","_isLeaf" ,"_childCount","_caption","orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","bid"     ,"ask"     ,"last"    ,"open"    ,"close"   ),
-        (true      ,0         ,"$root"   ,false     ,2         ,""        ,""        ,"[2]"     ,""        ,""        ,"Σ 3800.0",""        ,""        ,""        ,""        ,""        ),
         (true      ,1         ,"$root/chris",false     ,2         ,"chris"   ,""        ,"[1]"     ,""        ,""        ,"Σ 2200.0",""        ,""        ,""        ,""        ,""        ),
         (true      ,2         ,"$root/chris/VOD.L",false     ,5         ,"VOD.L"   ,""        ,"[1]"     ,"VOD.L"   ,""        ,"Σ 1500.0",""        ,""        ,""        ,""        ,""        ),
         (false     ,3         ,"$root/chris/VOD.L/NYC-0001",true      ,0         ,"NYC-0001","NYC-0001","chris"   ,"VOD.L"   ,1437732000000l,100       ,220.0     ,222.0     ,null      ,null      ,null      ),
@@ -114,9 +113,9 @@ class GroupByAndAggregate2Test extends AnyFeatureSpec with Matchers with GivenWh
         (false     ,3         ,"$root/chris/VOD.L/NYC-0004",true      ,0         ,"NYC-0004","NYC-0004","chris"   ,"VOD.L"   ,1437732000000l,400       ,220.0     ,222.0     ,null      ,null      ,null      ),
         (false     ,3         ,"$root/chris/VOD.L/NYC-0005",true      ,0         ,"NYC-0005","NYC-0005","chris"   ,"VOD.L"   ,1437732000000l,500       ,220.0     ,222.0     ,null      ,null      ,null      ),
         (true      ,1         ,"$root/steve",false     ,2         ,"steve"   ,""        ,"[1]"     ,""        ,""        ,"Σ 1600.0",""        ,""        ,""        ,""        ,""        ),
-        (false     ,2         ,"$root/steve/VOD.L",false     ,1         ,"VOD.L"   ,""        ,"[1]"     ,"VOD.L"   ,""        ,"Σ 600.0",""        ,""        ,""        ,""        ,""        ),
+        (false     ,2         ,"$root/steve/VOD.L",false     ,1         ,"VOD.L"   ,""        ,"[1]"     ,"VOD.L"   ,""        ,"Σ 600.0" ,""        ,""        ,""        ,""        ,""        ),
         (false     ,2         ,"$root/steve/BT.L",false     ,1         ,"BT.L"    ,""        ,"[1]"     ,"BT.L"    ,""        ,"Σ 1000.0",""        ,""        ,""        ,""        ,""        ),
-        (false     ,2         ,"$root/chris/BT.L",false     ,1         ,"BT.L"    ,""        ,"[1]"     ,"BT.L"    ,""        ,"Σ 700.0",""        ,""        ,""        ,""        ,""        )
+        (false     ,2         ,"$root/chris/BT.L",false     ,1         ,"BT.L"    ,""        ,"[1]"     ,"BT.L"    ,""        ,"Σ 700.0" ,""        ,""        ,""        ,""        ,""        )
       )
 
     }
@@ -130,8 +129,9 @@ class GroupByAndAggregate2Test extends AnyFeatureSpec with Matchers with GivenWh
 
     viewPortContainer.openNode(viewport.id, "$root/steve/VOD.L")
 
-    viewPortContainer.runOnce()
+    emptyQueues(viewport)
 
+    viewPortContainer.runOnce()
     viewPortContainer.runGroupByOnce()
 
     val updates2 = combineQs(viewport)
