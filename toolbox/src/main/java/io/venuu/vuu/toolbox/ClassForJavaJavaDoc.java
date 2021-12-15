6641444a4bc986649392fafbc6779a93b12633dd
chrisjstevo <chris.stevenson@gmail.com>
2021-12-15 17:34:51 +0000

    #125, fixed more mvn central issues

---
+++ toolbox/src/main/java/io/venuu/vuu/toolbox/ClassForJavaJavaDoc.java
@@ -0,0 +1,8 @@
+package io.venuu.vuu.toolbox;
+
+/**
+ * This class is only here so we can have a javadoc jar for mvn central
+ */
+public class ClassForJavaJavaDoc {
+    public static void main(String[] args){}
+}
