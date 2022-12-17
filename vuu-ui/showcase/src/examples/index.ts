e4fe2db5fa896e993e7330197e48c066f765af64
heswell <steve@heswell.com>
2022-12-17 22:13:14 +0000

    update build scripts and point salt icons to original library

--- vuu-ui/showcase/src/examples/index.ts
+++ vuu-ui/showcase/src/examples/index.ts
@@ -1,5 +1,6 @@
 export * as ColumnConfiguration from "./ColumnConfiguration";
 export * as DataGrid from "./DataGrid";
+export * as ColumnConfiguration from "./ColumnConfiguration";
 export * as Filters from "./Filters";
 export * as Layout from "./Layout";
 export * as ShowcaseControls from "./ShowcaseControls";
