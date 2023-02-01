35f343877511501c891a19d22b6daef967c7225d
heswell <steve@heswell.com>
2023-02-01 13:22:44 +0000

    add single script to build, deploy and launch demo app

---
+++ vuu-ui/sample-apps/app-vuu-example/demo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { LoginPanel } from "@finos/vuu-shell";
+import { SaltProvider } from "@salt-ds/core";
+import { uuid } from "@finos/vuu-utils";
+
+import "@finos/vuu-theme/index.css";
+import "./login.css";
+
+async function login(username: string, password: string) {
+  try {
+    const authToken = uuid();
+    const date = new Date();
+    const days = 1;
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `vuu-username=${username};expires=${date.toUTCString()};path=/`;
+    document.cookie = `vuu-auth-token=${authToken};expires=${date.toUTCString()};path=/`;
+    window.location.href = "/index.html";
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+ReactDOM.render(
+  <SaltProvider>
+    <LoginPanel onSubmit={login} />
+  </SaltProvider>,
+  document.getElementById("root")
+);
