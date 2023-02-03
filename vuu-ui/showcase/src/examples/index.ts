00b61432186b33097c716dedfeefb5b4aa7b5fb4
harryhartley <hhartley94@gmail.com>
2023-02-03 15:38:21 +0000

    vuu salt

--- vuu-ui/showcase/src/examples/index.ts
+++ vuu-ui/showcase/src/examples/index.ts
@@ -1,12 +1,12 @@
 export * as ColumnConfiguration from "./ColumnConfiguration";
 export * as DataGrid from "./DataGrid";
 export * as Filters from "./Filters";
+export * as html from "./html";
 export * as Layout from "./Layout";
+export * as salt from "./salt";
+export * as Shell from "./Shell";
 export * as ShowcaseControls from "./ShowcaseControls";
 export * as Table from "./Table";
 export * as Tools from "./Tools";
-export * as salt from "./salt";
 export * as VUU from "./VUU";
-export * as Shell from "./Shell";
 export * as VUU_salt from "./VUU-salt";
-export * as html from "./html";
