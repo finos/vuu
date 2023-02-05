e5ffc5c8d4dd66309ee03411ecf5c5066bcc30f0
heswell <steve@heswell.com>
2023-02-05 09:16:37 +0000

    fix showcase preview

---
+++ vuu-ui/showcase/scripts/preview.mjs
@@ -0,0 +1,14 @@
+import fs from "fs";
+import open from "open";
+import chalk from "chalk";
+import { execWait } from "../../scripts/utils.mjs";
+
+fs.copyFileSync("./templates/index.html", "./index.html");
+
+execWait("node ../node_modules/.bin/vite preview");
+
+console.log(`opening showcase at ${chalk.green("http://127.0.0.1:4173/")} ...`);
+
+setTimeout(() => {
+  open("http://127.0.0.1:4173/");
+}, 1000);
