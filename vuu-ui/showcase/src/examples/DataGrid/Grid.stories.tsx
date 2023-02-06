b0c94e25a1420d9ffd7d93f01bef25e082d2c21f
harryhartley <hhartley94@gmail.com>
2023-02-06 17:57:38 +0000

    grid config type fix

--- vuu-ui/showcase/src/examples/DataGrid/Grid.stories.tsx
+++ vuu-ui/showcase/src/examples/DataGrid/Grid.stories.tsx
@@ -299,7 +299,7 @@ export const BasicGrid = () => {
     gridRef.current?.style.setProperty("--grid-row-height", `20px`);
   };
 
-  const handleConfigChange = (config: any) => {
+  const handleConfigChange = (config: GridConfig) => {
     console.log(`handleConfigChange ${JSON.stringify(config, null, 2)}`);
   };
 
@@ -637,7 +637,7 @@ export const BufferVariations = () => {
   });
   const gridRef = useRef<HTMLDivElement>(null);
 
-  const handleConfigChange = (config: any) => {
+  const handleConfigChange = (config: GridConfig) => {
     console.log(`handleConfigChange ${JSON.stringify(config, null, 2)}`);
   };
 
