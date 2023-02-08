ea465abb1c851a5bfd8c553aff95b3acf9b863f7
heswell <steve@heswell.com>
2023-02-08 08:29:58 +0000

    fix showcase build

--- vuu-ui/showcase/scripts/preview.mjs
+++ vuu-ui/showcase/scripts/preview.mjs
@@ -1,9 +1,10 @@
-import fs from "fs";
 import open from "open";
 import chalk from "chalk";
 import { execWait } from "../../scripts/utils.mjs";
 
-fs.copyFileSync("./templates/index-preview.html", "./index.html");
+import "./copy-preview.mjs";
+
+// fs.copyFileSync("./templates/index-preview.html", "./index.html");
 
 execWait("node ../node_modules/.bin/vite preview");
 
