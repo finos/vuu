d27af83cdbc443fa9c602bdfa51661c859839548
heswell <steve@heswell.com>
2023-04-25 11:23:18 +0100

    add showcase example for nested ThemeProvider (#652)

--- vuu-ui/showcase/src/examples/Shell/index.ts
+++ vuu-ui/showcase/src/examples/Shell/index.ts
@@ -1,4 +1,5 @@
 export * as AppSidePanel from "./AppSidePanel.stories";
 export * as ConnectionStatus from "./ConnectionStatus.stories";
 export * as Shell from "./Shell.stories";
+export * as ThemeProvider from "./ThemeProvider.examples";
 export * as ThemeSwitch from "./ThemeSwitch.examples";
