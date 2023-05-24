2ab9b7fab6eb54a3798e776f3e1cb794282f2104
Pranav Bheemsetty <pranavsetty@gmail.com>
2023-05-24 22:49:08 +0200

    Initial site revamp (#688)
    * Initial site revamp
    
    * Delete yarn.lock file
    
    * Add css variable for main-bg-color
    
    * Feature 3 initial implementation
    
    ---------
    
    Co-authored-by: heswell <steve@heswell.com>

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,50 +1,60 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
-import Feature from '../components/feature';
-import { features } from '../components/feature-config';
-import FeaturesTwo from '../components/featuresTwo';
-import { featuresTwo } from '../components/featuresTwo-config';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./styles.module.css";
+import Feature from "../components/feature";
+import { features } from "../components/feature-config";
+import { featuresTwo } from "../components/featuresTwo-config";
+import Chart from "../../static/img/finos/chart.png";
+import DemoApp from "../../static/img/finos/demo-app.png";
+import Scroll from "../../static/img/finos/scroll.png";
+import Effect from "../../static/img/finos/effect.png";
+import Script from "../components/script";
 
 function Home() {
-
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description={`${siteConfig.tagline}`}>
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className={classnames('hero--subtitle')}>{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted
-              )}
-              to={'https://finos.org/'}>
-              GET STARTED
-            </Link>
-            <Link
-              className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted
-              )}
-              to={'https://github.com/finos'}>
-              GITHUB
-            </Link>
-          </div>
+          <h1>Delivering risk data </h1>
+          <h2>from trading systems, to human eyes</h2>
+          <p className="p-homepage">
+            Vuu deals with the complex task of viewporting, throttling <br />{" "}
+            and transmiting your data to clients, so you don't have to.
+          </p>
+          <Link
+            className={classnames(
+              "button button--lg get-started-button",
+              styles.getStarted
+            )}
+            to={"https://finos.org/"}
+          >
+            GET STARTED
+          </Link>
         </div>
+        <img
+          src={Chart}
+          alt="chart"
+          // style={{ height: "500px", width: "400px" }}
+        />
       </header>
+
       <main>
         {features && features.length && (
           <section className={styles.features}>
             <div className="container">
+              <img
+                className="home-page-images"
+                src={Scroll}
+                alt="demo-app"
+                style={{ height: "50px" }}
+              />
+              <h1 style={{ textAlign: "center" }}> Why VUU</h1>
+
               <div className="row">
                 {features.map((props, idx) => (
                   <Feature key={idx} {...props} />
@@ -55,15 +65,48 @@ function Home() {
         )}
         {featuresTwo && featuresTwo.length && (
           <section className={styles.members}>
-            <div className="container">
-              <div className="row row--center">
-                <h2></h2>
-              </div>
-              <div className="row">
-                {featuresTwo.map((props, idx) => (
-                  <FeaturesTwo key={idx} {...props} />
-                ))}
-              </div>
+            <Script />
+
+            <img
+              src={Effect}
+              className="home-page-images"
+              alt="demo-app"
+              style={{ height: "40px", marginTop: "40px" }}
+            />
+            <h1 style={{ textAlign: "center" }}> Demo App</h1>
+            <img
+              src={DemoApp}
+              className="home-page-images"
+              alt="demo-app"
+              style={{ height: "500px", width: "75%", borderRadius: "20px" }}
+            />
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "2vh",
+              }}
+            >
+              <Link
+                className={classnames(
+                  "button button--lg get-started-button",
+                  styles.getStarted
+                )}
+                to={"https://finos.org/"}
+              >
+                GET STARTED
+              </Link>
+              <Link
+                className={classnames(
+                  "button button--lg github-button",
+                  styles.getStarted
+                )}
+                to={"https://finos.org/"}
+              >
+                GITHUB
+              </Link>
             </div>
           </section>
         )}
