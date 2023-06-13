aa096f63f1cbfd40ca468c8ce8335b2b66bd5c7c
Pranav Bheemsetty <pranavsetty@gmail.com>
2023-06-13 12:19:53 +0100

    Update launch script for mobile

--- website/scripts/launch-mobile.mjs
+++ website/scripts/launch-mobile.mjs
@@ -1,2 +1,3 @@
 import fs from "fs";
 fs.copyFileSync("./templates/index-mobile.js", "./src/pages/index.js");
+fs.copyFileSync("./src/css/custom-mobile.css", "./src/css/custom.css");
--- website/templates/index-mobile.js
+++ website/templates/index-mobile.js
@@ -3,7 +3,7 @@ import classnames from "classnames";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-// import "../theme/Footer/Footer-mobile.css";
+import "../theme/Footer/Footer-mobile.css";
 import { RevolvingCaption } from "../components/RevolvingCaption";
 import { ChartMobile } from "../components/ChartMobile";
 const revolvingCaptions = [
