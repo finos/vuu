c539b6a74c11479f18ff61a0144913fa6157f9c5
heswell <steve@heswell.com>
2023-07-25 06:23:57 +0100

    Fix filters (#827)
    * restore missing file, remove unused stories
    
    * change import to correct file

--- vuu-ui/packages/vuu-filters/src/index.ts
+++ vuu-ui/packages/vuu-filters/src/index.ts
@@ -1,4 +1,3 @@
-export * from "./column-filter";
 export * from "./filter-input";
 export * from "./filter-toolbar";
 export * from "./filter-utils";
--- vuu-ui/showcase/src/examples/Filters/index.ts
+++ vuu-ui/showcase/src/examples/Filters/index.ts
@@ -1,4 +1,3 @@
 export * from "./FilterInput.stories";
 export * from "./FilterToolbar.stories";
-export * from "./ColumnFilter.stories";
-export * from "./NewFiltering.stories"
+export * from "./NewFiltering.stories";
