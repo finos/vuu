02280e5287ba9ee831dacb8d88f3b9f0a5fb4ada
Peter Ling <pling@scottlogic.com>
2023-08-30 15:33:48 +0100

    VUU-17: Remove RadioIconProps interface

--- vuu-ui/packages/vuu-ui-controls/src/list/RadioIcon.tsx
+++ vuu-ui/packages/vuu-ui-controls/src/list/RadioIcon.tsx
@@ -1,17 +1,13 @@
-import { HTMLAttributes } from "react";
 import cx from "classnames";
 
 import "./RadioIcon.css";
 
 const classBase = "vuuRadioIcon";
 
-export interface RadioIconProps extends HTMLAttributes<HTMLSpanElement> {
-  checked?: boolean;
-}
 export const RadioIcon = ({
   checked = false,
   ...htmlAttributes
-}: RadioIconProps) => (
+}) => (
   <span
     {...htmlAttributes}
     className={cx(classBase, { [`${classBase}-checked`]: checked })}
