92453b90128cec886d91f0f04825db1d983c2a75
chris <chris.stevenson@gmail.com>
2023-09-25 20:46:32 +0200

    #850 Added more detail to basket trading example.

--- vuu/src/main/scala/org/finos/vuu/core/module/basket/BasketModule.scala
+++ vuu/src/main/scala/org/finos/vuu/core/module/basket/BasketModule.scala
@@ -3,7 +3,7 @@ package org.finos.vuu.core.module.basket
 import org.finos.toolbox.lifecycle.LifecycleContainer
 import org.finos.toolbox.time.Clock
 import org.finos.vuu.api.{TableDef, VisualLinks}
-import org.finos.vuu.core.module.basket.provider.{BasketConstituentProvider, BasketProvider, NotYetImplementedProvider}
+import org.finos.vuu.core.module.basket.provider.{BasketConstituentProvider, BasketProvider, NotYetImplementedProvider, PriceStrategyProvider}
 import org.finos.vuu.core.module.{DefaultModule, ModuleFactory, ViewServerModule}
 import org.finos.vuu.core.table.Columns
 
@@ -11,74 +11,85 @@ object BasketModule extends DefaultModule {
 
   private final val NAME = "BASKET"
 
-  import BasketColumnNames._
-  import BasketConstituentColumnNames._
-  final val BASKET_CONSTITUENT_TABLE_DEF = TableDef(
-    name = "basketConstituent",
-    keyField = Ric,
-    columns = Columns.fromNames(Ric.string(), BasketId.string(), Weighting.double(), LastTrade.string(), Change.string(), Volume.string()), // we can join to instruments and other tables to get the rest of the data.....
-    VisualLinks(),
-    joinFields = Id
-  )
   def apply()(implicit clock: Clock, lifecycle: LifecycleContainer): ViewServerModule = {
 
+    import org.finos.vuu.core.module.basket.BasketModule.{BasketColumnNames => B}
+    import org.finos.vuu.core.module.basket.BasketModule.{BasketConstituentColumnNames => BC}
+    import org.finos.vuu.core.module.basket.BasketModule.{BasketTradingColumnNames => BT}
+    import org.finos.vuu.core.module.basket.BasketModule.{BasketTradingConstituentColumnNames => BTC}
+    import org.finos.vuu.core.module.basket.BasketModule.{PriceStrategy => PS}
 
     ModuleFactory.withNamespace(NAME)
       .addTable(
+        //this table should contain one row for each of .FTSE, .DJI, .HSI, .etc...
         TableDef(
           name = "basket",
-          keyField = Id,
-          columns = Columns.fromNames(Id.string(), Name.string(), NotionalValue.double(), NotionalValueUsd.double()),
+          keyField = B.Id,
+          columns = Columns.fromNames(B.Id.string(), B.Name.string(), B.NotionalValue.double(), B.NotionalValueUsd.double()),
           VisualLinks(),
-          joinFields = Id
+          joinFields = B.Id
         ),
         (table, vs) => new BasketProvider(table),
       )
       .addTable(
-        BASKET_CONSTITUENT_TABLE_DEF,
+        TableDef(
+          name = "basketConstituent",
+          keyField = BC.RicBasketId,
+          columns = Columns.fromNames(BC.RicBasketId.string(), BC.Ric.string(), BC.BasketId.string(), BC.Weighting.double(), BC.LastTrade.string(), BC.Change.string(), BC.Volume.string()), // we can join to instruments and other tables to get the rest of the data.....
+          VisualLinks(),
+          joinFields = BC.RicBasketId
+        ),
         (table, vs) => new BasketConstituentProvider(table),
       )
       .addTable(
         TableDef(
-          name = "basketDesign",
-          keyField = Ric,
-          columns = Columns.fromNames(Ric.string(), BasketId.string(), Weighting.double()), // we can join to instruments and other tables to get the rest of the data.....
-          VisualLinks(),
-          joinFields = Id
-        ),
-        (table, vs) => new NotYetImplementedProvider(table),
-      )
-      .addTable(
-        TableDef(
           name = "basketTrading",
-          keyField = Ric,
-          columns = Columns.fromNames(Ric.string(), BasketId.string(), Weighting.double()), // we can join to instruments and other tables to get the rest of the data.....
+          keyField = BT.InstanceId,
+          columns = Columns.fromNames(BT.InstanceId.string(), BT.BasketId.string(), BT.BasketName.string(), BT.Status.string(), BT.Units.int(), BT.FilledPct.double(), BT.FxRateToUsd.double(), BT.TotalNotional.double(), BT.TotalNotionalUsd.double()), // we can join to instruments and other tables to get the rest of the data.....
           VisualLinks(),
-          joinFields = Id
+          joinFields = BT.BasketId
         ),
         (table, vs) => new NotYetImplementedProvider(table),
       )
       .addTable(
         TableDef(
-          name = "basketConstituentDesign",
-          keyField = Ric,
-          columns = Columns.fromNames(Ric.string(), BasketId.string(), Weighting.double()), // we can join to instruments and other tables to get the rest of the data.....
+          name = "basketTradingConstituent",
+          keyField = BTC.InstanceIdRic,
+          columns = Columns.fromNames(BTC.InstanceIdRic.string(), BTC.InstanceId.string(), BTC.Ric.string(),
+                                      BTC.BasketId.string(), BTC.PriceStrategyId.int(),
+                                      BTC.Description.string(),
+                                      BTC.Bid.double(), BTC.Offer.double(), BTC.Last.double(),
+                                      BTC.NotionalUsd.double(), BTC.NotionalLocal.double(),
+                                      BTC.Venue.string(),
+                                      BTC.Algo.string(), BTC.AlgoParams.string(),
+                                      BTC.PctFilled.double(), BTC.Weighting.double(),
+                                      BTC.PriceSpread.int(),
+                                      BTC.LimitPrice.double()
+          ),// we can join to instruments and other tables to get the rest of the data.....
           VisualLinks(),
-          joinFields = Id
+          joinFields = BTC.InstanceIdRic
         ),
         (table, vs) => new NotYetImplementedProvider(table),
       )
       .addTable(
+        //lookup table for price strategies....
         TableDef(
-          name = "basketConstituentTrading",
-          keyField = Ric,
-          columns = Columns.fromNames(Ric.string(), BasketId.string(), Weighting.double()), // we can join to instruments and other tables to get the rest of the data.....
+          name = "priceStrategy",
+          keyField = PS.Id,
+          columns = Columns.fromNames(PS.Id.int(), PS.PriceStrategy.string()), // we can join to instruments and other tables to get the rest of the data.....
           VisualLinks(),
-          joinFields = Id
+          joinFields = PS.Id
         ),
-        (table, vs) => new NotYetImplementedProvider(table),
+        (table, _) => new PriceStrategyProvider(table),
       )
       .asModule()
+  }
+
+  object PriceStrategy {
+    final val Id = "id"
+    final val PriceStrategy = "priceStrategy"
+    final val NotionalValue = "notionalValue"
+    final val NotionalValueUsd = "notionalValue"
   }
 
   object BasketColumnNames {
@@ -89,6 +100,7 @@ object BasketModule extends DefaultModule {
   }
 
   object BasketConstituentColumnNames {
+    final val RicBasketId = "ricBasketId"
     final val Ric = "ric"
     final val BasketId = "basketId"
     final val Weighting = "weighting"
@@ -97,4 +109,38 @@ object BasketModule extends DefaultModule {
     final val Volume = "volume"
   }
 
+  object BasketTradingColumnNames {
+    final val BasketId = "basketId"
+    final val InstanceId = "instanceId"
+    final val BasketName = "basketName"
+    final val FxRateToUsd = "fxRateToUsd"
+    final val Units = "units"
+    final val TotalNotionalUsd = "totalNotionalUsd"
+    final val TotalNotional = "totalNotional"
+    //BasketTrading Screen
+    final val Status = "status"
+    final val FilledPct = "filledPct"
+  }
+
+  object BasketTradingConstituentColumnNames {
+    final val InstanceIdRic = "instanceIdRic"
+    final val InstanceId = "instanceId"
+    final val BasketId = "basketId"
+    final val Ric = "ric"
+    final val Description = "description"
+    final val Quantity = "quantity"
+    final val Last = "last"
+    final val Bid = "bid"
+    final val Offer = "offer"
+    final val LimitPrice = "limitPrice"
+    final val PriceStrategyId = "priceStrategyId"
+    final val NotionalUsd = "notionalUsd"
+    final val NotionalLocal = "notionalLocal"
+    final val Venue = "venue"
+    final val Algo = "algo"
+    final val AlgoParams = "algoParams"
+    final val PctFilled = "pctFilled"
+    final val Weighting = "weighting"
+    final val PriceSpread = "priceSpread"
+  }
 }
--- vuu/src/main/scala/org/finos/vuu/core/module/basket/provider/BasketProvider.scala
+++ vuu/src/main/scala/org/finos/vuu/core/module/basket/provider/BasketProvider.scala
@@ -1,9 +1,10 @@
 package org.finos.vuu.core.module.basket.provider
 
+import org.finos.toolbox.time.Clock
 import org.finos.vuu.core.table.DataTable
 import org.finos.vuu.provider.DefaultProvider
 
-class BasketProvider(val table: DataTable) extends DefaultProvider {
+class BasketProvider(val table: DataTable)(implicit clock: Clock) extends DefaultProvider {
 
   override val lifecycleId: String = "org.finos.vuu.core.module.basket.provider.BasketProvider"
 }
