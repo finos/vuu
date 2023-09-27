3e0646ff70824c9c476fefb07a8bb71905cf60df
heswell <steve@heswell.com>
2023-09-27 21:16:52 +0100

    measured-container

---
+++ vuu-ui/packages/vuu-layout/src/measured-container/MeasuredContainer.tsx
@@ -0,0 +1,71 @@
+import { CSSProperties, forwardRef, ForwardedRef, HTMLAttributes } from "react";
+import { MeasuredSize, useMeasuredContainer } from "./useMeasuredContainer";
+import { useForkRef } from "@salt-ds/core";
+import cx from "classnames";
+
+import "./MeasuredContainer.css";
+
+export interface MeasuredContainerProps extends HTMLAttributes<HTMLDivElement> {
+  height?: number | string;
+  onResize?: (size: MeasuredSize) => void;
+  width?: number | string;
+}
+
+const baseClass = "vuuMeasuredContainer";
+
+export const MeasuredContainer = forwardRef(function MeasuredContainer(
+  {
+    children,
+    className,
+    height,
+    onResize,
+    style,
+    width,
+    ...htmlAttributes
+  }: MeasuredContainerProps,
+  forwardedRef: ForwardedRef<HTMLDivElement>
+) {
+  const { containerRef, ...containerMeasurements } = useMeasuredContainer({
+    height,
+    onResize,
+    width,
+  });
+
+  const { cssSize, innerSize } = containerMeasurements;
+  const unmeasured = innerSize === undefined;
+
+  const getStyle = () => {
+    return unmeasured
+      ? ({
+          "--measured-css-height": `${cssSize.height}`,
+          "--measured-css-width": `${cssSize.width}`,
+        } as CSSProperties)
+      : ({
+          ...style,
+          "--measured-css-height": `${cssSize.height}`,
+          "--measured-css-width": `${cssSize.width}`,
+          "--measured-px-height": `${innerSize?.height}px`,
+          "--measured-px-width": `${innerSize?.width}px`,
+        } as CSSProperties);
+  };
+
+  const forkedRef = useForkRef(containerRef, forwardedRef);
+
+  return unmeasured ? (
+    <div
+      {...htmlAttributes}
+      className={cx(baseClass, `${baseClass}-ummeasured`)}
+      style={getStyle()}
+      ref={containerRef}
+    />
+  ) : (
+    <div
+      {...htmlAttributes}
+      className={cx(baseClass, className)}
+      ref={forkedRef}
+      style={getStyle()}
+    >
+      {children}
+    </div>
+  );
+});
---
+++ vuu-ui/packages/vuu-layout/src/measured-container/useMeasuredContainer.ts
@@ -0,0 +1,210 @@
+import { isValidNumber } from "@finos/vuu-utils";
+import {
+  CSSProperties,
+  RefObject,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { MeasuredContainerProps } from "./MeasuredContainer";
+import { useResizeObserver, ResizeHandler } from "./useResizeObserver";
+
+const ClientWidthHeight = ["clientHeight", "clientWidth"];
+const WidthOnly = ["clientWidth"];
+const HeightOnly = ["clientHeight"];
+const NO_MEASUREMENT = [] as const;
+
+export interface ClientSize {
+  clientHeight: number;
+  clientWidth: number;
+}
+
+export interface MeasuredProps
+  extends Pick<MeasuredContainerProps, "height" | "onResize" | "width"> {
+  defaultHeight?: number;
+  defaultWidth?: number;
+}
+
+export type CssSize = Pick<CSSProperties, "height" | "width">;
+
+export interface MeasuredSize {
+  height: number;
+  width: number;
+}
+
+interface MeasuredState {
+  css: CssSize;
+  outer: CssSize;
+  inner?: MeasuredSize;
+}
+
+const isNumber = (val: unknown): val is number => Number.isFinite(val);
+
+const FULL_SIZE: CssSize = { height: "100%", width: "100%" };
+
+export interface MeasuredContainerHookResult {
+  containerRef: RefObject<HTMLDivElement>;
+  cssSize: CssSize;
+  outerSize: CssSize;
+  innerSize?: MeasuredSize;
+}
+
+// If (outer) height and width are known at initialisation (i.e. they
+// were passed as props), use as initial values for inner size. If there
+// is no border on Table, these values will not change. If there is a border,
+// inner values will be updated once measured.
+const getInitialCssSize = (height: unknown, width: unknown): CssSize => {
+  if (isValidNumber(height) && isValidNumber(width)) {
+    return {
+      height: `${height}px`,
+      width: `${width}px`,
+    };
+  } else if (typeof height === "string" && typeof width === "string") {
+    return {
+      height,
+      width,
+    };
+  } else {
+    return FULL_SIZE;
+  }
+};
+
+const getInitialInnerSize = (
+  height: unknown,
+  width: unknown
+): MeasuredSize | undefined => {
+  if (isValidNumber(height) && isValidNumber(width)) {
+    return {
+      height,
+      width,
+    };
+  }
+};
+
+export const useMeasuredContainer = ({
+  defaultHeight = 0,
+  defaultWidth = 0,
+  height,
+  onResize: onResizeProp,
+  width,
+}: MeasuredProps): MeasuredContainerHookResult => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [size, setSize] = useState<MeasuredState>({
+    css: getInitialCssSize(height, width),
+    inner: getInitialInnerSize(height, width),
+    outer: {
+      height: height ?? "100%",
+      width: width ?? "100%",
+    },
+  });
+  const fixedHeight = typeof height === "number";
+  const fixedWidth = typeof width === "number";
+  const dimensions =
+    fixedHeight && fixedWidth
+      ? NO_MEASUREMENT
+      : fixedHeight
+      ? WidthOnly
+      : fixedWidth
+      ? HeightOnly
+      : ClientWidthHeight;
+
+  useMemo(() => {
+    setSize((currentSize) => {
+      const { inner, outer } = currentSize;
+      if (isValidNumber(height) && isValidNumber(width) && inner && outer) {
+        const { height: innerHeight, width: innerWidth } = inner;
+        const { height: outerHeight, width: outerWidth } = outer;
+
+        if (outerHeight !== height || outerWidth !== width) {
+          const heightDiff = isValidNumber(outerHeight)
+            ? outerHeight - innerHeight
+            : 0;
+          const widthDiff = isValidNumber(outerWidth)
+            ? outerWidth - innerWidth
+            : 0;
+          return {
+            ...currentSize,
+            outer: { height, width },
+            inner: { height: height - heightDiff, width: width - widthDiff },
+          };
+        }
+      }
+      return currentSize;
+    });
+  }, [height, width]);
+
+  const onResize: ResizeHandler = useCallback(
+    ({ clientWidth, clientHeight }: Partial<ClientSize>) => {
+      const { css, inner, outer } = size;
+      let newState: MeasuredState = size;
+
+      if (
+        fixedHeight &&
+        isNumber(clientWidth) &&
+        clientWidth !== inner?.width
+      ) {
+        newState = {
+          css,
+          outer,
+          inner: {
+            width: Math.floor(clientWidth) || defaultWidth,
+            height,
+          },
+        };
+      } else if (
+        fixedWidth &&
+        isNumber(clientHeight) &&
+        clientHeight !== inner?.height
+      ) {
+        newState = {
+          css,
+          outer,
+          inner: {
+            height: Math.floor(clientHeight) || defaultHeight,
+            width,
+          },
+        };
+      } else if (
+        isNumber(clientHeight) &&
+        isNumber(clientWidth) &&
+        (clientWidth !== inner?.width || clientHeight !== inner?.height)
+      ) {
+        newState = {
+          css,
+          outer,
+          inner: {
+            width: Math.floor(clientWidth) || defaultWidth,
+            height: Math.floor(clientHeight) || defaultHeight,
+          },
+        };
+      }
+
+      if (newState !== size) {
+        setSize(newState);
+        if (newState.inner !== undefined) {
+          onResizeProp?.(newState.inner);
+        }
+      }
+    },
+    [
+      defaultHeight,
+      defaultWidth,
+      fixedHeight,
+      fixedWidth,
+      height,
+      onResizeProp,
+      size,
+      width,
+    ]
+  );
+
+  useResizeObserver(containerRef, dimensions, onResize, true);
+
+  return {
+    containerRef,
+    cssSize: size.css,
+    outerSize: size.outer,
+    innerSize: size.inner,
+  };
+};
