07bca1a98c67c3b5e8a1231141baa3db371e8cf3
keikeicheung <keikei274@gmail.com>
2023-09-29 00:19:19 +0800

    fix test

--- vuu/src/test/scala/org/finos/vuu/provider/BasketConstituentProviderTest.scala
+++ vuu/src/test/scala/org/finos/vuu/provider/BasketConstituentProviderTest.scala
@@ -3,9 +3,11 @@ package org.finos.vuu.provider
 import org.finos.toolbox.jmx.{MetricsProvider, MetricsProviderImpl}
 import org.finos.toolbox.lifecycle.LifecycleContainer
 import org.finos.toolbox.time.TestFriendlyClock
-import org.finos.vuu.core.module.basket.BasketModule.{BASKET_CONSTITUENT_TABLE_DEF, BasketConstituentColumnNames}
+import org.finos.vuu.api.{TableDef, VisualLinks}
+import org.finos.vuu.core.module.FieldDefString
+import org.finos.vuu.core.module.basket.BasketModule.{BasketConstituentColumnNames => BC}
 import org.finos.vuu.core.module.basket.provider.BasketConstituentProvider
-import org.finos.vuu.core.table.{Column, SimpleDataTable, ViewPortColumnCreator}
+import org.finos.vuu.core.table.{Column, Columns, SimpleDataTable, ViewPortColumnCreator}
 import org.scalatest.BeforeAndAfter
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
@@ -16,7 +18,21 @@ class BasketConstituentProviderTest extends AnyFeatureSpec with Matchers with Be
   implicit val timeProvider: TestFriendlyClock = new TestFriendlyClock(TEST_TIME)
   implicit val metrics: MetricsProvider = new MetricsProviderImpl
   implicit val lifecycleContainer: LifecycleContainer = new LifecycleContainer
-  val table = new SimpleDataTable(BASKET_CONSTITUENT_TABLE_DEF, joinProvider)
+
+  val tableDef = TableDef(
+    name = "basketConstituent",
+    keyField = BC.RicBasketId,
+    columns = Columns.fromNames(new FieldDefString(BC.RicBasketId).string(),
+      new FieldDefString(BC.Ric).string(),
+      new FieldDefString(BC.BasketId).string(),
+      new FieldDefString(BC.Weighting).double(),
+      new FieldDefString(BC.LastTrade).string(),
+      new FieldDefString(BC.Change).string(),
+      new FieldDefString(BC.Volume).string()), // we can join to instruments and other tables to get the rest of the data.....
+    VisualLinks(),
+    joinFields = BC.RicBasketId
+  )
+  val table = new SimpleDataTable(tableDef, joinProvider)
   val provider = new BasketConstituentProvider(table)
   val columns: Array[Column] = provider.table.getTableDef.columns
   val headers: Array[String] = columns.map(_.name)
@@ -28,47 +44,47 @@ class BasketConstituentProviderTest extends AnyFeatureSpec with Matchers with Be
   Feature("Able to load basket constituents from .FTSE100 and show on basket constituent table") {
 
     Scenario("display ric") {
-      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BasketConstituentColumnNames.Ric)) == "AAL.L")
+      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BC.Ric)) == "AAL.L")
     }
 
     Scenario("display basket id") {
-      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BasketConstituentColumnNames.BasketId)) == ".FTSE100")
+      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BC.BasketId)) == ".FTSE100")
     }
 
     Scenario("display change") {
-      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BasketConstituentColumnNames.Change)) == "�5.35�(1.24%)")
+      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BC.Change)) == "�5.35�(1.24%)")
     }
 
     // TODO emily - volume with , is not parsed correctly
 //    Scenario("display volume") {
-//      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BasketConstituentColumnNames.Volume)) == "5,799,089")
+//      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BC.Volume)) == "5,799,089")
 //    }
 
     Scenario("display weighting") {
-      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BasketConstituentColumnNames.Weighting)) == null)
+      assert(getDataForBasket(".FTSE100")(0)(headers.indexOf(BC.Weighting)) == null)
     }
   }
 
   Feature("Able to load basket constituents from .NASDAQ100 and show on basket constituent table") {
 
     Scenario("display ric") {
-      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BasketConstituentColumnNames.Ric)) == "AAPL")
+      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BC.Ric)) == "AAPL")
     }
 
     Scenario("display basket id") {
-      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BasketConstituentColumnNames.BasketId)) == ".NASDAQ100")
+      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BC.BasketId)) == ".NASDAQ100")
     }
 
     Scenario("display change") {
-      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BasketConstituentColumnNames.Change)) == null)
+      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BC.Change)) == null)
     }
 
     Scenario("display volume") {
-      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BasketConstituentColumnNames.Volume)) == null)
+      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BC.Volume)) == null)
     }
 
     Scenario("display weighting") {
-      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BasketConstituentColumnNames.Weighting)) == "11.007")
+      assert(getDataForBasket(".NASDAQ100")(0)(headers.indexOf(BC.Weighting)) == "11.007")
     }
   }
 
@@ -79,6 +95,6 @@ class BasketConstituentProviderTest extends AnyFeatureSpec with Matchers with Be
     data
   }
   private def getDataForBasket(basketId:String) = {
-    getData.filter(e => e(headers.indexOf(BasketConstituentColumnNames.BasketId))==basketId).toArray
+    getData.filter(e => e(headers.indexOf(BC.BasketId))==basketId).toArray
   }
 }
