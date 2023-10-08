aed82c77b7f6402a9951e5e57460f55717cfceba
heswell <steve@heswell.com>
2023-10-08 10:57:03 +0100

    fix type issues

--- vuu-ui/packages/vuu-ui-controls/src/drag-drop/Draggable.tsx
+++ vuu-ui/packages/vuu-ui-controls/src/drag-drop/Draggable.tsx
@@ -15,7 +15,7 @@ import "./Draggable.css";
 const makeClassNames = (classNames: string) =>
   classNames.split(" ").map((className) => `vuuDraggable-${className}`);
 export const Draggable = forwardRef<
-  HTMLElement,
+  HTMLDivElement,
   {
     wrapperClassName: string;
     element: HTMLElement;
