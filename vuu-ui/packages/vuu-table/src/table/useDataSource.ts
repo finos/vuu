110a91b66333255a72e68a1c8efeebe1a591d0df
heswell <steve@heswell.com>
2023-10-16 14:30:44 +0100

    fix display of context menus scoped to selected rows (#908)

--- vuu-ui/packages/vuu-table/src/table/useDataSource.ts
+++ vuu-ui/packages/vuu-table/src/table/useDataSource.ts
@@ -257,6 +257,6 @@ export class MovingWindow {
   }
 
   getSelectedRows() {
-    return this.data.filter((row) => row[SELECTED] === 1);
+    return this.data.filter((row) => row[SELECTED] !== 0);
   }
 }
