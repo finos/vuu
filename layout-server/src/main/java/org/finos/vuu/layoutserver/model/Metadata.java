6c0de9aac12db0a245e6176ff7aeeaaec41a6c94
cfisher-scottlogic <cfisher@scottlogic.com>
2023-10-16 10:56:17 +0100

    VUU25: Increase max length of screenshot column

--- layout-server/src/main/java/org/finos/vuu/layoutserver/model/Metadata.java
+++ layout-server/src/main/java/org/finos/vuu/layoutserver/model/Metadata.java
@@ -1,14 +1,14 @@
 package org.finos.vuu.layoutserver.model;
 
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import lombok.Data;
-
-import java.util.Date;
-import java.util.UUID;
 
 @Data
 @Entity
@@ -22,9 +22,15 @@ public class Metadata {
     private Layout layout;
 
     private String name;
+
     private String group;
+
+    @Column(length = 16384)
     private String screenshot;
+
     private String user;
+
     private Date created = new Date();
+
     private Date updated;
 }
