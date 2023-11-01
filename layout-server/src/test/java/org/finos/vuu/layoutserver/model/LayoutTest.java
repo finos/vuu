76b2280e2b379f258cad5d1fa38261725cc77243
vferraro-scottlogic <vferraro@scottlogic.com>
2023-11-01 13:53:08 +0000

    VUU-89 sort imports and remove unneeded annotation

--- layout-server/src/test/java/org/finos/vuu/layoutserver/model/LayoutTest.java
+++ layout-server/src/test/java/org/finos/vuu/layoutserver/model/LayoutTest.java
@@ -1,15 +1,11 @@
 package org.finos.vuu.layoutserver.model;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
 
-@ExtendWith(MockitoExtension.class)
 class LayoutTest {
 
         @Test
@@ -17,8 +13,8 @@ class LayoutTest {
             UUID id = UUID.fromString("00000000-0000-0000-0000-000000000000");
             Layout layout = new Layout();
             Metadata metadata = new Metadata();
-
             layout.setMetadata(metadata);
+
             layout.setId(id);
 
             assertThat(layout.getId()).isEqualTo(id);
