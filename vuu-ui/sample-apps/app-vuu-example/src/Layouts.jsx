b9900397cc92fc527d269a00d5d31674b20b21e3
heswell <steve@heswell.com>
2023-11-01 14:46:57 +0000

    remove sample app

--- vuu-ui/sample-apps/app-vuu-example/src/Layouts.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { FlexboxLayout as Flexbox, Placeholder } from "@finos/vuu-layout";
-
-export const twoColumns = (
-  <Flexbox style={{ flexDirection: "column" }}>
-    <Placeholder data-resizeable style={{ flex: 1 }} />
-    <Placeholder data-resizeable style={{ flex: 1 }} />
-  </Flexbox>
-);
