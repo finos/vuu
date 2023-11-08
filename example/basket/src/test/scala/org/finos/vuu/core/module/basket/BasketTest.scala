dadc1147deeea1bd3775d4a24f48951a4a632fbd
chris <chris.stevenson@gmail.com>
2023-11-08 21:04:06 +0000

    #850 added ability to create viewport in test easily

--- example/basket/src/test/scala/org/finos/vuu/core/module/basket/BasketTest.scala
+++ example/basket/src/test/scala/org/finos/vuu/core/module/basket/BasketTest.scala
@@ -10,6 +10,7 @@ import org.finos.vuu.core.table.TableTestHelper.combineQs
 import org.finos.vuu.test.VuuServerTestCase
 import org.finos.vuu.util.table.TableAsserts.assertVpEq
 import org.scalatest.prop.Tables.Table
+
 class BasketTest extends VuuServerTestCase {
 
   Feature("Example Test Case") {
@@ -21,24 +22,25 @@ class BasketTest extends VuuServerTestCase {
       implicit val tableDefContainer: TableDefContainer = new TableDefContainer(Map())
       implicit val metricsProvider: MetricsProvider = new MetricsProviderImpl
 
-      withVuuServer(PriceModule(), BasketModule()) { vuuServer =>
+      withVuuServer(PriceModule(), BasketModule()) {
+        vuuServer =>
 
-        vuuServer.overrideViewPortDef("prices", (table, _, _, _) => ViewPortDef(table.getTableDef.columns, null))
+          vuuServer.overrideViewPortDef("prices", (table, _, _, _) => ViewPortDef(table.getTableDef.columns, null))
 
-        val pricesProvider = vuuServer.getProvider("PRICE", "prices")
+          val pricesProvider = vuuServer.getProvider("PRICE", "prices")
 
-        pricesProvider.tick("VOD.L", Map("ric" -> "VOD.L", "phase" -> "C"))
-//
-        val viewport = vuuServer.createViewPort("PRICE", "prices")
+          pricesProvider.tick("VOD.L", Map("ric" -> "VOD.L", "phase" -> "C"))
 
-        vuuServer.runOnce()
+          val viewport = vuuServer.createViewPort("PRICE", "prices")
 
-        assertVpEq(combineQs(viewport)){
-          Table(
-            ("ric", "bid", "ask", "bidSize", "askSize", "last", "open", "close", "phase", "scenario"),
-            ("VOD.L", null, null, null, null, null, null, null, "C", null)
-          )
-        }
+          vuuServer.runOnce()
+
+          assertVpEq(combineQs(viewport)) {
+            Table(
+              ("ric", "bid", "ask", "bidSize", "askSize", "last", "open", "close", "phase", "scenario"),
+              ("VOD.L", null, null, null, null, null, null, null, "C", null)
+            )
+          }
       }
     }
   }
