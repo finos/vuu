5cce83a60969488e7da0942b0ab62a5e76414d29
Vasco <98337074+vferraro-scottlogic@users.noreply.github.com>
2023-11-08 16:56:31 +0000

    Update layout-server/src/main/java/org/finos/vuu/layoutserver/repository/MetadataRepository.java
    Co-authored-by: Cara <99646608+cfisher-scottlogic@users.noreply.github.com>

--- layout-server/src/main/java/org/finos/vuu/layoutserver/repository/MetadataRepository.java
+++ layout-server/src/main/java/org/finos/vuu/layoutserver/repository/MetadataRepository.java
@@ -8,5 +8,4 @@ import java.util.UUID;
 
 
 @Repository
-public interface MetadataRepository extends CrudRepository<Metadata, UUID> {
-}
+public interface MetadataRepository extends CrudRepository<Metadata, UUID> {}
