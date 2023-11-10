a9af14738f0e09287b50ec4ad008d476bbd65103
vferraro-scottlogic <vferraro@scottlogic.com>
2023-11-10 15:59:25 +0000

    VUU-81 remove unused imports

--- layout-server/src/main/java/org/finos/vuu/layoutserver/dto/response/LayoutResponseDto.java
+++ layout-server/src/main/java/org/finos/vuu/layoutserver/dto/response/LayoutResponseDto.java
@@ -2,10 +2,7 @@ package org.finos.vuu.layoutserver.dto.response;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
-import org.finos.vuu.layoutserver.utils.ObjectNodeConverter;
 
-import javax.persistence.Column;
-import javax.persistence.Convert;
 import java.util.UUID;
 
 @Data
