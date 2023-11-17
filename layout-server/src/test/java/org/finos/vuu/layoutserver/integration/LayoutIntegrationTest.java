1ad1522446b135d04aa51cc10b38136abf9e9d7f
vferraro-scottlogic <vferraro@scottlogic.com>
2023-11-17 09:45:12 +0000

    VUU-81 improve naming in test

--- layout-server/src/test/java/org/finos/vuu/layoutserver/integration/LayoutIntegrationTest.java
+++ layout-server/src/test/java/org/finos/vuu/layoutserver/integration/LayoutIntegrationTest.java
@@ -40,7 +40,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class LayoutIntegrationTest {
 
     private static final String DEFAULT_LAYOUT_DEFINITION_STRING = "{\"id\":\"main-tabs\"}";
-    private static final String DEFAULT_UPDATED_LAYOUT_DEFINITION_STRING = "{\"id\":\"updated-main-tabs\"}";
+    private static final String UPDATED_LAYOUT_DEFINITION_STRING = "{\"id\":\"updated-main-tabs\"}";
     private static final String DEFAULT_LAYOUT_NAME = "Default layout name";
     private static final String DEFAULT_LAYOUT_GROUP = "Default layout group";
     private static final String DEFAULT_LAYOUT_SCREENSHOT = "Default layout screenshot";
@@ -127,7 +127,7 @@ public class LayoutIntegrationTest {
         UUID layout2Id = UUID.randomUUID();
         Layout layout1 = createLayoutWithIdInDatabase(layout1Id);
         Layout layout2 = createLayoutWithIdInDatabase(layout2Id);
-        layout2.setDefinition(objectNodeConverter.convertToEntityAttribute(DEFAULT_UPDATED_LAYOUT_DEFINITION_STRING));
+        layout2.setDefinition(objectNodeConverter.convertToEntityAttribute(UPDATED_LAYOUT_DEFINITION_STRING));
         layout2.getMetadata().getBaseMetadata().setName("Different name");
         layout2.getMetadata().getBaseMetadata().setGroup("Different group");
         layout2.getMetadata().getBaseMetadata().setScreenshot("Different screenshot");
@@ -271,7 +271,7 @@ public class LayoutIntegrationTest {
                 initialLayout);
 
         LayoutRequestDto layoutRequest = createValidLayoutRequest();
-        layoutRequest.setDefinition(objectNodeConverter.convertToEntityAttribute(DEFAULT_UPDATED_LAYOUT_DEFINITION_STRING));
+        layoutRequest.setDefinition(objectNodeConverter.convertToEntityAttribute(UPDATED_LAYOUT_DEFINITION_STRING));
         layoutRequest.getMetadata().getBaseMetadata().setName("Updated name");
         layoutRequest.getMetadata().getBaseMetadata().setGroup("Updated group");
         layoutRequest.getMetadata().getBaseMetadata().setScreenshot("Updated screenshot");
