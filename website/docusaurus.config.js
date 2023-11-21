726c16e0200fd1e92c9a923bb36373c134c199ea
heswell <steve@heswell.com>
2023-11-21 11:27:20 +0000

    add redirect script instead of inject

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ module.exports = {
   customFields: {
     repoUrl: `https://github.com/finos/${projectSlug}`,
   },
-  scripts: ["https://buttons.github.io/buttons.js"],
+  scripts: ["/scripts/redirect.js", "https://buttons.github.io/buttons.js"],
   stylesheets: [
     "https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700&display=swap",
     "https://fonts.googleapis.com/css?family=Raleway:400,500&display=swap",
---
+++ website/static/scripts/redirect.js
@@ -0,0 +1,6 @@
+if (
+  window.innerWidth > 1380 &&
+  window.location.href.indexOf("desktop") === -1
+) {
+  window.location.href = "/desktop";
+}
