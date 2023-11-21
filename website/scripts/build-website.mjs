01a44ff7b5767a098e5f0e3f0fe17dd600b64ce9
heswell <steve@heswell.com>
2023-11-21 12:24:05 +0000

    add script to switch desktop or mobile resources

--- website/scripts/build-website.mjs
+++ website/scripts/build-website.mjs
@@ -1,15 +1,14 @@
-import path from "path";
+import fs from "fs";
 import { execWait, writeFile } from "../../vuu-ui/scripts/utils.mjs";
 
-const desktopPath = path.resolve("../website-desktop");
-console.log(desktopPath);
-
 console.log(`run mobile build`);
+fs.copyFileSync("./src-mobile/pages/index-mobile.js", "./src/pages/index.js");
 await execWait("docusaurus build");
 
 console.log("do some stuff here");
 
 console.log(`run desktop build`);
+fs.copyFileSync("./src-desktop/pages/index-desktop.js", "./src/pages/index.js");
 await execWait(
   "docusaurus build --config ./docusaurus.config.desktop.js --out-dir ./build/desktop"
 );
