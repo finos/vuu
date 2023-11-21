37748f1d37a340734602d8254aedc4eefc3b6415
heswell <steve@heswell.com>
2023-11-21 11:30:54 +0000

    try adding desktop build

--- website/scripts/build-website.mjs
+++ website/scripts/build-website.mjs
@@ -1,10 +1,10 @@
 import { execWait, writeFile } from "../../vuu-ui/scripts/utils.mjs";
 
-// const desktopPath = path.resolve("../website-desktop");
-// console.log(desktopPath);
+const desktopPath = path.resolve("../website-desktop");
+console.log(desktopPath);
 
 console.log(`run mobile build`);
 await execWait("docusaurus build");
 
-// console.log(`run desktop build`);
-// await execWait(`node ./scripts/build-desktop.mjs`, desktopPath);
+console.log(`run desktop build`);
+await execWait(`node ./scripts/build-desktop.mjs`, desktopPath);
