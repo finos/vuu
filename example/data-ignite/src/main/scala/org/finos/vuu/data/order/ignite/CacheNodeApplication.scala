f9e8f9500350a7575e02152d4caceb5df4a78840
Tomasz Rumak <tomasz.rumak@gmail.com>
2023-11-24 23:02:39 +0100

    added query for slice of childorders

--- example/data-ignite/src/main/scala/org/finos/vuu/data/order/ignite/CacheNodeApplication.scala
+++ example/data-ignite/src/main/scala/org/finos/vuu/data/order/ignite/CacheNodeApplication.scala
@@ -3,7 +3,7 @@ package org.finos.vuu.data.order.ignite
 import org.apache.ignite.Ignition
 
 object CacheNodeApplication extends App {
-  IgniteLocalConfig.setPersistenceEnabled(true)
+  IgniteLocalConfig.setPersistenceEnabled(false)
   val configuration = IgniteLocalConfig.create(false)
 
   val ignite = Ignition.getOrStart(configuration)
