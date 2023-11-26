a0953036c82f5a7cdc398d13882a86e2a02a3d28
chris <chris.stevenson@gmail.com>
2023-11-26 15:56:25 +0100

    #989 Added first cut of a plugin structure

--- vuu/src/test/scala/org/finos/vuu/plugin/TestPlugin.scala
+++ vuu/src/test/scala/org/finos/vuu/plugin/TestPlugin.scala
@@ -1,7 +1,9 @@
 package org.finos.vuu.plugin
+import org.finos.toolbox.jmx.MetricsProvider
 import org.finos.vuu.api.TableDef
 import org.finos.vuu.core.table.DataTable
-import org.finos.vuu.feature.{Feature, TableFactory}
+import org.finos.vuu.feature.{Feature, FilterFactory, JoinTableFactory, SessionTableFactory, SortFactory, TableFactory, ViewPortFactory, ViewPortKeysCreator}
+import org.finos.vuu.provider.JoinTableProvider
 
 object TestPlugin{
   def apply(): Plugin = {
@@ -12,5 +14,22 @@ object TestPlugin{
 class TestPlugin extends Plugin with TableFactory {
   override def hasFeature(feature: Feature): Boolean = ???
   override def registerFeature(feature: Feature): Unit = ???
-  override def createTable(tableDef: TableDef): DataTable = ???
+
+  override def tableFactory(implicit metrics: MetricsProvider): TableFactory = ???
+
+  override def pluginType: PluginType = ???
+
+  override def joinTableFactory(implicit metrics: MetricsProvider): JoinTableFactory = ???
+
+  override def sessionTableFactory: SessionTableFactory = ???
+
+  override def viewPortKeysCreator: ViewPortKeysCreator = ???
+
+  override def viewPortFactory: ViewPortFactory = ???
+
+  override def filterFactory: FilterFactory = ???
+
+  override def sortFactory: SortFactory = ???
+
+  override def createTable(tableDef: TableDef, joinTableProvider: JoinTableProvider): DataTable = ???
 }
