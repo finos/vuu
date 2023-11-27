436c889d2c1c4fd3d0b03f9bea9676c71e9ac3c4
Na Lee Ha <na-lee.ha@ubs.com>
2023-11-27 09:26:12 +0000

    #966 when updating basket trade cell value, only update if different from current value

--- example/basket/src/main/scala/org/finos/vuu/core/module/basket/service/BasketTradingService.scala
+++ example/basket/src/main/scala/org/finos/vuu/core/module/basket/service/BasketTradingService.scala
@@ -4,19 +4,20 @@ import com.typesafe.scalalogging.StrictLogging
 import org.finos.toolbox.time.Clock
 import org.finos.vuu.core.module.basket.BasketModule
 import org.finos.vuu.core.module.basket.BasketModule.{BasketTradingConstituentTable, Sides}
-import org.finos.vuu.core.table.{DataTable, RowWithData, TableContainer}
+import org.finos.vuu.core.table.{DataTable, RowWithData, TableContainer, ViewPortColumnCreator}
 import org.finos.vuu.net.rpc.{EditRpcHandler, RpcHandler}
 import org.finos.vuu.net.{ClientSessionId, RequestContext}
 import org.finos.vuu.order.oms.{NewOrder, OmsApi}
 import org.finos.vuu.viewport._
-
-trait BasketTradingServiceIF extends EditRpcHandler {
+trait BasketTradingServiceIF extends EditRpcHandler{
   def sendToMarket(basketInstanceId: String)(ctx: RequestContext): ViewPortAction
 }
+
 
 class BasketTradingService(val table: DataTable, val tableContainer: TableContainer, val omsApi: OmsApi)(implicit clock: Clock) extends RpcHandler with BasketTradingServiceIF with StrictLogging {
 
   import org.finos.vuu.core.module.basket.BasketModule.{BasketTradingColumnNames => BT, BasketTradingConstituentColumnNames => BTC}
+
 
   /**
    * Send basket to market
@@ -54,35 +55,45 @@ class BasketTradingService(val table: DataTable, val tableContainer: TableContai
   }
 
   private def onEditCell(key: String, columnName: String, data: Any, vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    logger.info("Changing cell value for key:" + key + "(" + columnName + ":" + data + ")")
-    table.processUpdate(key, RowWithData(key, Map(BT.InstanceId -> key, columnName -> data)), clock.now())
+    logger.info("Change requested for cell value for key:" + key + "(" + columnName + ":" + data + ")")
 
-    columnName match {
-      case BT.Units =>
-        val constituentTable = tableContainer.getTable(BasketTradingConstituentTable)
-        val constituents = constituentTable.primaryKeys.map(key => constituentTable.pullRow(key)).filter(_.get(BTC.InstanceId) == key)
-        constituents.foreach(row => {
-          val unitsAsInt = data.asInstanceOf[Int]
-          val weighting = row.get(BTC.Weighting)
-          val quantity = (weighting.asInstanceOf[Double] * unitsAsInt).toLong
-          constituentTable.processUpdate(row.key(), RowWithData(row.key(), Map(BTC.InstanceIdRic -> row.key(), BTC.Quantity -> quantity)), clock.now())
-        })
-      case BT.Side =>
-        val constituentTable = tableContainer.getTable(BasketTradingConstituentTable)
-        val constituents = constituentTable.primaryKeys.map(key => constituentTable.pullRow(key)).filter(_.get(BTC.InstanceId) == key)
-        val side = data.asInstanceOf[String]
-        constituents.foreach(row => {
-          val newSide = row.get(BTC.Side) match {
-            case Sides.Buy => Sides.Sell
-            case _ => Sides.Buy
-          }
-          constituentTable.processUpdate(row.key(), RowWithData(row.key(), Map(BTC.InstanceIdRic -> row.key(), BTC.Side -> newSide)), clock.now())
-        })
-
-      case _ =>
+    val currentData = getRowData(key, columnName)
+    if (currentData == data) {
+      logger.info("Current cell value is same and therefore skipping update for key:" + key + "(" + columnName + ":" + data + ")")
     }
+    else {
+      logger.info("Changing cell value for key:" + key + "(" + columnName + ":" + data + ")")
+      table.processUpdate(key, RowWithData(key, Map(BT.InstanceId -> key, columnName -> data)), clock.now())
 
+      columnName match {
+        case BT.Units =>
+          val constituentTable = tableContainer.getTable(BasketTradingConstituentTable)
+          val constituents = constituentTable.primaryKeys.map(key => constituentTable.pullRow(key)).filter(_.get(BTC.InstanceId) == key)
+          constituents.foreach(row => {
+            val unitsAsInt = data.asInstanceOf[Int]
+            val weighting = row.get(BTC.Weighting)
+            val quantity = (weighting.asInstanceOf[Double] * unitsAsInt).toLong
+            constituentTable.processUpdate(row.key(), RowWithData(row.key(), Map(BTC.InstanceIdRic -> row.key(), BTC.Quantity -> quantity)), clock.now())
+          })
+        case BT.Side =>
+          val constituentTable = tableContainer.getTable(BasketTradingConstituentTable)
+          val constituents = constituentTable.primaryKeys.map(key => constituentTable.pullRow(key)).filter(_.get(BTC.InstanceId) == key)
+          constituents.foreach(row => {
+            val newSide = row.get(BTC.Side) match {
+              case Sides.Buy => Sides.Sell
+              case _ => Sides.Buy
+            }
+            constituentTable.processUpdate(row.key(), RowWithData(row.key(), Map(BTC.InstanceIdRic -> row.key(), BTC.Side -> newSide)), clock.now())
+          })
+        case _ =>
+      }
+    }
     ViewPortEditSuccess()
+  }
+
+  private def getRowData(rowKey: String, columnName: String): Any = {
+    val row = table.pullRow(rowKey, ViewPortColumnCreator.create(table, List(columnName)))
+    row.get(columnName)
   }
 
   override def deleteRowAction(): ViewPortDeleteRowAction = ???
@@ -98,6 +109,4 @@ class BasketTradingService(val table: DataTable, val tableContainer: TableContai
   override def onFormSubmit(): ViewPortFormSubmitAction = ???
 
   override def onFormClose(): ViewPortFormCloseAction = ???
-
-
 }
