c52991116605e46cd82af680ee12f32646e8be7d
Na Lee Ha <na-lee.ha@ubs.com>
2023-11-27 09:36:38 +0000

    #982 initial implementation of editing basket trade constituent side

--- example/basket/src/main/scala/org/finos/vuu/core/module/basket/service/BasketTradingConstituentJoinService.scala
+++ example/basket/src/main/scala/org/finos/vuu/core/module/basket/service/BasketTradingConstituentJoinService.scala
@@ -3,13 +3,22 @@ package org.finos.vuu.core.module.basket.service
 import com.typesafe.scalalogging.StrictLogging
 import org.finos.toolbox.time.Clock
 import org.finos.vuu.api.JoinTableDef
-import org.finos.vuu.core.module.basket.BasketModule.BasketTradingConstituentColumnNames.InstanceIdRic
+import org.finos.vuu.core.module.basket.BasketConstants.Side
+import org.finos.vuu.core.module.basket.BasketModule.{BasketTradingConstituentColumnNames => ColumnName}
 import org.finos.vuu.core.table.{DataTable, JoinTable, RowWithData, TableContainer}
 import org.finos.vuu.net.ClientSessionId
 import org.finos.vuu.net.rpc.{EditRpcHandler, RpcHandler}
 import org.finos.vuu.viewport._
 
-class BasketTradingConstituentJoinService(val table: DataTable, val tableContainer: TableContainer)(implicit clock: Clock) extends RpcHandler with EditRpcHandler with StrictLogging {
+trait BasketTradingConstituentJoinServiceIF extends EditRpcHandler {
+  def setSell(selection: ViewPortSelection, session: ClientSessionId): ViewPortAction
+
+  def setBuy(selection: ViewPortSelection, session: ClientSessionId): ViewPortAction
+}
+
+object BasketTradingConstituentJoinService {}
+
+class BasketTradingConstituentJoinService(val table: DataTable, val tableContainer: TableContainer)(implicit clock: Clock) extends BasketTradingConstituentJoinServiceIF with RpcHandler with StrictLogging {
 
   def onDeleteRow(key: String, vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
     ViewPortEditSuccess()
@@ -28,7 +37,7 @@ class BasketTradingConstituentJoinService(val table: DataTable, val tableContain
     val baseTableDef = joinTable.getTableDef.asInstanceOf[JoinTableDef].baseTable
     joinTable.sourceTables.get(baseTableDef.name) match {
       case Some(table: DataTable) =>
-        table.processUpdate(key, RowWithData(key, Map(InstanceIdRic -> key, columnName -> data)), clock.now())
+        table.processUpdate(key, RowWithData(key, Map(ColumnName.InstanceIdRic -> key, columnName -> data)), clock.now())
         ViewPortEditSuccess()
       case None =>
         ViewPortEditFailure("Could not find base table")
@@ -42,25 +51,24 @@ class BasketTradingConstituentJoinService(val table: DataTable, val tableContain
   }
 
   private def onFormSubmit(vp: ViewPort, session: ClientSessionId): ViewPortAction = {
-//    val table = vp.table.asTable
-//    val primaryKeys = table.primaryKeys
-//    val headKey = primaryKeys.head
-//    val sequencerNumber = table.pullRow(headKey).get("sequenceNumber").asInstanceOf[Int].toLong
-//
-//    if (sequencerNumber > 0) {
-//      logger.info("I would now send this fix seq to a fix engine to reset, we're all good:" + sequencerNumber)
-//      CloseDialogViewPortAction(vp.id)
-//    } else {
-//      logger.error("Seq number not set, returning error")
-//      ViewPortEditFailure("Sequencer number has not been set.")
-//    }
+    //    val table = vp.table.asTable
+    //    val primaryKeys = table.primaryKeys
+    //    val headKey = primaryKeys.head
+    //    val sequencerNumber = table.pullRow(headKey).get("sequenceNumber").asInstanceOf[Int].toLong
+    //
+    //    if (sequencerNumber > 0) {
+    //      logger.info("I would now send this fix seq to a fix engine to reset, we're all good:" + sequencerNumber)
+    //      CloseDialogViewPortAction(vp.id)
+    //    } else {
+    //      logger.error("Seq number not set, returning error")
+    //      ViewPortEditFailure("Sequencer number has not been set.")
+    //    }
     CloseDialogViewPortAction(vp.id)
   }
 
   private def onFormClose(vp: ViewPort, session: ClientSessionId): ViewPortAction = {
     CloseDialogViewPortAction(vp.id)
   }
-
 
   override def deleteRowAction(): ViewPortDeleteRowAction = ViewPortDeleteRowAction("", this.onDeleteRow)
 
@@ -77,16 +85,39 @@ class BasketTradingConstituentJoinService(val table: DataTable, val tableContain
   override def onFormClose(): ViewPortFormCloseAction = ViewPortFormCloseAction("", this.onFormClose)
 
   def setSell(selection: ViewPortSelection, session: ClientSessionId): ViewPortAction = {
-    ViewPortEditSuccess()
+    updateSelected(selection,  Map(ColumnName.Side -> Side.Sell))
   }
 
   def setBuy(selection: ViewPortSelection, session: ClientSessionId): ViewPortAction = {
+    updateSelected(selection,  Map(ColumnName.Side -> Side.Buy))
+  }
+
+  private def updateSelected(selection: ViewPortSelection, updates: Map[String, Any]): ViewPortAction = {
+    val selectedKeys = selection.rowKeyIndex.map({ case (key, _) => key }).toList
+    selectedKeys.foreach(key => {
+      //require source table primary key for join table updates
+      val sourceTableKey = Map(ColumnName.InstanceIdRic -> key)
+      update(key, sourceTableKey ++ updates)
+    })
+    //todo handle error
+    //todo care about checking current value? simpler not to
     ViewPortEditSuccess()
   }
 
+  //todo can update multiple rows in one go?
+  private def update(key: String, keyValuesToUpdate: Map[String, Any]): Unit = {
+    val joinTable = table.asTable.asInstanceOf[JoinTable]
+    val baseTableDef = joinTable.getTableDef.asInstanceOf[JoinTableDef].baseTable
+    joinTable.sourceTables.get(baseTableDef.name) match {
+      case Some(table: DataTable) =>
+        table.processUpdate(key, RowWithData(key, keyValuesToUpdate), clock.now())
+        ViewPortEditSuccess()
+      case None =>
+        ViewPortEditFailure("Could not find base table")
+    }
+  }
   override def menuItems(): ViewPortMenu = ViewPortMenu("Direction",
-      new SelectionViewPortMenuItem("Set Sell", "", this.setSell, "SET_SELECTION_SELL"),
-      new SelectionViewPortMenuItem("Set Buy", "", this.setBuy, "SET_SELECTION_Buy")
+    new SelectionViewPortMenuItem("Set Sell", "", this.setSell, "SET_SELECTION_SELL"),
+    new SelectionViewPortMenuItem("Set Buy", "", this.setBuy, "SET_SELECTION_Buy")
   )
-
 }
