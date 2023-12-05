d564d418e391cd2d65bd980a4e20717d5871c3cc
Na Lee Ha <na-lee.ha@ubs.com>
2023-12-05 10:04:15 +0000

    #1036 adding equals and hashcode override  methods to be able to compare ViewPortColumns

--- vuu/src/main/scala/org/finos/vuu/viewport/ViewPortColumns.scala
+++ vuu/src/main/scala/org/finos/vuu/viewport/ViewPortColumns.scala
@@ -2,40 +2,55 @@ package org.finos.vuu.viewport
 
 import org.finos.vuu.core.table.{CalculatedColumn, Column, RowData, RowWithData}
 
-class ViewPortColumns(sourceColumns: List[Column]){
+class ViewPortColumns(sourceColumns: List[Column]) {
 
-    @volatile private var columns: List[Column] = sourceColumns
+  @volatile private var columns: List[Column] = sourceColumns
 
-    def addColumn(column: Column): Unit = {
-      columns = columns ++ List(column)
+  def canEqual(a: Any): Boolean = a.isInstanceOf[ViewPortColumns]
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case that: ViewPortColumns =>
+        that.canEqual(this) &&
+          this.columns.size == that.columns.size &&
+          0 == this.columns.sortBy(c => c.name)
+            .zip(that.columns.sortBy(c => c.name))
+            .count(columnPair => columnPair._1.name != columnPair._2.name || columnPair._1.dataType != columnPair._2.dataType)
+      case _ => false
     }
 
-    def columnExists(name: String): Boolean = {
-      columns.exists(_.name == name)
-    }
+  override def hashCode(): Int = columns.sortBy(c => c.name).map(_.hashCode()).hashCode()
 
-    def getColumns(): List[Column] = columns
+  def addColumn(column: Column): Unit = {
+    columns = columns ++ List(column)
+  }
 
-    def getColumnForName(name: String): Option[Column] = {
-      columns.find(_.name == name)
-    }
+  def columnExists(name: String): Boolean = {
+    columns.exists(_.name == name)
+  }
 
-    def count(): Int = columns.size
+  def getColumns(): List[Column] = columns
 
-    private lazy val hasCalculatedColumn = columns.exists(c => c.isInstanceOf[CalculatedColumn])
+  def getColumnForName(name: String): Option[Column] = {
+    columns.find(_.name == name)
+  }
 
-    def pullRow(key: String, row: RowData): RowData = {
+  def count(): Int = columns.size
 
-      if(!hasCalculatedColumn){
-          row
-      }else{
-        val rowData = this.getColumns().map(c => c.name -> row.get(c)).toMap
-        RowWithData(key, rowData)
-      }
-    }
+  private lazy val hasCalculatedColumn = columns.exists(c => c.isInstanceOf[CalculatedColumn])
 
-  def pullRowAlwaysFilter(key: String, row: RowData): RowData = {
+  def pullRow(key: String, row: RowData): RowData = {
+
+    if (!hasCalculatedColumn) {
+      row
+    } else {
       val rowData = this.getColumns().map(c => c.name -> row.get(c)).toMap
       RowWithData(key, rowData)
+    }
+  }
+
+  def pullRowAlwaysFilter(key: String, row: RowData): RowData = {
+    val rowData = this.getColumns().map(c => c.name -> row.get(c)).toMap
+    RowWithData(key, rowData)
   }
 }
