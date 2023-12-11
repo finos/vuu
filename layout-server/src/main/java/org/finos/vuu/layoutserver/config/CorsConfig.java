f4e1cc409c4a5f24877221f3072038e5d7778ce0
Cara Fisher <cfisher@scottlogic.com>
2023-12-11 14:17:57 +0000

    SLVUU-79: Automatically redirect HTTP to HTTPS

--- layout-server/src/main/java/org/finos/vuu/layoutserver/config/CorsConfig.java
+++ layout-server/src/main/java/org/finos/vuu/layoutserver/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.finos.vuu.layoutserver;
+package org.finos.vuu.layoutserver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
