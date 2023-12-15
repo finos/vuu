388c837f9f5189eba6ffc2c0e40fc519d4736bed
Na Lee Ha <na-lee.ha@ubs.com>
2023-12-15 15:38:04 +0000

    #1052 adding test for loading basket constituent csv file and handling when it errors & sourcing test resources for test

--- example/basket/src/main/scala/org/finos/vuu/core/module/basket/csv/CsvStaticLoader.scala
+++ example/basket/src/main/scala/org/finos/vuu/core/module/basket/csv/CsvStaticLoader.scala
@@ -2,43 +2,69 @@ package org.finos.vuu.core.module.basket.csv
 
 import com.typesafe.scalalogging.StrictLogging
 
+import java.io.File
 import scala.io.Source
+import scala.util.control.NonFatal
 
 object CsvStaticLoader extends StrictLogging {
-  def loadConstituent(basketId: String): Array[Map[String, Any]] = {
+  def loadConstituent(basketId: String, resourcePath: Option[String] = None): Array[Map[String, Any]] = {
+    try {
 
-    val staticDirPath = getClass.getResource("/static").getPath
-    val dir = new java.io.File(staticDirPath)
-    val csvFiles = dir.listFiles.filter(_.isFile)
-      .filter(_.getName.endsWith(basketId.replace(".","").toLowerCase + ".csv"))
-    val csvFile = csvFiles(0)
-    logger.info("Loading basket static:" + basketId + "(" + csvFile + ")")
+      val constituentsFilesDirectory =
+        if(resourcePath.isDefined) resourcePath.get
+        else getClass.getResource("/static").getPath
+
+      val dir = new File(constituentsFilesDirectory)
+      val csvFiles = dir.listFiles.filter(_.isFile)
+        .filter(_.getName.endsWith(basketId.replace(".", "").toLowerCase + ".csv"))
+
+      if (csvFiles.isEmpty) {
+        logger.error(s"Failed to find constituents file for $basketId")
+        Array.empty
+      }
+      else {
+        val csvFile = csvFiles(0)
+        logger.info("Loading basket static:" + basketId + "(" + csvFile + ")")
+        val rows = readFileContent(csvFile)
+        logger.info(s"Found ${rows.length} constituents for basket $basketId")
+
+        val header = rows(0)
+        val symbolInd = header.indexOf("Symbol")
+        val nameInd = header.indexOf("Name")
+        val lastTradeInd = header.indexOf("Last Trade")
+        val volumeInd = header.indexOf("Volume")
+        val weightInd = header.indexOf("Weighting")
+        val changeInd = header.indexOf("Change")
+
+        val constituents = rows.tail.map(e => {
+          val weighting = if (getValueFromIndex(weightInd, e) == null) 0.0D else getValueFromIndex(weightInd, e).toDouble
+          Map[String, Any](
+            "Symbol" -> getValueFromIndex(symbolInd, e),
+            "Last Trade" -> getValueFromIndex(lastTradeInd, e),
+            "Name" -> getValueFromIndex(nameInd, e),
+            "Weighting" -> weighting,
+            "Volume" -> getValueFromIndex(volumeInd, e),
+            "Change" -> getValueFromIndex(changeInd, e)
+          )
+        })
+        constituents
+
+      }
+    }
+    catch {
+      case NonFatal(t) => logger.error(s"Failed to parse constituents for $basketId", t)
+        Array.empty
+    }
+  }
+
+
+  private def readFileContent(csvFile: File): Array[Array[String]] = {
     val bufferedSource = Source.fromFile(csvFile)
     val csv = for (line <- bufferedSource.getLines) yield line.split(",").map(_.trim)
     val array = csv.toArray
     bufferedSource.close
-    var data: Map[String, String] = Map()
-    val header = array(0)
-    val symbolInd = header.indexOf("Symbol")
-    val nameInd = header.indexOf("Name")
-    val lastTradeInd = header.indexOf("Last Trade")
-    val volumeInd = header.indexOf("Volume")
-    val weightInd = header.indexOf("Weighting")
-    val changeInd = header.indexOf("Change")
-    val list = array.tail.map(e => {
-      val weighting = if(getValueFromIndex(weightInd, e) == null) 0.0D else getValueFromIndex(weightInd, e).toDouble
-      Map[String, Any](
-        "Symbol" -> getValueFromIndex(symbolInd, e),
-        "Last Trade" -> getValueFromIndex(lastTradeInd, e),
-        "Name" -> getValueFromIndex(nameInd, e),
-        "Weighting" -> weighting,
-        "Volume" -> getValueFromIndex(volumeInd, e),
-        "Change" -> getValueFromIndex(changeInd, e)
-      )
-    })
-    list
+    array
   }
-
 
   def load: Array[String] = {
     val staticDirPath = getClass.getResource("/static").getPath
---
+++ example/basket/src/test/scala/org/finos/vuu/csv/CsvStaticLoaderTests.scala
@@ -0,0 +1,47 @@
+package org.finos.vuu.csv
+
+import org.finos.vuu.core.module.basket.csv.CsvStaticLoader
+import org.scalatest.featurespec.AnyFeatureSpec
+
+import scala.io.Source
+
+class CsvStaticLoaderTests extends AnyFeatureSpec{
+
+    Feature("CSV loading Test Case") {
+
+      Scenario("Can successfully load and parse basket constituents") {
+       var path = getClass.getResource("/constituents")
+        //his.class.getResourceAsStream("/myfile")
+        var x = ClassLoader.getSystemResource("")
+//        var x = Source.fromURL(path)
+//        var B = Source.fromResource("/static")
+      val testResourcePath =  this.getClass().getResource("/constituents").getPath
+        val constituents = CsvStaticLoader.loadConstituent(".FTSE100", Some(testResourcePath))
+
+        assert(constituents.length == 3)
+        val firstRow = constituents.head
+        assert(firstRow("Symbol") == "AAL.L")
+        assert(firstRow("Last Trade") == "436.35")
+        assert(firstRow("Name") == "Anglo American PLC")
+        assert(firstRow("Weighting") == 0.0278736825813547)
+        assert(firstRow("Volume") == "5799089")
+        assert(firstRow("Change") == "5.35")
+      }
+
+      Scenario("When parsing basket constituents fails return empty") {
+
+        val constituents = CsvStaticLoader.loadConstituent(".FTSEWithError")
+
+        assert(constituents.length == 0)
+      }
+
+
+      Scenario("When no matching basket constituents file return empty") {
+
+        val constituents = CsvStaticLoader.loadConstituent(".NoSuchFile")
+
+        assert(constituents.length == 0)
+      }
+
+    }
+}
