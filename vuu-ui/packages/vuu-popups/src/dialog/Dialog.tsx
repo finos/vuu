819ae99e113e888d86893978716ec67bbe5e63f6
heswell <steve@heswell.com>
2023-12-19 22:37:52 +0000

    WIP

--- vuu-ui/packages/vuu-popups/src/dialog/Dialog.tsx
+++ vuu-ui/packages/vuu-popups/src/dialog/Dialog.tsx
@@ -1,50 +1,85 @@
-import { Scrim } from "@salt-ds/lab";
 import cx from "classnames";
-import { HTMLAttributes, useCallback, useRef } from "react";
-import { Portal } from "../portal";
+import { useThemeAttributes } from "@finos/vuu-utils";
+import { HTMLAttributes, useCallback, useLayoutEffect, useRef } from "react";
 import { DialogHeader } from "../dialog-header";
 
 import "./Dialog.css";
+import { PopupComponentProps, useAnchoredPosition } from "../popup";
 
 const classBase = "vuuDialog";
 
-export interface DialogProps extends HTMLAttributes<HTMLDivElement> {
+const AnchorBody = { current: document.body };
+const EMPTY_PROPS = {};
+
+export interface DialogProps extends HTMLAttributes<HTMLDialogElement> {
+  PopupProps?: Partial<PopupComponentProps>;
   isOpen?: boolean;
   onClose?: () => void;
   hideCloseButton?: boolean;
 }
 
 export const Dialog = ({
+  PopupProps = EMPTY_PROPS,
   children,
   className,
   isOpen = false,
   onClose,
+  style,
   title,
   hideCloseButton = false,
-  ...props
+  ...htmlAttributes
 }: DialogProps) => {
-  const root = useRef<HTMLDivElement>(null);
+  const {
+    anchorElement = AnchorBody,
+    offsetLeft = 0,
+    offsetTop = 0,
+    placement = "below",
+  } = PopupProps;
+
+  const rootRef = useRef<HTMLDialogElement>(null);
+  const [themeClass, densityClass, dataMode] = useThemeAttributes();
+  const { position } = useAnchoredPosition({
+    anchorElement,
+    offsetLeft,
+    offsetTop,
+    placement,
+  });
 
   const close = useCallback(() => {
     onClose?.();
   }, [onClose]);
 
-  if (!isOpen) {
-    return null;
-  }
+  // if (!isOpen) {
+  //   return null;
+  // }
+
+  useLayoutEffect(() => {
+    if (rootRef.current) {
+      rootRef.current.showModal();
+      // if (confirmRef.current) {
+      //   confirmRef.current.focus();
+      // }
+      if (placement.endsWith("center")) {
+        const { width } = rootRef.current.getBoundingClientRect();
+        rootRef.current.style.marginLeft = `-${width / 2}px`;
+      }
+    }
+  }, [placement]);
 
   return (
-    <Portal>
-      <Scrim className={`${classBase}-scrim`} open={isOpen} autoFocusRef={root}>
-        <div {...props} className={cx(classBase, className)} ref={root}>
-          <DialogHeader
-            hideCloseButton={hideCloseButton}
-            onClose={close}
-            title={title}
-          />
-          <div className={`${classBase}-body`}>{children}</div>
-        </div>
-      </Scrim>
-    </Portal>
+    <dialog
+      {...htmlAttributes}
+      className={cx(classBase, themeClass)}
+      data-mode={dataMode}
+      ref={rootRef}
+      style={{ ...style, ...position }}
+    >
+      <DialogHeader
+        hideCloseButton={hideCloseButton}
+        onClose={close}
+        title={title}
+      />
+      <div className={`${classBase}-body`}>{children}</div>
+    </dialog>
   );
 };
