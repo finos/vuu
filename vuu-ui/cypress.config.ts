6a4a04c95070d94713e76ced272c2502f746fca7
heswell <steve@heswell.com>
2023-12-21 11:50:38 +0000

    try localhost in cypress config

--- vuu-ui/cypress.config.ts
+++ vuu-ui/cypress.config.ts
@@ -78,7 +78,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: "http://127.0.0.1:5173/",
+    baseUrl: "http://localhost:5173/",
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
       // implement node event listeners here
