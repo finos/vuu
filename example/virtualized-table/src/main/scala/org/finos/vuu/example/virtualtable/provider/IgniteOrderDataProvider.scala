7e7306de6ad09b342131b2f3000ef6718b784538
Na Lee Ha <na-lee.ha@ubs.com>
2024-01-19 19:57:07 +0000

    #989 mixing ignite filter query with window of rows - disabled filter

--- example/virtualized-table/src/main/scala/org/finos/vuu/example/virtualtable/provider/IgniteOrderDataProvider.scala
+++ example/virtualized-table/src/main/scala/org/finos/vuu/example/virtualtable/provider/IgniteOrderDataProvider.scala
@@ -1,11 +1,10 @@
 package org.finos.vuu.example.virtualtable.provider
 
-import org.apache.ignite.cache.query.IndexQueryCriterion
 import org.finos.toolbox.time.Clock
 import org.finos.vuu.core.filter.FilterSpecParser
-import org.finos.vuu.core.table.{DataTable, RowWithData}
+import org.finos.vuu.core.table.RowWithData
 import org.finos.vuu.data.order.ignite.IgniteOrderStore
-import org.finos.vuu.feature.ignite.filter.{IgniteIndexFilterClause, IgniteIndexFilterTreeVisitor}
+import org.finos.vuu.feature.ignite.filter.{IgniteSqlFilterClause, IgniteSqlFilterTreeVisitor}
 import org.finos.vuu.plugin.virtualized.table.{VirtualizedRange, VirtualizedSessionTable, VirtualizedViewPortKeys}
 import org.finos.vuu.provider.VirtualizedProvider
 import org.finos.vuu.viewport.ViewPort
@@ -24,20 +23,23 @@ class IgniteOrderDataProvider(final val igniteStore: IgniteOrderStore)(implicit 
     internalTable.setSize(totalSize)
     internalTable.setRange(VirtualizedRange(range.from, range.to))
 
-    //todo get filters
-    //todo find child by range + sort criteria + filter criteria
 
-//    val filterTreeVisitor = new IgniteFilterTreeVisitor
-//    val clause = FilterSpecParser.parse[IgniteFilterClause](viewPort.filterSpec.filter, filterTreeVisitor)
-//    val criteria = clause.toCriteria()
-//    val iterator = igniteStore.findChildOrderFilteredBy(criteria)
+//    val sqlFilterClause =
+//      if (viewPort.filterSpec.filter == null || viewPort.filterSpec.filter.isEmpty) {
+//        ""
+//      }
+//      else {
+//        val filterTreeVisitor = new IgniteSqlFilterTreeVisitor
+//        val clause = FilterSpecParser.parse[IgniteSqlFilterClause](viewPort.filterSpec.filter, filterTreeVisitor)
+//        clause.toSql()
+//      }
 
-    //val clause = FilterSpecParser.parse[IgniteFilterClause](viewPort.filterSpec.filter, filterTreeVisitor)
-//
-//    val sqlFilterClause =  ""
-//    val iterator = igniteStore.findChildOrder(sqlFilterClause, range.from, range.to)
+    val sqlFilterClause = ""
+
     val rowCount = if(range.to > range.from) range.to - range.from else 1
-    val iterator = igniteStore.findWindow(range.from, rowCount)
+    val iterator = igniteStore.findChildOrder(sqlFilterClause = sqlFilterClause, startIndex = range.from, rowCount = rowCount)
+
+   // val iterator = igniteStore.findWindow(range.from, rowCount)
 
     val index = new AtomicInteger(range.from) // todo: get rid of working assumption here that the dataset is fairly immutable.
     iterator.foreach(childOrder => {
