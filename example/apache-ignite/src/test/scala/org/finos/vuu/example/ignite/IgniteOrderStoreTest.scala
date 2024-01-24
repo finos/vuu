ccb08a013f201f44a1e1b23d6d6d53bb9d8eced1
Na Lee Ha <na-lee.ha@ubs.com>
2024-01-24 16:18:33 +0000

    989 ignite filter check against column type & supply sort criteria to ignite query

--- example/apache-ignite/src/test/scala/org/finos/vuu/example/ignite/IgniteOrderStoreTest.scala
+++ example/apache-ignite/src/test/scala/org/finos/vuu/example/ignite/IgniteOrderStoreTest.scala
@@ -5,12 +5,16 @@ import org.apache.ignite.{Ignite, IgniteCache}
 import org.finos.vuu.core.module.simul.model.{ChildOrder, ParentOrder}
 import org.scalatest.BeforeAndAfter
 import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatest.matchers.should.Matchers
 
-class IgniteOrderStoreTest extends AnyFunSuiteLike with BeforeAndAfter {
+class IgniteOrderStoreTest extends AnyFunSuiteLike with BeforeAndAfter with Matchers {
   private var ignite: Ignite = _
   private var parentOrderCache: IgniteCache[Int, ParentOrder] = _
   private var childOrderCache: IgniteCache[Int, ChildOrder] = _
   private var orderStore: IgniteOrderStore = _
+
+  private val emptySortQueries: String = ""
+  private val emptyFilterQueries: String = ""
 
   before {
     ignite = TestUtils.setupIgnite()
@@ -84,7 +88,7 @@ class IgniteOrderStoreTest extends AnyFunSuiteLike with BeforeAndAfter {
     parentOrder2 = GivenParentHasChildOrder(parentOrder2, 6)
 
     val filterQueries = "parentId = 2"
-    val childOrder = orderStore.findChildOrder(filterQueries, 1,2)
+    val childOrder = orderStore.findChildOrder(filterQueries, emptySortQueries, 2, 1)
 
     assert(childOrder != null)
     assert(childOrder.size == 2)
@@ -105,7 +109,7 @@ class IgniteOrderStoreTest extends AnyFunSuiteLike with BeforeAndAfter {
     parentOrder2 = GivenParentHasChildOrder(parentOrder2, 6)
 
     val filterQueries = "ric = \'ric\'"
-    val childOrder = orderStore.findChildOrder(filterQueries, 0, 100)
+    val childOrder = orderStore.findChildOrder(filterQueries, emptySortQueries, 100, 0)
 
     assert(childOrder != null)
     assert(childOrder.size == 6)
@@ -119,13 +123,43 @@ class IgniteOrderStoreTest extends AnyFunSuiteLike with BeforeAndAfter {
     parentOrder2 = GivenParentHasChildOrder(parentOrder2, 4)
     parentOrder2 = GivenParentHasChildOrder(parentOrder2, 5)
 
-    val filterQueries = ""
-    val childOrder = orderStore.findChildOrder(filterQueries, 0, 100)
+    val childOrder = orderStore.findChildOrder(emptyFilterQueries, emptySortQueries, 100, 0)
 
     assert(childOrder != null)
     assert(childOrder.size == 3)
   }
 
+  test("Ignite Store With Custom Sql Sort Single Column") {
+    var parentOrder: ParentOrder = GivenParentOrder(1)
+    parentOrder = GivenParentHasChildOrder(parentOrder, 1)
+    parentOrder = GivenParentHasChildOrder(parentOrder, 3)
+
+    var parentOrder2: ParentOrder = GivenParentOrder(2)
+    parentOrder2 = GivenParentHasChildOrder(parentOrder2, 2)
+
+    val sortByValues = "parentId ASC"
+    val childOrder = orderStore.findChildOrder(emptyFilterQueries, sortByValues, 100, 0)
+
+    assert(childOrder != null)
+    assert(childOrder.size == 3)
+    childOrder.map(c => c.id).toArray shouldBe Array(1,3,2)
+  }
+
+  test("Ignite Store With Custom Sql Sort Multiple Columns") {
+    var parentOrder: ParentOrder = GivenParentOrder(1)
+    parentOrder = GivenParentHasChildOrder(parentOrder, 1)
+    parentOrder = GivenParentHasChildOrder(parentOrder, 3)
+
+    var parentOrder2: ParentOrder = GivenParentOrder(2)
+    parentOrder2 = GivenParentHasChildOrder(parentOrder2, 2)
+
+    val sortByValues = "parentId DESC, id ASC"
+    val childOrder = orderStore.findChildOrder(emptyFilterQueries, sortByValues, 100, 0)
+
+    assert(childOrder != null)
+    assert(childOrder.size == 3)
+    childOrder.map(c => c.id).toArray shouldBe Array(2, 1, 3)
+  }
   private def GivenParentOrder(parentOrderId: Int): ParentOrder = {
     val parentOrder = TestUtils.createParentOrder(parentOrderId)
     orderStore.storeParentOrder(parentOrder)
--- plugin/ignite-plugin/src/test/scala/org/finos/vuu/feature/ignite/IgniteTestStore.scala
+++ plugin/ignite-plugin/src/test/scala/org/finos/vuu/feature/ignite/IgniteTestStore.scala
@@ -1,12 +1,11 @@
 package org.finos.vuu.feature.ignite
 
 import com.typesafe.scalalogging.StrictLogging
+import org.apache.ignite.cache.query.{IndexQuery, IndexQueryCriterion, SqlFieldsQuery}
 import org.apache.ignite.cache.{QueryEntity, QueryIndex, QueryIndexType}
 import org.apache.ignite.{Ignite, IgniteCache, Ignition}
-import org.apache.ignite.cache.query.{IndexQuery, IndexQueryCriteriaBuilder, IndexQueryCriterion, SqlFieldsQuery}
 import org.apache.ignite.cluster.ClusterState
 import org.apache.ignite.configuration.{CacheConfiguration, IgniteConfiguration}
-import org.apache.ignite.lang.IgniteBiPredicate
 
 import java.util
 import scala.collection.mutable
@@ -94,13 +93,21 @@ class IgniteTestStore (private val orderCache: IgniteCache[Int, TestOrderEntity]
       .map(x => x.getValue)
   }
 
-  def getFilteredBy(sqlFilterClause: String): Iterable[TestOrderEntity] = {
-
-    val whereClause = if (sqlFilterClause == null || sqlFilterClause.isEmpty) "" else s" where $sqlFilterClause"
+  def getFilteredBy(sqlFilterQuery: String): Iterable[TestOrderEntity] = {
+    val whereClause = if (sqlFilterQuery == null || sqlFilterQuery.isEmpty) "" else s" where $sqlFilterQuery"
     val value = s"select * from TestOrderEntity$whereClause"
+    getSQLAndMapResult(value)
+  }
 
-    logger.info("Querying ignite for "+ value)
-    val query = new SqlFieldsQuery(value)
+  def getSortBy(sqlSortQuery: String): Iterable[TestOrderEntity] = {
+    val orderByClause = if (sqlSortQuery == null || sqlSortQuery.isEmpty) "" else s" order by $sqlSortQuery"
+    val value = s"select * from TestOrderEntity$orderByClause"
+    getSQLAndMapResult(value)
+  }
+
+  private def getSQLAndMapResult(sqlQuery: String): Iterable[TestOrderEntity] = {
+    logger.info("Querying ignite for " + sqlQuery)
+    val query = new SqlFieldsQuery(sqlQuery)
 
     val results = orderCache.query(query)
 
