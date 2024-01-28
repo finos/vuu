c910166ce3b0e41fe54a4eb62b17652b43cf107b
naleeha <nlha86@gmail.com>
2024-01-28 22:38:40 +0000

    #989 introducing interface for selecting column value on type ahead rpc command

---
+++ vuu/src/main/scala/org/finos/vuu/core/module/typeahead/GenericTypeAheadRpcHandler.scala
@@ -0,0 +1,35 @@
+package org.finos.vuu.core.module.typeahead
+
+import com.typesafe.scalalogging.StrictLogging
+import org.finos.vuu.core.table.{DataTable, TableContainer}
+import org.finos.vuu.net.RequestContext
+import org.finos.vuu.net.rpc.RpcHandler
+
+class GenericTypeAheadRpcHandler(val tableContainer: TableContainer) extends TypeAheadRpcHandler with RpcHandler with StrictLogging {
+
+  def getUniqueFieldValues(tableMap: Map[String, String], column: String, ctx: RequestContext): Array[String] = {
+
+    val tableName = tableMap("table")
+
+    tableContainer.getTable(tableName) match {
+      case dataTable: DataTable =>
+        val columValueProvider = dataTable.getColumnValueProvider
+        columValueProvider.getUniqueValues(column)
+      case null =>
+        throw new Exception("Could not find table by name:" + tableName)
+    }
+  }
+
+  override def getUniqueFieldValuesStartingWith(tableMap: Map[String, String], column: String, starts: String, ctx: RequestContext): Array[String] = {
+
+    val tableName = tableMap("table")
+
+    tableContainer.getTable(tableName) match {
+      case dataTable: DataTable =>
+        val columValueProvider = dataTable.getColumnValueProvider
+        columValueProvider.getUniqueValuesStartingWith(column, starts)
+      case null =>
+        throw new Exception("Could not find table by name:" + tableName)
+    }
+  }
+}
---
+++ vuu/src/main/scala/org/finos/vuu/core/table/ColumnValueProvider.scala
@@ -0,0 +1,53 @@
+package org.finos.vuu.core.table
+
+import com.typesafe.scalalogging.StrictLogging
+
+trait ColumnValueProvider {
+
+  //todo currently only returns first 10 results.. so can't scrolling through values
+  //could return everything let ui decide how many results to display but there is cost to the json serialisig for large dataset
+  //todo how to handle nulls - for different data types
+  //todo should this be returning null or rely on json deserialiser rules?
+
+  def getUniqueValues(columnName:String):Array[String]
+  def getUniqueValuesStartingWith(columnName:String, starts: String):Array[String]
+
+}
+
+class InMemColumnValueProvider(dataTable: InMemDataTable) extends ColumnValueProvider with StrictLogging{
+  override def getUniqueValues(columnName: String): Array[String] =
+    dataTable.columnForName(columnName) match {
+    case c: Column =>
+      dataTable.primaryKeys.foldLeft(Set[String]())(addUnique(dataTable, c, _, _)).toArray.sorted.take(10)
+    case null =>
+      logger.error(s"Column $columnName not found in table ${dataTable.name}")
+      Array()
+  }
+
+  override def getUniqueValuesStartingWith(columnName: String, starts: String): Array[String] =
+    dataTable.columnForName(columnName) match {
+      case c: Column =>
+        dataTable.primaryKeys.foldLeft(Set[String]())(addUnique(dataTable, c, _, _)).filter(_.startsWith(starts)).toArray.sorted.take(10)
+      case null =>
+        logger.error(s"Column $columnName not found in table ${dataTable.name}")
+        Array()
+    }
+
+  private def addUnique(dt: DataTable, c: Column, set: Set[String], key: String): Set[String] = {
+    val row = dt.pullRow(key)
+    row.get(c) match {
+      case null =>
+        Set()
+      case x: String =>
+        set.+(x)
+      case x: Long =>
+        set.+(x.toString)
+      case x: Double =>
+        set.+(x.toString)
+      case x: Int =>
+        set.+(x.toString)
+      case x =>
+        set.+(x.toString)
+    }
+  }
+}
