c8244b3cdc5dbd9eb5d63474cd38b2e37995e756
chris <chris.stevenson@gmail.com>
2024-01-28 09:15:54 +0100

    #1000 Resolved issue with calc columns not working on joined tables.

--- vuu/src/test/scala/org/finos/vuu/viewport/CalculatedColumnsViewPortTest.scala
+++ vuu/src/test/scala/org/finos/vuu/viewport/CalculatedColumnsViewPortTest.scala
@@ -1,11 +1,16 @@
 package org.finos.vuu.viewport
 
 import org.finos.toolbox.jmx.{MetricsProvider, MetricsProviderImpl}
+import org.finos.toolbox.lifecycle.LifecycleContainer
 import org.finos.toolbox.time.{Clock, TestFriendlyClock}
+import org.finos.vuu.api.{JoinSpec, JoinTableDef, JoinTo, LeftOuterJoin, TableDef}
 import org.finos.vuu.client.messages.RequestId
 import org.finos.vuu.core.table.TableTestHelper.combineQs
-import org.finos.vuu.core.table.ViewPortColumnCreator
-import org.finos.vuu.util.table.TableAsserts.assertVpEq
+import org.finos.vuu.core.table.{Columns, TableContainer, ViewPortColumnCreator}
+import org.finos.vuu.net.ClientSessionId
+import org.finos.vuu.provider.{JoinTableProviderImpl, MockProvider, ProviderContainer}
+import org.finos.vuu.util.OutboundRowPublishQueue
+import org.finos.vuu.util.table.TableAsserts.{assertVpEq, assertVpEqWithMeta}
 import org.scalatest.GivenWhenThen
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.prop.Tables.Table
@@ -36,17 +41,17 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
       //it should return a compound error
       assertVpEq(combinedUpdates) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","logicTest"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ,"Boo"     ),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ,"Boo"     ),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ,"Boo"     ),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ,"Boo"     ),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ,"Boo"     ),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ,"Boo"     ),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ,"Boo"     ),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ,"Boo"     ),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ,"Boo"     ),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       ,"Boo"     )
+          ("orderId", "trader", "ric", "tradeTime", "quantity", "logicTest"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100, "Boo"),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101, "Boo"),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102, "Boo"),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103, "Boo"),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104, "Boo"),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105, "Boo"),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106, "Boo"),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107, "Boo"),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108, "Boo"),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109, "Boo")
         )
       }
     }
@@ -71,17 +76,17 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
 
       assertVpEq(combinedUpdates) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","logicTest"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ,"Boo"     ),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ,"Boo"     ),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ,"Boo"     ),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ,"Boo"     ),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ,"Boo"     ),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ,"Boo"     ),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ,"Boo"     ),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ,"Boo"     ),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ,"Boo"     ),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       ,"Yay"     )
+          ("orderId", "trader", "ric", "tradeTime", "quantity", "logicTest"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100, "Boo"),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101, "Boo"),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102, "Boo"),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103, "Boo"),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104, "Boo"),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105, "Boo"),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106, "Boo"),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107, "Boo"),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108, "Boo"),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109, "Yay")
         )
       }
     }
@@ -105,17 +110,17 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
 
       assertVpEq(combinedUpdates) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","quantityTimes100"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ,10000     ),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ,10100     ),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ,10200     ),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ,10300     ),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ,10400     ),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ,10500     ),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ,10600     ),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ,10700     ),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ,10800     ),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       ,10900     )
+          ("orderId", "trader", "ric", "tradeTime", "quantity", "quantityTimes100"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100, 10000),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101, 10100),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102, 10200),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103, 10300),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104, 10400),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105, 10500),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106, 10600),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107, 10700),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108, 10800),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109, 10900)
         )
       }
 
@@ -130,17 +135,17 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
 
       assertVpEq(combinedUpdates2) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","textConcat"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ,"NYC-0000VOD.L"),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ,"NYC-0001VOD.L"),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ,"NYC-0002VOD.L"),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ,"NYC-0003VOD.L"),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ,"NYC-0004VOD.L"),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ,"NYC-0005VOD.L"),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ,"NYC-0006VOD.L"),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ,"NYC-0007VOD.L"),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ,"NYC-0008VOD.L"),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       ,"NYC-0009VOD.L")
+          ("orderId", "trader", "ric", "tradeTime", "quantity", "textConcat"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100, "NYC-0000VOD.L"),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101, "NYC-0001VOD.L"),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102, "NYC-0002VOD.L"),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103, "NYC-0003VOD.L"),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104, "NYC-0004VOD.L"),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105, "NYC-0005VOD.L"),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106, "NYC-0006VOD.L"),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107, "NYC-0007VOD.L"),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108, "NYC-0008VOD.L"),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109, "NYC-0009VOD.L")
         )
       }
     }
@@ -165,17 +170,17 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
 
       assertVpEq(combinedUpdates) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       )
+          ("orderId", "trader", "ric", "tradeTime", "quantity"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109)
         )
       }
 
@@ -187,20 +192,96 @@ class CalculatedColumnsViewPortTest extends AbstractViewPortTestCase with Matche
 
       assertVpEq(combinedUpdates2) {
         Table(
-          ("orderId" ,"trader"  ,"ric"     ,"tradeTime","quantity","textConcat"),
-          ("NYC-0000","chris"   ,"VOD.L"   ,1311544800000L,100       ,"NYC-0000VOD.L"),
-          ("NYC-0001","chris"   ,"VOD.L"   ,1311544800000L,101       ,"NYC-0001VOD.L"),
-          ("NYC-0002","chris"   ,"VOD.L"   ,1311544800000L,102       ,"NYC-0002VOD.L"),
-          ("NYC-0003","chris"   ,"VOD.L"   ,1311544800000L,103       ,"NYC-0003VOD.L"),
-          ("NYC-0004","chris"   ,"VOD.L"   ,1311544800000L,104       ,"NYC-0004VOD.L"),
-          ("NYC-0005","chris"   ,"VOD.L"   ,1311544800000L,105       ,"NYC-0005VOD.L"),
-          ("NYC-0006","chris"   ,"VOD.L"   ,1311544800000L,106       ,"NYC-0006VOD.L"),
-          ("NYC-0007","chris"   ,"VOD.L"   ,1311544800000L,107       ,"NYC-0007VOD.L"),
-          ("NYC-0008","chris"   ,"VOD.L"   ,1311544800000L,108       ,"NYC-0008VOD.L"),
-          ("NYC-0009","chris"   ,"VOD.L"   ,1311544800000L,109       ,"NYC-0009VOD.L")
+          ("orderId", "trader", "ric", "tradeTime", "quantity", "textConcat"),
+          ("NYC-0000", "chris", "VOD.L", 1311544800000L, 100, "NYC-0000VOD.L"),
+          ("NYC-0001", "chris", "VOD.L", 1311544800000L, 101, "NYC-0001VOD.L"),
+          ("NYC-0002", "chris", "VOD.L", 1311544800000L, 102, "NYC-0002VOD.L"),
+          ("NYC-0003", "chris", "VOD.L", 1311544800000L, 103, "NYC-0003VOD.L"),
+          ("NYC-0004", "chris", "VOD.L", 1311544800000L, 104, "NYC-0004VOD.L"),
+          ("NYC-0005", "chris", "VOD.L", 1311544800000L, 105, "NYC-0005VOD.L"),
+          ("NYC-0006", "chris", "VOD.L", 1311544800000L, 106, "NYC-0006VOD.L"),
+          ("NYC-0007", "chris", "VOD.L", 1311544800000L, 107, "NYC-0007VOD.L"),
+          ("NYC-0008", "chris", "VOD.L", 1311544800000L, 108, "NYC-0008VOD.L"),
+          ("NYC-0009", "chris", "VOD.L", 1311544800000L, 109, "NYC-0009VOD.L")
         )
       }
 
     }
+
+    Scenario("Check calc columns on joined tables") {
+
+      implicit val lifecycle: LifecycleContainer = new LifecycleContainer
+
+      val dateTime = 1437728400000L
+
+      val ordersDef = TableDef(
+        name = "orders",
+        keyField = "orderId",
+        columns = Columns.fromNames("orderId:String", "trader:String", "ric:String", "tradeTime:Long", "quantity:Double"),
+        joinFields = "ric", "orderId")
+
+      val pricesDef = TableDef("prices", "ric", Columns.fromNames("ric:String", "bid:Double", "ask:Double", "last:Double", "open:Double", "close:Double"), "ric")
+
+      val joinDef = JoinTableDef(
+        name = "orderPrices",
+        baseTable = ordersDef,
+        joinColumns = Columns.allFrom(ordersDef) ++ Columns.allFromExcept(pricesDef, "ric"),
+        joins =
+          JoinTo(
+            table = pricesDef,
+            joinSpec = JoinSpec(left = "ric", right = "ric", LeftOuterJoin)
+          ),
+        joinFields = Seq()
+      )
+
+      val joinProvider = JoinTableProviderImpl()
+
+      val tableContainer = new TableContainer(joinProvider)
+
+      val orders = tableContainer.createTable(ordersDef)
+      val prices = tableContainer.createTable(pricesDef)
+      val orderPrices = tableContainer.createJoinTable(joinDef)
+
+      val ordersProvider = new MockProvider(orders)
+      val pricesProvider = new MockProvider(prices)
+
+      val providerContainer = new ProviderContainer(joinProvider)
+
+      val viewPortContainer = setupViewPort(tableContainer, providerContainer)
+
+      joinProvider.start()
+
+      ordersProvider.tick("NYC-0001", Map("orderId" -> "NYC-0001", "trader" -> "chris", "tradeTime" -> dateTime, "quantity" -> 100D, "ric" -> "VOD.L"))
+      pricesProvider.tick("VOD.L", Map("ric" -> "VOD.L", "bid" -> 220.0, "ask" -> 222.0, "last" -> 30))
+
+      pricesProvider.tick("BT.L", Map("ric" -> "BT.L", "bid" -> 500.0, "ask" -> 501.0, "last" -> 40))
+      ordersProvider.tick("NYC-0002", Map("orderId" -> "NYC-0002", "trader" -> "chris", "tradeTime" -> dateTime, "quantity" -> 100D, "ric" -> "BT.L"))
+
+      joinProvider.runOnce()
+
+      val session = ClientSessionId("sess-01", "chris")
+
+      val outQueue = new OutboundRowPublishQueue()
+
+      val vpcolumns = ViewPortColumnCreator.create(orderPrices, List("orderId", "trader", "tradeTime", "quantity", "ric", "bid", "ask", "qtyBid:Double:=quantity * bid")) //.map(orderPrices.getTableDef.columnForName(_)).toList
+
+      val viewPort = viewPortContainer.create(RequestId.oneNew(), session, outQueue, orderPrices, DefaultRange, vpcolumns)
+
+      viewPortContainer.runOnce()
+
+      val combinedUpdates = combineQs(viewPort)
+
+      When("we pull rows with a calc column as array we don't get a failure")
+      val rows = combinedUpdates
+        .filter(vpu => vpu.vpUpdate == RowUpdateType)
+        .map(vpu => orderPrices.pullRowAsArray(vpu.key.key, vpcolumns).toList )
+        .toList
+
+      rows should equal(List(
+        List("NYC-0001", "chris", 1437728400000L, 100.0D, "VOD.L", 220.0D, 222.0D, 22000.0D),
+        List("NYC-0002", "chris", 1437728400000L, 100.0D, "BT.L", 500.0D, 501.0D, 50000.0D),
+      ))
+    }
   }
 }
+
