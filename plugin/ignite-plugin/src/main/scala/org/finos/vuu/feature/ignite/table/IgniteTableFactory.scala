d130fc34998a3c66c4c8f46a5bffccc730617409
naleeha <nlha86@gmail.com>
2024-02-12 12:02:56 +0000

    #989 adding tables to the container in the plugin table factory methods to be consistent

--- plugin/ignite-plugin/src/main/scala/org/finos/vuu/feature/ignite/table/IgniteTableFactory.scala
+++ plugin/ignite-plugin/src/main/scala/org/finos/vuu/feature/ignite/table/IgniteTableFactory.scala
@@ -1,10 +1,10 @@
 package org.finos.vuu.feature.ignite.table
 
 import org.finos.vuu.api.TableDef
-import org.finos.vuu.core.table.DataTable
+import org.finos.vuu.core.table.{DataTable, TableContainer}
 import org.finos.vuu.feature.TableFactory
 import org.finos.vuu.provider.JoinTableProvider
 
 class IgniteTableFactory extends TableFactory {
-  override def createTable(tableDef: TableDef, joinTableProvider: JoinTableProvider): DataTable = ???
+  override def createTable(tableDef: TableDef, tableContainer: TableContainer, joinTableProvider: JoinTableProvider): DataTable = ???
 }
--- plugin/virtualized-table-plugin/src/main/scala/org/finos/vuu/plugin/virtualized/plugin/ViewPortVirtualizedTableCreator.scala
+++ plugin/virtualized-table-plugin/src/main/scala/org/finos/vuu/plugin/virtualized/plugin/ViewPortVirtualizedTableCreator.scala
@@ -9,20 +9,22 @@ import org.finos.vuu.plugin.virtualized.api.VirtualizedSessionTableDef
 import org.finos.vuu.plugin.virtualized.table.VirtualizedSessionTable
 import org.finos.vuu.viewport.{GroupBy, RowSource}
 
-object ViewPortVirtualizedTableCreator extends ViewPortTableCreator{
+object ViewPortVirtualizedTableCreator extends ViewPortTableCreator {
 
-  override def create(table: RowSource, clientSession: ClientSessionId, groupBy: GroupBy, tableContainer: TableContainer)(implicit metrics: MetricsProvider, clock: Clock): RowSource = {
+override def create(table: RowSource, clientSession: ClientSessionId, groupBy: GroupBy, tableContainer: TableContainer)(implicit metrics: MetricsProvider, clock: Clock): RowSource = {
 
     assert(table.asTable.getTableDef.isInstanceOf[VirtualizedSessionTableDef])
 
     val sessionTableDef = table.asTable.getTableDef.asInstanceOf[VirtualizedSessionTableDef]
 
+    createSessionTable(clientSession, sessionTableDef, tableContainer)
+  }
+
+  private def createSessionTable(clientSession: ClientSessionId, sessionTableDef: VirtualizedSessionTableDef, tableContainer: TableContainer) (implicit metrics: MetricsProvider, clock: Clock) : RowSource= {
     val sessionTable = new VirtualizedSessionTable(clientSession, sessionTableDef, tableContainer.joinTableProvider, cacheSize = 20_000)
 
     val archetypeTable = tableContainer.getTable(sessionTableDef.name)
-
     val provider = archetypeTable.getProvider
-
     sessionTable.setProvider(provider)
 
     tableContainer.addTable(sessionTable)
