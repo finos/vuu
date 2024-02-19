38ab9ddee708105d6b1801cc013186c77a7c500e
Malik, Junaid <junaidzm@connect.hku.hk>
2024-02-19 16:11:25 +0800

    #1184 use single JsonMapper for JsonUtil object methods
    - we were instantiating an ObjectMapper everytime toRawJson or toPrettyRawJson
      methods were being called, which is wasteful. With this change we
      1) instantiate the mapper once on JsonUtil object creation
      2) share the same mapper between all methods
      3) use JsonMapper, a JSON specific implementation of ObjectMapper

--- toolbox/src/test/scala/org/finos/toolbox/json/JsonUtilTest.scala
+++ toolbox/src/test/scala/org/finos/toolbox/json/JsonUtilTest.scala
@@ -1,8 +1,9 @@
 package org.finos.toolbox.json
 
-import org.finos.toolbox.json.JsonUtil.{fromJson, toRawJson}
+import org.finos.toolbox.json.JsonUtil.{fromJson, toPrettyJson, toRawJson}
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks.{Table, forAll}
 
 class JsonUtilTest extends AnyFeatureSpec with Matchers {
 
@@ -16,13 +17,43 @@ class JsonUtilTest extends AnyFeatureSpec with Matchers {
     }
   }
 
+  Feature("toPrettyJson") {
+    Scenario("can convert an instance of a case class to pretty json string") {
+      val dto = TestDto(name = "orange", value = 13.7, quantity = Some(5))
+
+      val result = toPrettyJson(dto)
+
+      result shouldEqual "{\n  \"name\" : \"orange\",\n  \"value\" : 13.7,\n  \"quantity\" : 5\n}"
+    }
+  }
+
   Feature("fromJson") {
-    Scenario("can create an instance of a case class from json string") {
-      val jsonAsString = """{"name": "apple", "value": 20.5, "quantity": 7}"""
+    forAll(Table(
+      ("scenario", "json-as-string"),
+      ("json with double-quotes", """{"name": "apple", "value": 20.5, "quantity": 7}"""),
+      ("json with single-quotes", """{'name': 'apple', 'value': 20.5, 'quantity': 7}"""),
+      ("json with unquoted field names", """{name: "apple", value: 20.5, quantity: 7}"""),
+      ("json with mixed single & double quotes", """{'name': "apple", "value": 20.5, 'quantity': 7}"""),
+      ("json with quoted and non-quoted field names", """{name: 'apple', "value": 20.5, 'quantity': 7}"""),
+    ))((scenario, jsonAsString) => {
+      Scenario(s"can create an instance of a case class from `$scenario`") {
+        val result = fromJson[TestDto](jsonAsString)
+        result shouldEqual TestDto(name = "apple", value = 20.5, quantity = Some(7))
+      }
+    })
 
-      val result = fromJson[TestDto](jsonAsString)
+    Scenario("can generate an array of DTOs from a json array") {
+      val jsonArray ="""[
+          |{"name": "apple", "value": 20.5, "quantity": 15},
+          |{"name": "orange", "value": 0.45, "quantity": 7}
+          |]""".stripMargin
 
-      result shouldEqual TestDto(name = "apple", value = 20.5, quantity = Some(7))
+      val result = fromJson[List[TestDto]](jsonArray)
+
+      result shouldEqual List(
+        TestDto(name = "apple", value = 20.5, quantity = Some(15)),
+        TestDto(name = "orange", value = 0.45, quantity = Some(7))
+      )
     }
 
     Scenario("falls back to `None` when an `Option` field is missing") {
