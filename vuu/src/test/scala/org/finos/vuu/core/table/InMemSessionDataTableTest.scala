4ed478357423c22bb64074a04de43ecaefe84a3c
Malik, Junaid <junaidzm@connect.hku.hk>
2024-02-22 19:39:07 +0800

    #1195 enable metric tracking for each instance of session table
    - there were two bugs that needed fixing:
      1. TableContainer.getTables() returned array where table name actually
         reflected the tableDef.name. Because of this returned session tables
         and its blueprints had same names and the metrics table only added
         one and it seemed as if we're only considering the archetype.
      2. this one was a bit more involved. First we were using tableDef.name
         to update metrics in InMemDataTable, hence, if we updated metrics for
         a session table it appeared as if we're updating for the archetype.
         Second, if we even had tried calling sessionTable.name in InMemDataTable
         to initialize the metric counters/meters it still would've not worked
         because in session table we used an instance variable `createInstant`
         (used in session-table name) which is not initialized at the time we
         call super class (InMemDataTable) constructor.

---
+++ vuu/src/test/scala/org/finos/vuu/core/table/InMemSessionDataTableTest.scala
@@ -0,0 +1,41 @@
+package org.finos.vuu.core.table
+
+import org.finos.toolbox.jmx.{MetricsProvider, MetricsProviderImpl}
+import org.finos.toolbox.time.{Clock, DefaultClock}
+import org.finos.vuu.api.{Indices, SessionTableDef}
+import org.finos.vuu.core.module.ModuleFactory.stringToString
+import org.finos.vuu.net.ClientSessionId
+import org.finos.vuu.test.TestFriendlyJoinTableProvider
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
+
+class InMemSessionDataTableTest extends AnyFeatureSpec with Matchers {
+  private implicit val metricsProvider: MetricsProvider = new MetricsProviderImpl()
+  private implicit val clock: Clock = new DefaultClock()
+  private val clientSessionId = ClientSessionId(sessionId = "sessionId", user = "user")
+  private val joinProvider = new TestFriendlyJoinTableProvider()
+
+  private val sessionTableDef: SessionTableDef = new SessionTableDef(
+    name = "test-table",
+    keyField = "id",
+    columns = Columns.fromNames("id".long(), "field".string()),
+    indices = Indices(),
+    joinFields = Seq.empty
+  )
+
+  private val inMemSessionDataTable = new InMemSessionDataTable(clientSessionId, sessionTableDef, joinProvider)
+
+  Feature("Metrics update") {
+    Scenario("Should correctly update metrics WHEN processUpdate called") {
+      inMemSessionDataTable.processUpdate("1", RowWithData("1", Map("id" -> 1, "field" -> "value1")), clock.now())
+      inMemSessionDataTable.processUpdate("2", RowWithData("2", Map("id" -> 2, "field" -> "value2")), clock.now())
+
+      val counter = metricsProvider.counter(inMemSessionDataTable.name + ".processUpdates.Counter")
+      val meter = metricsProvider.meter(inMemSessionDataTable.name + ".processUpdates.Meter")
+
+      counter.getCount shouldEqual 2
+      meter.getCount shouldEqual 2
+    }
+  }
+
+}
