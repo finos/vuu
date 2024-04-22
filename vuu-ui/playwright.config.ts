e5a1b60e24c465a17fa085338d3f660aff5ead8b
heswell <steve@heswell.com>
2024-04-22 22:05:49 +0100

    improve playwright file naming

--- vuu-ui/playwright.config.ts
+++ vuu-ui/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["html", { outputFolder: "./playwright/reports" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -26,6 +26,7 @@ export default defineConfig({
     },
   ],
   snapshotPathTemplate: "./playwright/snapshots/{testFilePath}/{arg}{ext}",
+  outputDir: "./playwright/test-results",
   webServer: {
     command: "npm run showcase:preview",
   },
