a1344c0545d72d540c1b408a902cee5b42a2101b
Na Lee Ha <na-lee.ha@ubs.com>
2024-04-30 16:31:44 +0100

    #1318 example of how to use schema mapper to get data and update in memory table

---
+++ vuu/src/main/scala/org/finos/vuu/api/ColumnBuilder.scala
@@ -0,0 +1,32 @@
+package org.finos.vuu.api
+
+import org.finos.vuu.core.table.{Column, Columns}
+
+import scala.collection.mutable.ArrayBuilder
+
+class ColumnBuilder {
+
+  val columns = new ArrayBuilder.ofRef[String]()
+
+  def addString(columnName: String): ColumnBuilder = {
+    columns +: (columnName + ":String")
+    this
+  }
+
+  def addDouble(columnName: String): ColumnBuilder = {
+    columns +: (columnName + ":Double")
+    this
+  }
+
+  def addInt(columnName: String): ColumnBuilder = {
+    columns +: (columnName + ":Int")
+    this
+  }
+
+  def addLong(columnName: String): ColumnBuilder = {
+    columns +: (columnName + ":Long")
+    this
+  }
+
+  def build: Array[Column] = Columns.fromNames(columns.result())
+}
