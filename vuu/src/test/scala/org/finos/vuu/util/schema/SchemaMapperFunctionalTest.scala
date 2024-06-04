587b0466ef030b4d42232f618b9f476311bea81c
Na Lee Ha <na-lee.ha@ubs.com>
2024-06-04 16:49:54 +0100

    #1318 schema mapper functional test for when table column type is different from field type

--- vuu/src/test/scala/org/finos/vuu/util/schema/SchemaMapperFunctionalTest.scala
+++ vuu/src/test/scala/org/finos/vuu/util/schema/SchemaMapperFunctionalTest.scala
@@ -3,6 +3,7 @@ package org.finos.vuu.util.schema
 import org.finos.vuu.api.{ColumnBuilder, TableDef}
 import org.finos.vuu.core.table.Columns
 import org.finos.vuu.test.FakeInMemoryTable
+import org.finos.vuu.util.types.{TypeConverter, TypeConverterContainerBuilder}
 
 class SchemaMapperFunctionalTest extends SchemaMapperFunctionalTestBase {
 
@@ -118,7 +119,47 @@ class SchemaMapperFunctionalTest extends SchemaMapperFunctionalTestBase {
       assert(existingTableRows.head.get("notionalValue") == 10.5)
     }
 
-    Scenario("When table has columns with different type from fields on external entity") {}
+    Scenario("When table has columns with different type from fields on external entity") {
+
+      val externalEntitySchema: ExternalEntitySchema = ExternalEntitySchemaBuilder()
+        .withField("id", classOf[Int])
+        .withField("decimalValue", classOf[BigDecimal])
+        .withIndex("ID_INDEX", List("id"))
+        .build()
+
+      val tableDef = TableDef(
+        name = "MyExampleTable",
+        keyField = "id",
+        columns = new ColumnBuilder()
+          .addString("id")
+          .addDouble("doubleValue")
+          .build()
+      )
+      val typeConverterContainer = TypeConverterContainerBuilder()
+        .withConverter(TypeConverter[BigDecimal, Double](classOf[BigDecimal], classOf[Double], _.doubleValue))
+        .withConverter(TypeConverter[Double, BigDecimal](classOf[Double], classOf[BigDecimal], v => BigDecimal(v.toString)))
+        .build()
+      val schemaMapper = SchemaMapperBuilder(externalEntitySchema, tableDef.columns)
+        .withFieldsMap(
+          Map(
+            "id" -> "id",
+            "decimalValue" -> "doubleValue",
+          )
+        )
+        .withTypeConverters(typeConverterContainer)
+        .build()
+      val table = new FakeInMemoryTable("SchemaMapTest", tableDef)
+      givenQueryReturns(queryName, List(List(10, BigDecimal("1.0001"))))
+
+      getDataAndUpdateTable(table, schemaMapper, queryName)
+
+      val existingTableRows = table.pullAllRows()
+      assert(existingTableRows.size == 1)
+      assert(existingTableRows.head.get("id") == "10")
+      assert(existingTableRows.head.get("doubleValue") == 1.0001d)
+
+    }
+
     Scenario("When query result has less number of fields than table columns return useful error") {}
     Scenario("When column and field order does not match but no fields map defined on schema return useful error") {}
     Scenario("When getting data from source fails return useful error") {}
