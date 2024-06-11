6bc0cf3b9a2e3b378ac0b5628ce093cad177d21a
heswell <steve@heswell.com>
2024-06-11 23:18:57 +0100

    fixed for dialogs and filtering
    showDialog should not cause apop to rerender
    filter that reduces resultset to less rows than
    visible in viewport should always render correctly

---
+++ vuu-ui/showcase/src/examples/Popups/DialogProvider.examples.tsx
@@ -0,0 +1,48 @@
+import { DialogProvider } from "@finos/vuu-popups";
+import { useDialogContext } from "@finos/vuu-popups";
+import { Button } from "@salt-ds/core";
+import { MouseEventHandler, useCallback, useRef, useState } from "react";
+
+let displaySequence = 1;
+
+const ComponentThatShowsDialog = () => {
+  const renderCountRef = useRef(0);
+  const [, forceRefresh] = useState({});
+  const { showDialog } = useDialogContext();
+
+  renderCountRef.current += 1;
+
+  const handleClick = useCallback(() => {
+    showDialog(
+      <div style={{ width: 200, height: 130, background: "cornflowerblue" }} />,
+      "A Dialog"
+    );
+  }, [showDialog]);
+
+  const buttonClick = useCallback<MouseEventHandler>((e) => {
+    e.stopPropagation();
+    forceRefresh({});
+  }, []);
+
+  return (
+    <div
+      style={{ width: 700, height: 500, background: "yellow" }}
+      onClick={handleClick}
+    >
+      <p>Click the button to force a render.</p>
+      <p>Click yellow div to show a Dialog</p>
+      <p>Show Dialog should not trigger render</p>
+
+      <Button
+        onClick={buttonClick}
+      >{`rendered ${renderCountRef.current} times`}</Button>
+    </div>
+  );
+};
+
+export const DefaultDialogProvider = () => (
+  <DialogProvider>
+    <ComponentThatShowsDialog />
+  </DialogProvider>
+);
+DefaultDialogProvider.displaySequence = displaySequence++;
