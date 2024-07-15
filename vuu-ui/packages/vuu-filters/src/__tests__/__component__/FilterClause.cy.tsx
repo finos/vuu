ea8b8d11b46be8fe888efb2804670a255bac33c4
heswell <steve@heswell.com>
2024-07-15 15:18:43 +0100

    fixes and tests for filterclause

---
+++ vuu-ui/packages/vuu-filters/src/__tests__/__component__/FilterClause.cy.tsx
@@ -0,0 +1,34 @@
+import {
+  NewFilterClause,
+  PartialFilterClauseColumnAndOperator,
+} from "../../../../../showcase/src/examples/Filters/FilterBar/FilterClause.examples";
+
+describe("FilterClause", () => {
+  describe("WHEN new filter clause is rendered", () => {
+    it("THEN expected classname is present", () => {
+      cy.mount(<NewFilterClause />);
+      const container = cy.findByTestId("filterclause");
+      container.should("have.class", "vuuFilterClause");
+    });
+
+    it("THEN component is rendered with column input, is focused and shows suggestions", () => {
+      cy.mount(<NewFilterClause />);
+      const container = cy.findByTestId("filterclause");
+      container.find(".vuuFilterClauseField").should("have.length", 1);
+      container.find("input").should("be.focused");
+      cy.findAllByRole("option", { name: "currency" }).should("be.visible");
+    });
+  });
+
+  describe("WHEN partial filter clause with Column and Operator is rendered", () => {
+    it("THEN component is rendered with controls for column, operator and value", () => {
+      cy.mount(<PartialFilterClauseColumnAndOperator />);
+      const container = cy.findByTestId("filterclause");
+      container.find(".vuuFilterClauseField").should("have.length", 3);
+      cy.findByTestId("filterclause")
+        .find(".vuuFilterClauseValue input")
+        .should("be.focused");
+      cy.findAllByRole("option", { name: "GBP" }).should("be.visible");
+    });
+  });
+});
