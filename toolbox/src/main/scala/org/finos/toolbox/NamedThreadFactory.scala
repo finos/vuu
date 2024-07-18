9aaa401dc5272b604042996927dfd74c76586bea
Tomasz Rumak <tomasz.rumak@gmail.com>
2024-07-18 11:08:00 +0200

    cleanup

--- toolbox/src/main/scala/org/finos/toolbox/NamedThreadFactory.scala
+++ toolbox/src/main/scala/org/finos/toolbox/NamedThreadFactory.scala
@@ -1,23 +1,31 @@
 package org.finos.toolbox
 
 import java.util.concurrent.ThreadFactory
-import java.util.concurrent.atomic.AtomicInteger
 
-object NamedThreadFactory  {
-  val map = new java.util.HashMap[String, AtomicInteger]()
+object NamedThreadFactory{
 
-  def nextName(name: String): String  = {
-    if (!map.containsKey(name)) {
-      map.put(name, new AtomicInteger())
-     }
+  private val map = new java.util.HashMap[String, Int]()
 
-    name + map.get(name).incrementAndGet()
-   }
+  def nextName(name: String): String = {
+
+    map.synchronized {
+
+      val index = if (map.containsKey(name)) {
+        map.get(name) + 1
+      } else {
+        1
+      }
+
+      map.put(name, index)
+      name + index
+    }
+
+  }
 }
 
-class NamedThreadFactory(name: String) extends ThreadFactory  {
-  override def newThread(r: Runnable): Thread  = {
-    val threadName  = NamedThreadFactory.nextName(name)
+class NamedThreadFactory(name: String) extends ThreadFactory {
+  override def newThread(r: Runnable): Thread = {
+    val threadName = NamedThreadFactory.nextName(name)
     new Thread(r, threadName)
-   }
+  }
 }
