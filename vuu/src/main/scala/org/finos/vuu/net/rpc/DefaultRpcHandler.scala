8ba95284c7c328d23f9c96ce9ba26818a95b0c47
Tomasz Rumak <tomasz.rumak@gmail.com>
2024-07-22 16:03:15 +0200

    Work in progress on improved rpc handling

--- vuu/src/main/scala/org/finos/vuu/net/rpc/DefaultRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/DefaultRpcHandler.scala
@@ -1,7 +1,7 @@
 package org.finos.vuu.net.rpc
 
 import com.typesafe.scalalogging.StrictLogging
-import org.finos.vuu.net.{Error, JsonViewServerMessage, RequestContext, RpcCall, RpcResponse, RpcSuccess, ViewServerMessage, VsMsg}
+import org.finos.vuu.net.{Error, RequestContext, RpcCall, RpcResponse, ViewServerMessage, VsMsg}
 import org.finos.vuu.viewport.{ViewPortAction, ViewPortRpcFailure, ViewPortRpcSuccess}
 
 import java.util.concurrent.ConcurrentHashMap
@@ -24,25 +24,6 @@ class DefaultRpcHandler extends RpcHandler with StrictLogging {
     methodHandlers.put(methodName, handler)
   }
 
-  /**
-   * Register a function as a handler for a given rpc method
-   *
-   * @param methodName name of the rpc method
-   * @param handler    function that takes RpcParams and returns RpcMethodCallResult
-   * @return
-   */
-  def registerRpcMethodHandler(methodName: String, handler: java.util.function.Function[RpcParams, RpcMethodCallResult]): RpcMethodHandler = {
-    val rpcMethodHandler = new RpcJavaFunctionMethodHandler(handler)
-
-    registerRpcMethodHandler(methodName, rpcMethodHandler)
-  }
-
-  def registerRpcMethodHandler(methodName: String, handler: RpcParams => RpcMethodCallResult): RpcMethodHandler = {
-    val rpcMethodHandler = new RpcFunctionMethodHandler(handler)
-
-    registerRpcMethodHandler(methodName, rpcMethodHandler)
-  }
-
   override def processViewPortRpcCall(methodName: String, params: Array[Any], namedParams: Map[String, Any])(ctx: RequestContext): ViewPortAction = {
     val result = processRpcMethodHandler(methodName, params, namedParams, ctx)
     result match {
@@ -55,17 +36,18 @@ class DefaultRpcHandler extends RpcHandler with StrictLogging {
     val method = rpc.method
     val params = rpc.params
     val namedPars = rpc.namedParams
+    val module = Option(msg).map(_.module).getOrElse("")
 
     processRpcMethodHandler(method, params, namedPars, ctx) match {
       case result: RpcMethodSuccess => Some(VsMsg(ctx.requestId, ctx.session.sessionId, ctx.token, ctx.session.user, RpcResponse(method, result, error = null)))
-      case error: RpcMethodFailure => Some(VsMsg(ctx.requestId, ctx.session.sessionId, ctx.token, ctx.session.user, RpcResponse(rpc.method, null, Error(error.error, error.code)), module = msg.module))
+      case error: RpcMethodFailure => Some(VsMsg(ctx.requestId, ctx.session.sessionId, ctx.token, ctx.session.user, RpcResponse(rpc.method, null, Error(error.error, error.code)), module))
     }
   }
 
   private def processRpcMethodHandler(methodName: String, params: Array[Any], namedParams: Map[String, Any], ctx: RequestContext) = {
     if (methodHandlers.containsKey(methodName)) {
       methodHandlers.get(methodName) match {
-        case null => new RpcMethodFailure("Could not find rpcMethodHandler $methodName")
+        case null => new RpcMethodFailure(s"Could not find rpcMethodHandler $methodName")
         case handler: RpcMethodHandler =>
           try {
             handler.call(new RpcParams(params, namedParams, ctx))
@@ -76,7 +58,7 @@ class DefaultRpcHandler extends RpcHandler with StrictLogging {
           }
       }
     } else {
-      new RpcMethodFailure("Could not find rpcMethodHandler $methodName")
+      new RpcMethodFailure(s"Could not find rpcMethodHandler $methodName")
     }
   }
 }
--- vuu/src/test/scala/org/finos/vuu/net/rpc/DefaultRpcHandlerTest.scala
+++ vuu/src/test/scala/org/finos/vuu/net/rpc/DefaultRpcHandlerTest.scala
@@ -1,66 +1,87 @@
 package org.finos.vuu.net.rpc
 
-import org.finos.vuu.net.JsonViewServerMessage
-import org.finos.vuu.viewport.{ViewPortRpcFailure, ViewPortRpcSuccess}
+import org.finos.vuu.net.{ClientSessionId, Error, JsonViewServerMessage, RequestContext, RpcCall, RpcResponse}
+import org.finos.vuu.util.PublishQueue
+import org.finos.vuu.viewport.{ViewPortRpcFailure, ViewPortRpcSuccess, ViewPortUpdate}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class DefaultRpcHandlerTest extends AnyFlatSpec with Matchers {
-  val handler = new DefaultRpcHandler
+class DefaultRpcHandlerTest extends AnyFlatSpec with Matchers with BeforeAndAfterEach {
+  var handler: DefaultRpcHandler = _
+  val ctx = RequestContext("requestId", ClientSessionId("sessionId", "user"), null, "token")
 
-//  "The processViewPortRpcCall method" should "return ViewPortRpcSuccess when the rpc method is successful" in {
-//    val result = handler.processViewPortRpcCall("myMethod", Array("param1"), Map("namedParam1" -> "value1"))(null)
-//
-//    result should be (ViewPortRpcSuccess())
-//  }
-//
-//  it should "return ViewPortRpcFailure when the rpc method fails" in {
-//    val result = handler.processViewPortRpcCall("myMethod", Array("param1"), Map("namedParam1" -> "value1"))(null)
-//
-//    result should be (ViewPortRpcFailure("Exception occurred calling rpc myMethod"))
-//  }
+  override def beforeEach(): Unit = {
+    handler = new DefaultRpcHandler
+  }
 
-//  "The processRpcCall method" should "return a VsMsg with RpcResponse when the rpc method is successful" in {
-//
-//
-//    val result = handler.processRpcCall(msg, rpc)(null)
-//
-//    result should be (Some(VsMsg(msg.requestId, msg.session.sessionId, msg.token, msg.session.user, RpcResponse("myMethod", RpcSuccess(), null), "myModule")))
-//  }
+  "The registerRpcMethodHandler method" should "register a handler for a given rpc method" in {
+    val rpcFunctionMethodHandler = new RpcFunctionMethodHandler(_ => RpcMethodSuccess("result"))
 
-//  it should "return a VsMsg with RpcResponse and Error when the rpc method fails" in {
-//    val msg = JsonViewServerMessage.parse("""{
-//                                            |  "msgType": "RpcCall",
-//                                            |  "module": "myModule",
-//                                            |  "rpc": {
-//                                            |    "method": "myMethod",
-//                                            |    "params": ["param1"],
-//                                            |    "namedParams": {
-//                                            |      "namedParam1": "value1"
-//                                            |    }
-//                                            |  }
-//                                            |}""")
-//
-//    val result = handler.processRpcCall(msg, msg.rpc)(null)
-//
-//    result should be (Some(VsMsg(msg.requestId, msg.session.sessionId, msg.token, msg.session.user, RpcResponse("myMethod", null, Error("Exception occurred calling rpc myMethod", 1)), "myModule")))
-//  }
-//
-//  it should "return None when the method is not found" in {
-//    val msg = JsonViewServerMessage.parse("""{
-//                                            |  "msgType": "RpcCall",
-//                                            |  "module": "myModule",
-//                                            |  "rpc": {
-//                                            |    "method": "myMethod",
-//                                            |    "params": ["param1"],
-//                                            |    "namedParams": {
-//                                            |      "namedParam1": "value1"
-//                                            |    }
-//                                            |  }
-//                                            |}""")
-//
-//    val result = handler.processRpcCall(msg, msg.rpc)(null)
-//
-//    result should be (None)
-//  }
+    handler.registerRpcMethodHandler("myMethod", rpcFunctionMethodHandler)
+
+    handler.processViewPortRpcCall("myMethod", Array("param1"), Map("namedParam1" -> "value1"))(null) should be (ViewPortRpcSuccess())
+  }
+
+  "The registerRpcMethodHandler method" should "not register a function under already registered name" in {
+    val rpcFunctionMethodHandler = new RpcFunctionMethodHandler(_ => RpcMethodSuccess("result"))
+
+    handler.registerRpcMethodHandler("myMethod", rpcFunctionMethodHandler)
+
+    assertThrows[IllegalArgumentException] {
+      handler.registerRpcMethodHandler("myMethod", rpcFunctionMethodHandler)
+    }
+  }
+
+  "The registerRpcMethodHandler method" should "register a java function and call it" in {
+    val rpcJavaFunctionMethodHandler = new RpcJavaFunctionMethodHandler(params => RpcMethodSuccess(params.namedParams("namedParam1").toString))
+
+    handler.registerRpcMethodHandler("myMethod", rpcJavaFunctionMethodHandler)
+
+    handler.processViewPortRpcCall("myMethod", Array("param1"), Map("namedParam1" -> "value1"))(null) should be (ViewPortRpcSuccess())
+  }
+
+  "The processViewPortRpcCall method with null params" should "return ViewPortRpcSuccess when the rpc method is successful" in {
+    handler.registerRpcMethodHandler("myMethod", _ => RpcMethodSuccess("result"))
+
+    val result = handler.processViewPortRpcCall("myMethod", null, null)(null)
+
+    result should be (ViewPortRpcSuccess())
+  }
+
+  "The processViewPortRpcCall method with null params" should "return ViewPortRpcFailure when the rpc method fails" in {
+    handler.registerRpcMethodHandler("myMethod", _ => RpcMethodFailure(1, "error", new Exception("exception")))
+
+    val result = handler.processViewPortRpcCall("myMethod", null, null)(null)
+
+    result should be (ViewPortRpcFailure("Exception occurred calling rpc myMethod"))
+  }
+
+  "The processRpcCall method" should "return Some(ViewServerMessage) when the rpc method is successful" in {
+    handler.registerRpcMethodHandler("myMethod", _ => RpcMethodSuccess("result"))
+
+    val rpcCall = RpcCall("myService", "myMethod", Array("param1"), Map("namedParam1" -> "value1"))
+
+    val result = handler.processRpcCall(null, rpcCall)(ctx)
+
+    result should be (Some(JsonViewServerMessage("requestId", "sessionId", "token", "user", RpcResponse("myMethod", RpcMethodSuccess("result"), null))))
+  }
+
+  "The processRpcCall method" should "return Some(ViewServerMessage) when the rpc method fails" in {
+    handler.registerRpcMethodHandler("myMethod", _ => RpcMethodFailure(1, "error", new Exception("exception")))
+
+    val rpcCall = RpcCall("myService", "myMethod", Array("param1"), Map("namedParam1" -> "value1"))
+
+
+    val result = handler.processRpcCall(null, rpcCall)(ctx)
+
+    result should be (Some(JsonViewServerMessage("requestId", "sessionId", "token", "user", RpcResponse("myMethod", null, Error("error", 1)), "")))
+  }
+
+  "The processRpcCall method" should "return error when method not found " in {
+    val rpcCall = RpcCall("myService", "myMethod", Array("param1"), Map("namedParam1" -> "value1"))
+    handler.processRpcCall(null, rpcCall)(ctx) should be (Some(JsonViewServerMessage("requestId", "sessionId", "token", "user", RpcResponse("myMethod", null, Error("Could not find rpcMethodHandler myMethod", 1)), "")))
+  }
+
 }
