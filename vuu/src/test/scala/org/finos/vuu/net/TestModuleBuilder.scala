4e7c25e5da73e0ba7eb3cfe0d964830a67d9726d
Na Lee Ha <na-lee.ha@ubs.com>
2024-08-12 16:29:12 +0100

    #1434 adding test for type ahead request at a view port level. Data is loaded into table at initialisation

--- vuu/src/test/scala/org/finos/vuu/net/TestModuleBuilder.scala
+++ vuu/src/test/scala/org/finos/vuu/net/TestModuleBuilder.scala
@@ -3,8 +3,10 @@ package org.finos.vuu.net
 import org.finos.toolbox.lifecycle.LifecycleContainer
 import org.finos.toolbox.time.Clock
 import org.finos.vuu.api.{TableDef, ViewPortDef}
+import org.finos.vuu.core.IVuuServer
 import org.finos.vuu.core.module.{ModuleFactory, ModuleFactoryNode, TableDefContainer, ViewServerModule}
-import org.finos.vuu.provider.MockProvider
+import org.finos.vuu.core.table.{DataTable, TableContainer}
+import org.finos.vuu.provider.{MockProvider, Provider, ProviderContainer}
 
 
 object TestExtension {
@@ -17,11 +19,15 @@ object TestExtension {
       )
     }
 
-    def addTableForTest(tableDef: TableDef, viewPortDef: ViewPortDef)(implicit clock: Clock, lifecycle: LifecycleContainer): ModuleFactoryNode = {
+    def addTableForTest(
+                         tableDef: TableDef,
+                         ViewPortDefFactory: (DataTable, Provider, ProviderContainer, TableContainer) => ViewPortDef,
+                         providerFactory: (DataTable, IVuuServer) => Provider
+                       )(implicit clock: Clock, lifecycle: LifecycleContainer): ModuleFactoryNode = {
       moduleFactoryNode.addTable(
         tableDef,
-        (table, _) => new MockProvider(table),
-        (_, _, _, _) => viewPortDef
+        providerFactory,
+        ViewPortDefFactory
       )
     }
 
@@ -29,8 +35,8 @@ object TestExtension {
 }
 class TestModuleBuilder(moduleName:String)(implicit clock: Clock, lifecycle: LifecycleContainer, tableDefContainer: TableDefContainer){
 
-
   private var moduleFactory = ModuleFactory.withNamespace(moduleName)
+
   def withTable(tableDef: TableDef, viewPortDef: ViewPortDef): TestModuleBuilder = {
     moduleFactory = moduleFactory.addTable(
       tableDef,
@@ -39,7 +45,6 @@ class TestModuleBuilder(moduleName:String)(implicit clock: Clock, lifecycle: Lif
     )
     this
   }
-
 
   def build(moduleName: String, tableDef: TableDef, viewPortDef: ViewPortDef)(implicit clock: Clock, lifecycle: LifecycleContainer, tableDefContainer: TableDefContainer): ViewServerModule =
   ModuleFactory.withNamespace(moduleName)
