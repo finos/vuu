9efda62ba98fa1181c07beb65446ed020f915d51
heswell <steve@heswell.com>
2024-09-11 22:52:35 +0100

    missing dependency in data-react (#1487)

---
+++ vuu-ui/packages/vuu-data-react/src/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from "./useLookupValues";
+export * from "./useVuuMenuActions";
+export * from "./useVuuTables";
+export * from "./useVisualLinks";
+export * from "./useServerConnectionStatus";
+export * from "./useServerConnectionQuality";
+export * from "./useTypeaheadSuggestions";
