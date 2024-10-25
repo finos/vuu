58647d8170a86155ab5825c164df161a2b5867e2
Zhen <chuqi.zhen@ubs.com>
2024-10-24 20:47:08 -0400

    bulk edit row validation

--- vuu-ui/packages/vuu-data-react/src/data-editing/index.ts
+++ vuu-ui/packages/vuu-data-react/src/data-editing/index.ts
@@ -2,5 +2,6 @@ export * from "./get-data-item-edit-control";
 export * from "./EditForm";
 export * from "./edit-rule-validation-checker";
 export * from "./UnsavedChangesReport";
+export * from "./useEditBulk";
 export * from "./useEditForm";
 export * from "./form-edit-state";
