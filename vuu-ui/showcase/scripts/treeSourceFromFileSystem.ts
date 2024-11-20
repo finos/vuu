4433e6ebdfcdaae5cd94fa7fa2bbd617ecbbd084
heswell <steve@heswell.com>
2024-11-20 09:56:01 +0000

    import fails in showcase script, inline function

--- vuu-ui/showcase/scripts/treeSourceFromFileSystem.ts
+++ vuu-ui/showcase/scripts/treeSourceFromFileSystem.ts
@@ -1,10 +1,14 @@
 import fs from "fs";
 import path from "path";
-import { dropLastPathSegment, type TreeSourceNode } from "@finos/vuu-utils";
+import { type TreeSourceNode } from "@finos/vuu-utils";
 
 const lastPathSegment = (path: string, separator = "/") => {
   const root = path.endsWith(separator) ? path.slice(0, -1) : path;
   return root.slice(root.lastIndexOf(separator) + 1);
+};
+
+export const dropLastPathSegment = (path: string, separator = "/") => {
+  return path.slice(0, path.lastIndexOf(separator));
 };
 
 const exportPattern = /export const ([A-Z][a-zA-Z]+) = /g;
