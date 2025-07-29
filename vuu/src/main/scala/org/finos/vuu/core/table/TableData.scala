de63b4b2b3059d898380bbca305cf3dd1799981f
tanjjj <34288072+tanjjj@users.noreply.github.com>
2025-07-29 17:07:29 +0100

    add default columns to table definition

--- vuu/src/main/scala/org/finos/vuu/core/table/TableData.scala
+++ vuu/src/main/scala/org/finos/vuu/core/table/TableData.scala
@@ -1,7 +1,5 @@
 package org.finos.vuu.core.table
 
-import org.finos.toolbox.collection.array.ImmutableArray
-
 trait TableData {
   def dataByKey(key: String): RowData
   def update(key: String, update: RowData): TableData
