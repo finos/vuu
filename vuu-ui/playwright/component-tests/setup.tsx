c1b004377d35fd575dbf4c1a20dbf9b85c9d5a6f
Laurence Hartgill <laurence@hartgill.co.uk>
2025-09-08 11:56:40 +0100

    Fix linting issues in Playwright setup file

--- vuu-ui/playwright/component-tests/setup.tsx
+++ vuu-ui/playwright/component-tests/setup.tsx
@@ -1,12 +1,11 @@
 import { beforeMount, afterMount } from '@playwright/experimental-ct-react/hooks';
-import React from 'react';
 
 // Global setup for all component tests
-beforeMount(async ({ hooksConfig }) => {
+beforeMount(async ({ hooksConfig: _hooksConfig }) => {
   // Add any global setup here if needed
   // For example, setting up providers, themes, etc.
 });
 
-afterMount(async ({ hooksConfig }) => {
+afterMount(async ({ hooksConfig: _hooksConfig }) => {
   // Add any cleanup here if needed
 });
