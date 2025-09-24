319a9086aa008705562418af6dacaf3bbaaead01
tanjjj <34288072+tanjjj@users.noreply.github.com>
2025-09-24 15:32:16 +0100

    #1721 comment

--- vuu/src/main/scala/org/finos/vuu/feature/ViewPortKeys.scala
+++ vuu/src/main/scala/org/finos/vuu/feature/ViewPortKeys.scala
@@ -5,9 +5,9 @@ import org.finos.vuu.core.table.TablePrimaryKeys
 trait ViewPortKeys extends Iterable[String] {
     def create(tableKeys: TablePrimaryKeys): ViewPortKeys
     def get(index: Int): String
-    // From a key until another key
+    // From a key (inclusive) until another key (exclusive)
     def sliceToArray(from: Int, to: Int): Array[String]
-    // From a key until another key
+    // From a key (inclusive) until another key (exclusive)
     def sliceToKeys(from: Int, to: Int): ViewPortKeys
     def length: Int
     def toArray(): Array[String]
