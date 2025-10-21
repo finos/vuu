3112a8dd805bcaaaa3202d8e97cff954e1444b4d
heswell <steve@heswell.com>
2025-10-21 22:46:38 +0100

    simplify freeze control

--- vuu-ui/packages/vuu-table-extras/src/freeze-control/FreezeControl.tsx
+++ vuu-ui/packages/vuu-table-extras/src/freeze-control/FreezeControl.tsx
@@ -1,6 +1,6 @@
 import { useComponentCssInjection } from "@salt-ds/styles";
 import { useWindow } from "@salt-ds/window";
-import { Badge, Switch } from "@salt-ds/core";
+import { Switch } from "@salt-ds/core";
 import freezeControlCss from "./FreezeControl.css";
 import { useFreezeControl, type FreezeProps } from "./useFreezeControl";
 import { HTMLAttributes } from "react";
@@ -10,11 +10,16 @@ const classBase = "FreezeControl";
 
 export interface FreezeControlProps
   extends HTMLAttributes<HTMLDivElement>,
-    FreezeProps {}
+    FreezeProps {
+  activeLabel?: string;
+  lockedLabel?: string;
+}
 
 export const FreezeControl = ({
+  activeLabel = "Active",
   dataSource,
   className,
+  lockedLabel = "Locked",
   ...htmlAttributes
 }: FreezeControlProps) => {
   const targetWindow = useWindow();
@@ -24,25 +29,15 @@ export const FreezeControl = ({
     window: targetWindow,
   });
 
-  const { label, isFrozen, lastUpdateMessage, newRecordCount, onSwitchChange } =
-    useFreezeControl({ dataSource });
+  const { frozen, onSwitchChange } = useFreezeControl({
+    dataSource,
+  });
 
   return (
-    <Badge value={newRecordCount} max={99}>
-      <div
-        {...htmlAttributes}
-        className={cx(classBase, className, {
-          [`${classBase}-showBadge`]: newRecordCount > 0,
-        })}
-      >
-        <span className={`${classBase}-label`}>{label}</span>
-        <Switch
-          checked={isFrozen}
-          onChange={onSwitchChange}
-          className="vuuLarge"
-        />
-        <span className={`${classBase}-lastUpdated`}>{lastUpdateMessage}</span>
-      </div>
-    </Badge>
+    <div {...htmlAttributes} className={cx(classBase, className)}>
+      <span className={`${classBase}-label-active`}>{activeLabel}</span>
+      <Switch checked={frozen} onChange={onSwitchChange} className="vuuLarge" />
+      <span className={`${classBase}-label-frozen`}>{lockedLabel}</span>
+    </div>
   );
 };
--- vuu-ui/packages/vuu-table-extras/src/freeze-control/useFreezeControl.ts
+++ vuu-ui/packages/vuu-table-extras/src/freeze-control/useFreezeControl.ts
@@ -1,99 +1,28 @@
-import {
-  DataSource,
-  DataSourceSubscribeCallback,
-} from "@vuu-ui/vuu-data-types";
-import { VuuTable } from "@vuu-ui/vuu-protocol-types";
-import { formatDate, messageHasSize, useData } from "@vuu-ui/vuu-utils";
-import { ChangeEventHandler, useCallback, useMemo, useState } from "react";
+import { DataSource } from "@vuu-ui/vuu-data-types";
+import { ChangeEventHandler, useCallback, useState } from "react";
 
 export interface FreezeProps {
   dataSource: DataSource;
 }
 
-type FreezeState = {
-  isFrozen: boolean;
-  label: string;
-  lastUpdateMessage: string;
-  newRecordCount: number;
-};
-
-const timeFormatter = formatDate({ time: "hh:mm:ss" });
-
-const formatFreezeTime = (ts?: number) => {
-  if (ts === undefined) {
-    throw Error("[useFreezeControl] formatFreezeTime, freezeTime undefined");
-  }
-  return timeFormatter(new Date(ts));
-};
-
-const FreezeState = (
-  dataSource: DataSource,
-  newRecordCount: number,
-): FreezeState => ({
-  isFrozen: dataSource.isFrozen ?? false,
-  label: dataSource.isFrozen ? "View Frozen" : "Freeze View",
-  lastUpdateMessage: dataSource.isFrozen
-    ? `at ${formatFreezeTime(dataSource.freezeTimestamp)}`
-    : "",
-  newRecordCount,
-});
-
 export const useFreezeControl = ({ dataSource }: FreezeProps) => {
-  const { VuuDataSource } = useData();
-  const table = useMemo<VuuTable>(() => {
-    if (dataSource.table === undefined) {
-      throw Error(`[useFreezeControls] dataSource must have VuuTable`);
-    }
-    return dataSource.table;
-  }, [dataSource]);
-  const [freezeState, setFreezeState] = useState<FreezeState>(
-    FreezeState(dataSource, 0),
-  );
-
-  const [startTrackingNewRows, stopTrackingNewRows] = useMemo(() => {
-    let ds: DataSource | undefined = undefined;
-
-    const dataCallback: DataSourceSubscribeCallback = (message) => {
-      if (messageHasSize(message)) {
-        setFreezeState(FreezeState(dataSource, message.size));
-      }
-    };
-
-    const start = (ts: number) => {
-      ds = new VuuDataSource({ table });
-      ds.subscribe(
-        {
-          filterSpec: {
-            filter: `created > ${ts}`,
-          },
-        },
-        dataCallback,
-      );
-    };
-
-    const stop = () => {
-      ds?.unsubscribe();
-    };
-
-    return [start, stop];
-  }, [VuuDataSource, dataSource, table]);
+  const [frozen, setFrozen] = useState(dataSource.isFrozen);
 
   const handleSwitchChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
     (evt) => {
-      if (evt.target.checked) {
+      const isFrozen = evt.target.checked;
+      if (isFrozen) {
         dataSource.freeze?.();
-        startTrackingNewRows(dataSource.freezeTimestamp as number);
       } else {
         dataSource.unfreeze?.();
-        stopTrackingNewRows();
       }
-      setFreezeState(FreezeState(dataSource, 0));
+      setFrozen(isFrozen);
     },
-    [dataSource, startTrackingNewRows, stopTrackingNewRows],
+    [dataSource],
   );
 
   return {
-    ...freezeState,
+    frozen,
     onSwitchChange: handleSwitchChange,
   };
 };
