296a1661d82dc53bf7d8c94811b9004824673d35
tanjjj <34288072+tanjjj@users.noreply.github.com>
2025-11-19 11:08:07 +0000

    #1790 add more rpc

--- vuu/src/main/scala/org/finos/vuu/net/rpc/EditTableRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/EditTableRpcHandler.scala
@@ -4,10 +4,22 @@ import org.finos.vuu.core.table.TableContainer
 
 abstract class EditTableRpcHandler(implicit val tableContainer: TableContainer) extends DefaultRpcHandler {
   registerRpc(RpcNames.DeleteRowRpc, this.deleteRow)
+  registerRpc(RpcNames.DeleteCellRpc, this.deleteCell)
+  registerRpc(RpcNames.AddRowRpc, this.addRow)
+  registerRpc(RpcNames.EditCellRpc, this.editCell)
+  registerRpc(RpcNames.EditRowRpc, this.editRow)
   registerRpc(RpcNames.OnFormSubmitRpc, this.submitForm)
   registerRpc(RpcNames.OnFormCloseRpc, this.closeForm)
 
   def deleteRow(params: RpcParams): RpcFunctionResult
+
+  def deleteCell(params: RpcParams): RpcFunctionResult
+
+  def addRow(params: RpcParams): RpcFunctionResult
+
+  def editCell(params: RpcParams): RpcFunctionResult
+
+  def editRow(params: RpcParams): RpcFunctionResult
 
   def submitForm(params: RpcParams): RpcFunctionResult
 
