0d347eaab34f353b8c4c732c583b3e7e5da1d46d
tanjjj <34288072+tanjjj@users.noreply.github.com>
2025-11-19 11:49:36 +0000

    #1790 rename

--- example/editable/src/test/scala/org/finos/vuu/core/module/editable/EditableTestModule.scala
+++ example/editable/src/test/scala/org/finos/vuu/core/module/editable/EditableTestModule.scala
@@ -68,7 +68,7 @@ class TestEditableService(val table: DataTable, val tableContainer: TableContain
   override def onFormClose(): ViewPortFormCloseAction = ViewPortFormCloseAction("", formClose)
 }
 
-class EditableTestService(val table: DataTable)(using tableContainer: TableContainer) extends EditTableRpcHandler with StrictLogging {
+class EditTableTestService(val table: DataTable)(using tableContainer: TableContainer) extends EditTableRpcHandler with StrictLogging {
 
   override def deleteRow(params: RpcParams): RpcFunctionResult = {
     val key: String = params.namedParams("key").asInstanceOf[String]
@@ -180,7 +180,7 @@ object EditTableTestModule {
         (table, _) => new NullProvider(),
         (table, _, _, tableContainer) => ViewPortDef(
           columns = table.getTableDef.columns,
-          service = new EditableTestService(table)(using tableContainer)
+          service = new EditTableTestService(table)(using tableContainer)
         )
       ).asModule()
 
