f0039807238cab9f300089cc9daf498e79c5c797
stonesmi <8273759+stonesmi@users.noreply.github.com>
2025-11-25 22:23:58 +0000

    Add test for epoch filter clauses

--- vuu/src/test/scala/org/finos/vuu/core/filter/FilterClauseTest.scala
+++ vuu/src/test/scala/org/finos/vuu/core/filter/FilterClauseTest.scala
@@ -1,10 +1,12 @@
 package org.finos.vuu.core.filter
 
+import org.finos.vuu.core.table.datatype.EpochTimestamp
 import org.finos.vuu.core.table.{RowWithData, SimpleColumn}
 import org.finos.vuu.viewport.ViewPortColumns
 import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.matchers.should.Matchers
-import org.scalatest.prop.TableDrivenPropertyChecks._
+import org.scalatest.prop.TableDrivenPropertyChecks.forAll
+import org.scalatest.prop.Tables.Table
 
 class FilterClauseTest extends AnyFeatureSpec with Matchers {
 
@@ -20,60 +22,60 @@ class FilterClauseTest extends AnyFeatureSpec with Matchers {
       "true-col" -> true,
       "false-col" -> false,
       "empty-string-col" -> "",
+      "epoch-column" -> EpochTimestamp(1L),
     )
     val testRow = RowWithData("Key", rowData)
 
-    forAll(Table(
-      ("columnType", "columnName", "value", "expected"),
-      ("String", "ric", "TEST.L", true),
-      ("String", "ric", "TEST.NOT-EQ", false),
-      ("Int", "size", "4", true),
-      ("Int", "size", "7", false),
-      ("Long", "timestamp", "5", true),
-      ("Long", "timestamp", "0", false),
-      ("Float", "priceInFloat", "100.0", true),
-      ("Float", "priceInFloat", "100.1", false),
-      ("Double", "priceInDouble", "100.0", true),
-      ("Double", "priceInDouble", "100.1", false),
-      ("Boolean", "true-col", "true", true),
-      ("Boolean", "true-col", "false", false)
-    ))((columnType, columnName, value, expected) => {
-        Scenario(
-          s"($columnType) should return $expected when value is " +
-            s"$value and row data is ${testRow.get(columnName)}"
-        ) {
-          EqualsClause(columnName, value).filter(testRow) should equal(expected)
-        }
-    })
+    Scenario("Basic type checks") {
 
-    forAll(Table(
-      ("value", "expected"),
-      ("", true),
-      ("non-empty", false),
-    ))((value, expected) => {
-      Scenario(
-        s"should return $expected when value is \"$value\" and row data is empty string"
-      ) {
+      forAll(Table(
+        ("columnType", "columnName", "value", "expected"),
+        ("String", "ric", "TEST.L", true),
+        ("String", "ric", "TEST.NOT-EQ", false),
+        ("Int", "size", "4", true),
+        ("Int", "size", "7", false),
+        ("Long", "timestamp", "5", true),
+        ("Long", "timestamp", "0", false),
+        ("Float", "priceInFloat", "100.0", true),
+        ("Float", "priceInFloat", "100.1", false),
+        ("Double", "priceInDouble", "100.0", true),
+        ("Double", "priceInDouble", "100.1", false),
+        ("Boolean", "true-col", "true", true),
+        ("Boolean", "true-col", "false", false),
+        ("EpochTimestamp", "epoch-column", "1", true),
+        ("EpochTimestamp", "epoch-column", "2", false)
+      ))((columnType, columnName, value, expected) => {
+        EqualsClause(columnName, value).filter(testRow) should equal(expected)
+      })
+
+    }
+
+    Scenario("Strings") {
+      forAll(Table(
+        ("value", "expected"),
+        ("", true),
+        ("non-empty", false),
+      ))((value, expected) => {
         EqualsClause("empty-string-col", value).filter(testRow) should equal(expected)
-      }
-    })
+      })
+    }
 
-    Scenario("should return false if row data is null at the given field") {
+    Scenario("Nulls") {
       EqualsClause("null-col", "some-value").filter(testRow) should be (false)
     }
 
-    forAll(Table(
-      ("columnName", "value"),
-      ("true-col", "TRUE"),
-      ("true-col", "TrUe"),
-      ("false-col", "FALSE"),
-      ("false-col", "FaLse"),
-    ))((columnName, value)=> {
-      Scenario(s"should be case insensitive for boolean values - " +
-        s"$value should be considered equal to ${testRow.get(columnName)}") {
-        EqualsClause(columnName, value).filter(testRow) should equal(true)
-      }
-    })
+    Scenario("Booleans") {
+      forAll(Table(
+        ("columnName", "value"),
+        ("true-col", "TRUE"),
+        ("true-col", "TrUe"),
+        ("false-col", "FALSE"),
+        ("false-col", "FaLse"),
+      ))((columnName, value) => {
+          EqualsClause(columnName, value).filter(testRow) should equal(true)
+      })
+    }
+
   }
 
   Feature("ContainsClause.filter") {
@@ -103,6 +105,7 @@ class FilterClauseTest extends AnyFeatureSpec with Matchers {
   }
 
   Feature("InClause.filter") {
+
     Scenario("should return true when row at a given column matches the only value in the list") {
       val row = givenARow(assetClass = "Fixed-income")
 
