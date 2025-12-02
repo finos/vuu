1d12cd4d66c7c3c55d3a3fa0a90cb38828c0cc26
Michael <8273759+stonesmi@users.noreply.github.com>
2025-12-02 16:57:14 +0000

    #1756 Work In Progress

---
+++ vuu/src/test/scala/org/finos/vuu/core/filter/type/BaseFilterTest.scala
@@ -0,0 +1,60 @@
+package org.finos.vuu.core.filter.`type`
+
+import org.finos.vuu.core.sort.FilterAndSortFixture.{setupTable, setupTableWithCreationTime}
+import org.finos.vuu.viewport.ViewPortColumns
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
+
+class BaseFilterTest extends AnyFeatureSpec with Matchers {
+
+  Feature("Applying base filters") {
+
+    Scenario("No freeze filter") {
+
+      val table = setupTable()
+      val permissionFilter = PermissionFilter("ric", Set("VOD.L","AAPL.L"))
+      val baseFilter = BaseFilter.apply(permissionFilter, Option.empty)
+
+      val results = baseFilter.doFilter(table, table.primaryKeys, ViewPortColumns(table.columns().toList), true)
+
+      results.length shouldEqual 5
+      results.toList shouldEqual List("NYC-0004", "LDN-0001", "LDN-0003", "NYC-0002", "NYC-0010")
+    }
+
+    Scenario("Freeze filter with created time column missing") {
+
+      val table = setupTable()
+      val permissionFilter = PermissionFilter("ric", Set("VOD.L","AAPL.L"))
+      val baseFilter = BaseFilter(permissionFilter, Option(10001L))
+
+      val results = baseFilter.doFilter(table, table.primaryKeys, ViewPortColumns(table.columns().toList), true)
+
+      results.length shouldEqual 0
+    }
+
+    Scenario("Freeze filter with created time column present") {
+
+      val table = setupTableWithCreationTime()
+      val permissionFilter = PermissionFilter("ric", Set("VOD.L","AAPL.L"))
+      val baseFilter = BaseFilter(permissionFilter, Option(10001L))
+
+      val results = baseFilter.doFilter(table, table.primaryKeys, ViewPortColumns(table.columns().toList), true)
+
+      results.length shouldEqual 2
+      results.toList shouldEqual List("NYC-0004", "LDN-0003")
+    }
+
+    Scenario("Freeze filter with no permission filter") {
+
+      val table = setupTableWithCreationTime()
+      val baseFilter = BaseFilter(AllowAllPermissionFilter, Option(10002L))
+
+      val results = baseFilter.doFilter(table, table.primaryKeys, ViewPortColumns(table.columns().toList), true)
+
+      results.length shouldEqual 4
+      results.toList shouldEqual List("NYC-0004", "LDN-0001", "LDN-0003", "LDN-0008")
+    }
+
+  }
+
+}
