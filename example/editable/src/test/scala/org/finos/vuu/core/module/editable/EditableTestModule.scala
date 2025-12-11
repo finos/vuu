ffe07c6152f6bea976506ac70193bbe73b9589bf
tanjjj <34288072+tanjjj@users.noreply.github.com>
2025-12-11 10:53:00 +0000

    #1790 replace rpc hanlder in test

--- example/editable/src/test/scala/org/finos/vuu/core/module/editable/EditableTestModule.scala
+++ example/editable/src/test/scala/org/finos/vuu/core/module/editable/EditableTestModule.scala
@@ -11,63 +11,6 @@ import org.finos.vuu.net.ClientSessionId
 import org.finos.vuu.net.rpc.{EditRpcHandler, EditTableRpcHandler, RpcFunctionResult, RpcFunctionSuccess, RpcHandler, RpcParams}
 import org.finos.vuu.viewport.{ViewPort, ViewPortAddRowAction, ViewPortDeleteCellAction, ViewPortDeleteRowAction, ViewPortEditAction, ViewPortEditCellAction, ViewPortEditRowAction, ViewPortEditSuccess, ViewPortFormCloseAction, ViewPortFormSubmitAction}
 
-trait TestEditableServiceIF extends EditRpcHandler {
-
-}
-
-@deprecated("#1790 to be replaced by EditableTestService")
-class TestEditableService(val table: DataTable, val tableContainer: TableContainer)(implicit clock: Clock) extends RpcHandler with TestEditableServiceIF with StrictLogging {
-
-  def addRow(key: String, data: Map[String, Any], vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    table.processUpdate(key, RowWithData(key, data))
-    ViewPortEditSuccess()
-  }
-
-  def editRow(key: String, data: Map[String, Any], vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    table.processUpdate(key, RowWithData(key, data))
-    ViewPortEditSuccess()
-  }
-
-  def editCell(key: String, column: String, data: Any, vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    table.processUpdate(key, RowWithData(key, Map("rowId" -> key, column -> data)))
-    ViewPortEditSuccess()
-  }
-
-  def deleteRow(key: String, vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    table.processDelete(key)
-    ViewPortEditSuccess()
-  }
-
-  def deleteCell(key: String, column: String, vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    table.processUpdate(key, RowWithData(key, Map("rowId" -> key, column -> null)))
-    ViewPortEditSuccess()
-  }
-
-  def formSubmit(vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    //table.processUpdate(key, RowWithData(key, Map("rowId" -> key, column -> null)), clock.now())
-    ViewPortEditSuccess()
-  }
-
-  def formClose(vp: ViewPort, session: ClientSessionId): ViewPortEditAction = {
-    //table.processUpdate(key, RowWithData(key, Map("rowId" -> key, column -> null)), clock.now())
-    ViewPortEditSuccess()
-  }
-
-  override def deleteRowAction(): ViewPortDeleteRowAction = ViewPortDeleteRowAction("", deleteRow)
-
-  override def deleteCellAction(): ViewPortDeleteCellAction = ViewPortDeleteCellAction("", deleteCell)
-
-  override def addRowAction(): ViewPortAddRowAction = ViewPortAddRowAction("", addRow)
-
-  override def editCellAction(): ViewPortEditCellAction = ViewPortEditCellAction("", editCell)
-
-  override def editRowAction(): ViewPortEditRowAction = ViewPortEditRowAction("", editRow)
-
-  override def onFormSubmit(): ViewPortFormSubmitAction = ViewPortFormSubmitAction("", formSubmit)
-
-  override def onFormClose(): ViewPortFormCloseAction = ViewPortFormCloseAction("", formClose)
-}
-
 class EditTableTestService()(using tableContainer: TableContainer) extends EditTableRpcHandler with StrictLogging {
 
   override def deleteRow(params: RpcParams): RpcFunctionResult = {
@@ -134,33 +77,6 @@ class EditTableTestService()(using tableContainer: TableContainer) extends EditT
   }
 }
 
-@deprecated("#1790 to be replaced by EditTableTestModule")
-object EditableTestModule {
-
-  final val NAME = "EDITTEST"
-
-  def apply()(implicit clock: Clock, lifecycle: LifecycleContainer, tableDefContainer: TableDefContainer): ViewServerModule = {
-
-    ModuleFactory.withNamespace(NAME)
-      .addTable(
-        TableDef(
-          name = "editTestTable",
-          keyField = "rowId",
-          columns = Columns.fromNames("rowId".string(), "A".string(), "B".double(), "C".int(), "D".boolean()),
-          VisualLinks(),
-          joinFields = "rowId"
-        ),
-        (table, _) => new NullProvider(),
-        (table, _, _, tableContainer) => ViewPortDef(
-          columns = table.getTableDef.getColumns,
-          service = new TestEditableService(table, tableContainer)
-        )
-      ).asModule()
-
-  }
-
-}
-
 object EditTableTestModule {
 
   final val NAME = "EDIT_TABLE_TEST"
@@ -169,7 +85,6 @@ object EditTableTestModule {
 
     ModuleFactory.withNamespace(NAME)
       .addTable(
-        //this table should contain one row for each of .FTSE, .DJI, .HSI, .etc...
         TableDef(
           name = "editTestTable",
           keyField = "rowId",
