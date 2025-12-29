ecd970417827a93a6ba528674b93fb7bc6a370f3
Michael <8273759+stonesmi@users.noreply.github.com>
2025-12-29 23:01:17 +0000

    Added native (epoll and kqueue) transport support for linux and OSX

--- vuu/src/main/scala/org/finos/vuu/net/ws/WebSocketClient.scala
+++ vuu/src/main/scala/org/finos/vuu/net/ws/WebSocketClient.scala
@@ -16,6 +16,7 @@ class WebSocketClient(url: String, port: Int, nativeTransport: Boolean = true)(u
 
   private val transport: Transport = Transport(nativeTransport)
   private val eventLoopGroup: EventLoopGroup = transport.eventLoopGroup(1)
+  private val bootstrap = new Bootstrap()
   private val uri: URI = new URI(url)
   private val handler: WebSocketClientHandler = new WebSocketClientHandler(
     WebSocketClientHandshakerFactory.newHandshaker(uri, WebSocketVersion.V13, null, true, new DefaultHttpHeaders,
@@ -36,33 +37,7 @@ class WebSocketClient(url: String, port: Int, nativeTransport: Boolean = true)(u
 
   def awaitMessage(): String = handler.awaitMessage()
 
-  @Override
   override def doStart(): Unit = {
-    val bootstrap: Bootstrap = new Bootstrap
-    
-    bootstrap.group(eventLoopGroup)
-      .channel(transport.channelClass)
-      .handler(new ChannelInitializer[SocketChannel] {
-
-      @Override
-      protected def initChannel(ch: SocketChannel): Unit = {
-        val pipeline: ChannelPipeline = ch.pipeline
-        applySSL(ch, pipeline)
-        pipeline.addLast("http-codec", new HttpClientCodec())
-        pipeline.addLast("aggregator", new HttpObjectAggregator(WebSocketConstants.MAX_CONTENT_LENGTH))
-        pipeline.addLast("compression", new WebSocketClientCompressionHandler(WebSocketConstants.MAX_CONTENT_LENGTH))
-        pipeline.addLast("handler", handler)
-      }
-
-      private def applySSL(socketChannel: SocketChannel, channelPipeline: ChannelPipeline): Unit = {
-        if (uri.getScheme == "wss") {
-          val sslContext = SslContextBuilder.forClient.trustManager(InsecureTrustManagerFactory.INSTANCE).build()
-          channelPipeline.addLast("ssl", sslContext.newHandler(socketChannel.alloc(), uri.getHost, port))
-        }
-      }
-
-    })
-
     channel = bootstrap.connect(uri.getHost, port).sync.channel
   }
 
@@ -70,12 +45,36 @@ class WebSocketClient(url: String, port: Int, nativeTransport: Boolean = true)(u
     if (channel != null && channel.isOpen) {
       channel.close()
     }
-    eventLoopGroup.shutdownGracefully()
   }
 
-  override def doInitialize(): Unit = {}
+  override def doInitialize(): Unit = {
+    bootstrap.group(eventLoopGroup)
+      .channel(transport.channelClass)
+      .handler(new ChannelInitializer[SocketChannel] {
 
-  override def doDestroy(): Unit = {}
+        @Override
+        protected def initChannel(ch: SocketChannel): Unit = {
+          val pipeline: ChannelPipeline = ch.pipeline
+          applySSL(ch, pipeline)
+          pipeline.addLast("http-codec", new HttpClientCodec())
+          pipeline.addLast("aggregator", new HttpObjectAggregator(WebSocketConstants.MAX_CONTENT_LENGTH))
+          pipeline.addLast("compression", new WebSocketClientCompressionHandler(WebSocketConstants.MAX_CONTENT_LENGTH))
+          pipeline.addLast("handler", handler)
+        }
+
+        private def applySSL(socketChannel: SocketChannel, channelPipeline: ChannelPipeline): Unit = {
+          if (uri.getScheme == "wss") {
+            val sslContext = SslContextBuilder.forClient.trustManager(InsecureTrustManagerFactory.INSTANCE).build()
+            channelPipeline.addLast("ssl", sslContext.newHandler(socketChannel.alloc(), uri.getHost, port))
+          }
+        }
+
+      })
+  }
+
+  override def doDestroy(): Unit = {
+    eventLoopGroup.shutdownGracefully()
+  }
 
   override val lifecycleId: String = "webSocketClient"
 
