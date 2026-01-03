6f9c4765aff615281199c6e396a6aae8605e3b15
Michael <8273759+stonesmi@users.noreply.github.com>
2026-01-03 22:00:50 +0000

    WiP

--- vuu/src/main/scala/org/finos/vuu/core/table/JoinTableIndices.scala
+++ vuu/src/main/scala/org/finos/vuu/core/table/JoinTableIndices.scala
@@ -14,31 +14,28 @@ object JoinTableIndices extends StrictLogging {
 
   def apply(joinTableDef: JoinTableDef, sourceTables: Map[String, DataTable]): JoinTableIndices = {
 
-    val baseTableJoinColumns = joinTableDef.joinColumns
-      .map(c => c.asInstanceOf[JoinColumn])
-      .filter(jc => jc != null && jc.sourceTable == joinTableDef.baseTable)
-      .toList
+    val baseTableIndices = collectBaseTableIndices(joinTableDef, sourceTables)
+    //TODO There should be a way to also use use indices from the non-base tables,
+    // but we'd need to map the other table primary keys to a set of valid baseTable primary keys (using an index maybe?)
 
-    val baseSourceTable = sourceTables(joinTableDef.baseTable.name)
+    logger.debug(s"Join table ${joinTableDef.name} has indices on columns: ${baseTableIndices.keys.map(_.name).mkString(", ")}")
+    JoinTableIndicesImpl(baseTableIndices)
+  }
 
-    val indices: Map[JoinColumn, IndexedField[_]] = baseTableJoinColumns
-      .filter(jc => baseSourceTable.indexForColumn(jc.sourceColumn).isDefined)
-      .map(jc => jc -> baseSourceTable.indexForColumn(jc.sourceColumn).get)
-      .toMap()
-
-    logger.debug(s"Adding indices for columns ${indices.keys.map(f => f.name)} in join table ${joinTableDef.name}")
-    JoinTableIndicesImpl(indices)
+  private def collectBaseTableIndices(joinTableDef: JoinTableDef, sourceTables: Map[String, DataTable]): Map[Column, IndexedField[_]] = {
+    joinTableDef.joinColumns.collect {
+      case jc: JoinColumn if jc.sourceTable == joinTableDef.baseTable => jc
+    }.flatMap {
+      jc => sourceTables(joinTableDef.baseTable.name).indexForColumn(jc.sourceColumn).map(jc -> _)
+    }.toMap
   }
 
 }
 
-private case class JoinTableIndicesImpl(indices: Map[JoinColumn, IndexedField[_]]) extends JoinTableIndices {
+private case class JoinTableIndicesImpl(indices: Map[Column, IndexedField[_]]) extends JoinTableIndices {
 
   override def indexForColumn(column: Column): Option[IndexedField[_]] = {
-    column match {
-      case joinColumn: JoinColumn => indices.get(joinColumn)
-      case _ => None
-    }
+    indices.get(column)
   }
 
 }
