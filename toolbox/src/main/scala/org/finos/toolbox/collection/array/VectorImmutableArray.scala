5a4dffab6dae5aeca855ae58e9f17844cc1650b8
Michael <8273759+stonesmi@users.noreply.github.com>
2026-02-26 16:23:09 +0000

    WiP

--- toolbox/src/main/scala/org/finos/toolbox/collection/array/VectorImmutableArray.scala
+++ toolbox/src/main/scala/org/finos/toolbox/collection/array/VectorImmutableArray.scala
@@ -1,24 +1,52 @@
 package org.finos.toolbox.collection.array
 
+import com.typesafe.scalalogging.Logger
+
+import scala.collection.immutable.BitSet
 import scala.reflect.ClassTag
+import scala.util.hashing.MurmurHash3
+
+trait VectorImmutableArray[T] extends ImmutableArray[T] { }
 
 object VectorImmutableArray {
 
-  def from[T <: Object : ClassTag](arr: IterableOnce[T]): ImmutableArray[T] = {
-    new VectorImmutableArray[T](Vector.from(arr))
+  private val logger: Logger = Logger.apply(classOf[VectorImmutableArray[_]])
+
+  def from[T <: Object : ClassTag](iterable: IterableOnce[T]): ImmutableArray[T] = {
+    val dataBuilder = Vector.newBuilder[T]
+    val bitSetBuilder = BitSet.newBuilder
+    if (iterable.knownSize > 0) {
+      dataBuilder.sizeHint(iterable.knownSize)
+      bitSetBuilder.sizeHint(iterable.knownSize)
+    }
+
+    val it = iterable.iterator
+    var addedCount = 0
+    while (it.hasNext) {
+      val item = it.next()
+      require(item != null, "This collection does not support null elements.")
+      dataBuilder += item
+      bitSetBuilder += addedCount
+      addedCount += 1
+    }
+
+    VectorImmutableArrayImpl(dataBuilder.result(), bitSetBuilder.result(), addedCount, logger)
   }
 
   def empty[T <: Object : ClassTag](): ImmutableArray[T] = {
-    new VectorImmutableArray[T](Vector.empty)
+    new VectorImmutableArrayImpl[T](Vector.empty, BitSet.empty, 0, logger)
   }
 
   def of[T <: Object : ClassTag](element: T): ImmutableArray[T] = {
-    new VectorImmutableArray[T](Vector(element))
+    new VectorImmutableArrayImpl[T](Vector(element), BitSet(0), 1, logger)
   }
 
 }
 
-private class VectorImmutableArray[T <: Object :ClassTag](private val vector: Vector[T]) extends ImmutableArray[T] {
+private class VectorImmutableArrayImpl[T <: Object :ClassTag](private val data: Vector[T],
+                                                          private val activeIndices: BitSet,
+                                                          override val length: Int,
+                                                          logger: Logger) extends VectorImmutableArray[T] {
 
   override def +(element: T): ImmutableArray[T] = add(element)
 
@@ -26,73 +54,140 @@ private class VectorImmutableArray[T <: Object :ClassTag](private val vector: Ve
 
   override def ++(iterable: IterableOnce[T]): ImmutableArray[T] = addAll(iterable)
 
-  override def add(element: T): ImmutableArray[T] = VectorImmutableArray(vector.appended(element))
+  override def add(element: T): ImmutableArray[T] = {
+    require(element != null, "This collection does not support null elements.")
+    val newData = data.appended(element)
+    val newActive = activeIndices + (newData.length - 1)
+    VectorImmutableArrayImpl(newData, newActive, length + 1, logger)
+  }
 
   override def remove(element: T): ImmutableArray[T] = {
-    val index = vector.indexOf(element)
-    if (index >= 0) remove(index) else this
+    val physIdx = indexOf(element)
+    if (physIdx == -1) this
+    else doRemove(physIdx)
   }
 
   override def addAll(iterable: IterableOnce[T]): ImmutableArray[T] = {
-    VectorImmutableArray(vector.appendedAll(iterable))
+    val dataBuilder = Vector.newBuilder[T]
+    val bitSetBuilder = BitSet.newBuilder
+    if (iterable.knownSize > 0) {
+      dataBuilder.sizeHint(data.length + iterable.knownSize)
+      bitSetBuilder.sizeHint(data.length + iterable.knownSize)
+    }
+    dataBuilder ++= data
+    bitSetBuilder ++= activeIndices
+
+    val it = iterable.iterator
+    var currentPhysicalIndex = data.length
+    var addedCount = 0
+    while (it.hasNext) {
+      val item = it.next()
+      require(item != null, "This collection does not support null elements.")
+      dataBuilder += item
+      bitSetBuilder += currentPhysicalIndex
+      currentPhysicalIndex += 1
+      addedCount += 1
+    }
+
+    VectorImmutableArrayImpl(dataBuilder.result(), bitSetBuilder.result(), length + addedCount, logger)
   }
 
   override def getIndex(index: Int): T = {
-    vector(index)
+    apply(index)
   }
 
   override def indexOf(element: T): Int = {
-    vector.indexOf(element)
+    var index = 0
+    val it = activeIndices.iterator
+    while (it.hasNext) {
+      if (data(it.next()) == element) return index
+      index += 1
+    }
+    -1
   }
 
-  override def contains(element: T): Boolean = vector.contains(element)
+  override def contains(element: T): Boolean = indexOf(element) != -1
 
-  override def length: Int = vector.length
-
-  override def apply(i: Int): T = vector(i)
-
-  override def set(index: Int, element: T): ImmutableArray[T] = VectorImmutableArray(vector.updated(index, element))
-
-  override def remove(index: Int): ImmutableArray[T] = VectorImmutableArray(vector.patch(index, Nil, 1))
-
-  override def iterator: Iterator[T] = vector.iterator
-
-  override def foreach[U](f: T => U): Unit = vector.foreach(f)
-
-  override def size: Int = vector.size
-
-  override def knownSize: Int = vector.knownSize
-
-  override def isEmpty: Boolean = vector.isEmpty
-
-  override def head: T = vector.head
-
-  override def last: T = vector.last
-
-  override def headOption: Option[T] = vector.headOption
-
-  override def lastOption: Option[T] = vector.lastOption
-
-  override def slice(from: Int, until: Int): Iterable[T] = vector.slice(from, until)
-
-  override def take(n: Int): Iterable[T] = vector.take(n)
-
-  override def drop(n: Int): Iterable[T] = vector.drop(n)
-
-  override def copyToArray[B >: T](xs: Array[B], start: Int, len: Int): Int = {
-    vector.copyToArray(xs, start, len)
+  override def apply(index: Int): T = {
+    if (index < 0 || index >= length) throw new IndexOutOfBoundsException(s"Index $index")
+    data(findPhysicalIndex(index))
   }
 
-  private lazy val hash = vector.hashCode()
+  override def set(index: Int, element: T): ImmutableArray[T] = {
+    require(element != null, "This collection does not support null elements.")
+    val physIdx = findPhysicalIndex(index)
+    VectorImmutableArrayImpl(data.updated(physIdx, element), activeIndices, length, logger)
+  }
+
+  override def remove(index: Int): ImmutableArray[T] = {
+    val physIdx = findPhysicalIndex(index)
+    doRemove(physIdx)
+  }
+
+  override def iterator: Iterator[T] = activeIndices.iterator.map(data(_))
+
+  override def foreach[U](f: T => U): Unit = {
+    val it = activeIndices.iterator
+    while (it.hasNext) {
+      val physicalIndex = it.next()
+      f(data(physicalIndex))
+    }
+  }
+
+  private def findPhysicalIndex(userIndex: Int): Int = {
+    var count = 0
+    val it = activeIndices.iterator
+    while (it.hasNext) {
+      val physicalIdx = it.next()
+      if (count == userIndex) return physicalIdx
+      count += 1
+    }
+    throw new IndexOutOfBoundsException(s"Index $userIndex")
+  }
+
+  private def doRemove(physIdx: Int): VectorImmutableArray[T] = {
+    val updated = VectorImmutableArrayImpl(data, activeIndices - physIdx, length - 1, logger)
+    if (shouldCompact) {
+      logger.debug(s"Compacting ${data.length - length} records")
+      updated.compact()
+    } else {
+      updated
+    }
+  }
+
+  private def shouldCompact: Boolean = {
+    if (data.length == length) false
+    else (data.length - length) > Math.max(100, data.length * 0.10)
+  }
+
+  private def compact(): VectorImmutableArray[T] = {
+    val dataBuilder = Vector.newBuilder[T]
+    dataBuilder.sizeHint(this.length)
+    this.iterator.foreach(dataBuilder += _)
+    val newData = dataBuilder.result()
+    val newActive = BitSet.fromSpecific(newData.indices)
+    VectorImmutableArrayImpl(newData, newActive, newData.length, logger)
+  }
+
+  private lazy val hash = MurmurHash3.orderedHash(this.iterator)
 
   override def hashCode(): Int = hash
 
-  override def equals(obj: Any): Boolean = {
-    obj match {
-      case that: VectorImmutableArray[_] =>
-        (this eq that) || (this.vector == that.vector)
-      case _ => false
-    }
+  override def equals(other: Any): Boolean = other match {
+    case that: VectorImmutableArray[_] =>
+      if (this eq that) true
+      else if (this.length != that.length) false
+      else if (this.hashCode() != that.hashCode()) false
+      else {
+        val it1 = this.iterator
+        val it2 = that.iterator
+        var same = true
+        while (it1.hasNext && it2.hasNext && same) {
+          if (it1.next() != it2.next()) same = false
+        }
+        same
+      }
+    case _ => false
   }
 
 }
