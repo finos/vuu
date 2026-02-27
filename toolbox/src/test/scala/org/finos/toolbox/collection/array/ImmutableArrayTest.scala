f15eb38378e1bcfd066064921900e8717ef74230
stonesmi <8273759+stonesmi@users.noreply.github.com>
2026-02-27 13:39:40 +0000

    WiP

--- toolbox/src/test/scala/org/finos/toolbox/collection/array/ImmutableArrayTest.scala
+++ toolbox/src/test/scala/org/finos/toolbox/collection/array/ImmutableArrayTest.scala
@@ -58,14 +58,6 @@ class ImmutableArrayTest extends AnyFunSuite with Matchers {
     a.addAll(b) shouldBe (a ++ b)
   }
 
-  test("getIndex retrieves the correct element") {
-    val a = arr("alpha", "beta", "gamma")
-    a.getIndex(1) shouldBe "beta"
-
-    val b = a.remove("beta")
-    b.getIndex(1) shouldBe "gamma"
-  }
-
   test("apply retrieves the correct element") {
     val a = arr("cat", "dog", "mouse")
     a(1) shouldBe "dog"
@@ -124,29 +116,44 @@ class ImmutableArrayTest extends AnyFunSuite with Matchers {
     b.map(_.toUpperCase).toList shouldBe List("X", "Z")
   }
 
-  test("check removal of all rows and that compaction doesn't break things") {
-    val count = 1000
+  test("check removal of all rows by index and that compaction doesn't break things") {
+    val count = 20_000
     val numbers = (0 until count).map(_.toString)
     var array = arr(numbers: _*)
 
     var index = 0
     while (index < count) {
       array.length shouldEqual (count - index)
-      array.iterator.count(f => true) shouldEqual array.length
-      array.contains(index.toString) shouldBe true
       array(0) shouldEqual index.toString
-      array.head shouldEqual index.toString
-      array.last shouldEqual (count - 1).toString
+      array(array.length - 1) shouldEqual (count -1).toString
 
       array = array.remove(0)
 
       array.length shouldEqual (count - index - 1)
-      array.iterator.count(f => true) shouldEqual array.length
-      array.contains(index.toString) shouldBe false
       if (array.nonEmpty) {
         array(0) shouldEqual (index + 1).toString
-        array.head shouldEqual (index + 1).toString
-        array.last shouldEqual (count - 1).toString
+        array(array.length - 1) shouldEqual (count -1).toString
+      }
+
+      index += 1
+    }
+  }
+
+  test("check removal of all rows by last element and that compaction doesn't break things") {
+    val count = 20_000
+    val numbers = (0 until count).map(_.toString)
+    var array = arr(numbers: _*)
+
+    var index = 0
+    while (index < count) {
+      array.length shouldEqual (count - index)
+      array(array.length - 1) shouldEqual (array.length - 1).toString
+
+      array = array.remove(array(array.length - 1))
+
+      array.length shouldEqual (count - index - 1)
+      if (array.nonEmpty) {
+        array(array.length - 1) shouldEqual (array.length - 1).toString
       }
 
       index += 1
