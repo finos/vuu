87dded3e3a144c46aa8c7bee65b5d6a5cb645ace
stonesmi <8273759+stonesmi@users.noreply.github.com>
2026-03-30 13:42:14 +0100

    Removed all ignite code and examples, as no one is willing to maintain it.

--- plugin/ignite-plugin/src/main/scala/org/finos/vuu/feature/ignite/api/IgniteSessionTableDef.scala
+++
@@ -1,9 +0,0 @@
-package org.finos.vuu.feature.ignite.api
-
-import org.finos.vuu.api.{Indices, SessionTableDef, VisualLinks}
-import org.finos.vuu.core.table.Column
-
-case class IgniteSessionTableDef(override val name: String, override val keyField: String, override val customColumns: Array[Column])
-  extends SessionTableDef(name, keyField, customColumns, Seq(), false, VisualLinks(), Indices())
-  with VuuIgnitePluginLocator {
-}
