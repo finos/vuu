17bfba8837a692655e79e0a92b3c88a29d1c88c7
heswell <steve@heswell.com>
2026-05-15 16:58:51 +0100

    fix standalone table app, make remote rather than local (#2148)

--- vuu-ui/sample-apps/standalone-table/index.tsx
+++ vuu-ui/sample-apps/standalone-table/index.tsx
@@ -1,8 +1,17 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./src/App";
+import { VuuDataSourceProvider } from "@vuu-ui/vuu-data-react";
 
 const container = document.getElementById("root");
 if (container) {
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+    <VuuDataSourceProvider
+      autoConnect
+      authenticate={false}
+      websocketUrl="ws://localhost:8090/websocket"
+    >
+      <App />
+    </VuuDataSourceProvider>,
+  );
 }
