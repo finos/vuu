3a7b9422ee15f94360d0f7c4422424e9b19a908c
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-06-04 11:15:54 +0100

    #2130 add javadoc

--- vuu-java/src/main/java/org/finos/vuu/util/SortSpecBuilder.java
+++ vuu-java/src/main/java/org/finos/vuu/util/SortSpecBuilder.java
@@ -7,14 +7,28 @@ import java.util.List;
 
 import static org.finos.vuu.util.ScalaCollectionConverter.toScalaSeq;
 
+/**
+ * Builder for {@link SortSpec}.
+ */
 public class SortSpecBuilder {
     private List<SortDef> sortDefs = List.of();
 
+    /**
+     * Sets SortDefs.
+     *
+     * @param sortDefs sortDefs
+     * @return this builder
+     */
     public SortSpecBuilder sortDefs(List<SortDef> sortDefs) {
         this.sortDefs = sortDefs;
         return this;
     }
 
+    /**
+     * Creates a {@link SortSpec}.
+     *
+     * @return SortSpec
+     */
     public SortSpec build() {
         return new SortSpec(toScalaSeq(sortDefs).toList());
     }
