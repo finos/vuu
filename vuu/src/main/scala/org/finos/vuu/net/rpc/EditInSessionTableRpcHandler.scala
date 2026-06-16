c3c38029b6a68470584c1f3c5fd908deb831ddb4
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-06-16 12:05:05 +0100

    #2169 add test

--- vuu/src/main/scala/org/finos/vuu/net/rpc/EditInSessionTableRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/EditInSessionTableRpcHandler.scala
@@ -2,11 +2,19 @@ package org.finos.vuu.net.rpc
 
 import org.finos.vuu.core.table.TableContainer
 
-abstract class EditInSessionTableRpcHandler(using val tableContainer: TableContainer) extends DefaultRpcHandler {
+class EditInSessionTableRpcHandler(using val tableContainer: TableContainer) extends DefaultRpcHandler {
   registerRpc(RpcNames.BeginEditSessionRpc, this.beginEditSession)
   registerRpc(RpcNames.EndEditSessionRpc, this.endEditSession)
 
-  def beginEditSession(params: RpcParams): RpcFunctionResult
+  def beginEditSession(params: RpcParams): RpcFunctionResult = {
+    // check if tabledef editable, if so create session table based on enum, if no reject
+    null
+  }
 
-  def endEditSession(params: RpcParams): RpcFunctionResult
+
+  def endEditSession(params: RpcParams): RpcFunctionResult = {
+    // close session table
+    null
+  }
+
 }
