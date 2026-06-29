f5a68d7b6484f1498487a4c1c56003cb13977163
stonesmi <8273759+stonesmi@users.noreply.github.com>
2026-06-29 22:02:19 +0100

    WIP

--- vuu-java/src/main/java/org/finos/vuu/util/SortSpecBuilder.java
+++ vuu-java/src/main/java/org/finos/vuu/util/SortSpecBuilder.java
@@ -1,8 +1,10 @@
 package org.finos.vuu.util;
 
+import org.finos.vuu.core.sort.SortDirection;
 import org.finos.vuu.net.SortDef;
 import org.finos.vuu.net.SortSpec;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.finos.vuu.util.ScalaCollectionConverter.toScalaSeq;
@@ -11,16 +13,50 @@ import static org.finos.vuu.util.ScalaCollectionConverter.toScalaSeq;
  * Builder for {@link SortSpec}.
  */
 public class SortSpecBuilder {
-    private List<SortDef> sortDefs = List.of();
+
+    private final List<SortDef> sortDefs = new ArrayList<>();
 
     /**
-     * Sets SortDefs.
+     * Adds one SortDef.
      *
-     * @param sortDefs sortDefs
+     * @param sortDef to be added
      * @return this builder
      */
-    public SortSpecBuilder sortDefs(List<SortDef> sortDefs) {
-        this.sortDefs = sortDefs;
+    public SortSpecBuilder addSortDef(SortDef sortDef) {
+        this.sortDefs.add(sortDef);
+        return this;
+    }
+    
+    /**
+     * Adds SortDefs.
+     *
+     * @param sortDefs to be added
+     * @return this builder
+     */
+    public SortSpecBuilder addSortDefs(List<SortDef> sortDefs) {
+        this.sortDefs.addAll(sortDefs);
+        return this;
+    }
+
+    /**
+     * Adds one column to be sorted Ascendingly
+     *
+     * @param columnName to be sorted by
+     * @return this builder
+     */
+    public SortSpecBuilder addAscending(String columnName) {
+        sortDefs.add(new SortDef(columnName, SortDirection.ASCENDING().external()));
+        return this;
+    }
+
+    /**
+     * Adds one column to be sorted Descendingly
+     *
+     * @param columnName to be sorted by
+     * @return this builder
+     */
+    public SortSpecBuilder addDescending(String columnName) {
+        sortDefs.add(new SortDef(columnName, SortDirection.DESCENDING().external()));
         return this;
     }
 
--- vuu-java/src/test/java/org/finos/vuu/util/SortSpecBuilderTest.java
+++ vuu-java/src/test/java/org/finos/vuu/util/SortSpecBuilderTest.java
@@ -11,9 +11,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class SortSpecBuilderTest {
 
     @Test
-    void buildWithAllParameters() {
+    void buildWithSortDef() {
         SortSpec sortSpec = new SortSpecBuilder()
-                .sortDefs(List.of(new SortDef("myColumn", 'D')))
+                .addSortDef(new SortDef("myColumn", 'D'))
+                .build();
+
+        assertEquals(1, sortSpec.sortDefs().length());
+        assertEquals("myColumn", sortSpec.sortDefs().apply(0).column());
+        assertEquals('D', sortSpec.sortDefs().apply(0).sortType());
+    }
+
+    @Test
+    void buildWithSortDefs() {
+        SortSpec sortSpec = new SortSpecBuilder()
+                .addSortDefs(
+                        List.of(
+                                new SortDef("myColumn", 'D'),
+                                new SortDef("myColumn2", 'A')
+                        ))
+                .build();
+
+        assertEquals(2, sortSpec.sortDefs().length());
+        assertEquals("myColumn", sortSpec.sortDefs().apply(0).column());
+        assertEquals('D', sortSpec.sortDefs().apply(0).sortType());
+        assertEquals("myColumn2", sortSpec.sortDefs().apply(1).column());
+        assertEquals('A', sortSpec.sortDefs().apply(1).sortType());
+    }
+
+    @Test
+    void buildWithAscending() {
+        SortSpec sortSpec = new SortSpecBuilder()
+                .addAscending("myColumn")
+                .build();
+
+        assertEquals(1, sortSpec.sortDefs().length());
+        assertEquals("myColumn", sortSpec.sortDefs().apply(0).column());
+        assertEquals('A', sortSpec.sortDefs().apply(0).sortType());
+    }
+
+    @Test
+    void buildWithDescending() {
+        SortSpec sortSpec = new SortSpecBuilder()
+                .addDescending("myColumn")
                 .build();
 
         assertEquals(1, sortSpec.sortDefs().length());
