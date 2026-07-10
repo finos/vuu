2789b5562784ad29a61247ed5afc2f7437984e07
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-07-10 10:51:49 +0100

    #2169 refactor

--- vuu/src/main/scala/org/finos/vuu/api/ColumnBuilder.scala
+++ vuu/src/main/scala/org/finos/vuu/api/ColumnBuilder.scala
@@ -8,57 +8,101 @@ class ColumnBuilder {
 
   val columns = new mutable.ArrayBuilder.ofRef[String]()
 
-  def addString(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addString(columnName: String): ColumnBuilder = {
+    addString(columnName, false)
+  }
+
+  def addString(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":String:" + isEditable)
     this
   }
 
-  def addDouble(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addDouble(columnName: String): ColumnBuilder = {
+    addDouble(columnName, false)
+  }
+
+  def addDouble(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":Double:" + isEditable)
     this
   }
 
-  def addInt(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addInt(columnName: String): ColumnBuilder = {
+    addInt(columnName, false)
+  }
+
+  def addInt(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":Int:" + isEditable)
     this
   }
 
-  def addLong(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addLong(columnName: String): ColumnBuilder = {
+    addLong(columnName, false)
+  }
+
+  def addLong(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":Long:" + isEditable)
     this
   }
 
-  def addBoolean(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addBoolean(columnName: String): ColumnBuilder = {
+    addBoolean(columnName, false)
+  }
+
+  def addBoolean(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":Boolean:" + isEditable)
     this
   }
 
-  def addChar(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addChar(columnName: String): ColumnBuilder = {
+    addChar(columnName, false)
+  }
+
+  def addChar(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":Char:" + isEditable)
     this
   }
 
-  def addEpochTimestamp(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addEpochTimestamp(columnName: String): ColumnBuilder = {
+    addEpochTimestamp(columnName, false)
+  }
+
+  def addEpochTimestamp(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":EpochTimestamp:" + isEditable)
     this
   }
 
-  def addScaledDecimal2(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addScaledDecimal2(columnName: String): ColumnBuilder = {
+    addScaledDecimal2(columnName, false)
+  }
+
+  def addScaledDecimal2(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":ScaledDecimal2:" + isEditable)
     this
   }
 
-  def addScaledDecimal4(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addScaledDecimal4(columnName: String): ColumnBuilder = {
+    addScaledDecimal4(columnName, false)
+  }
+
+  def addScaledDecimal4(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":ScaledDecimal4:" + isEditable)
     this
   }
 
-  def addScaledDecimal6(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addScaledDecimal6(columnName: String): ColumnBuilder = {
+    addScaledDecimal6(columnName, false)
+  }
+
+  def addScaledDecimal6(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":ScaledDecimal6:" + isEditable)
     this
   }
 
-  def addScaledDecimal8(columnName: String, isEditable: Boolean = false): ColumnBuilder = {
+  def addScaledDecimal8(columnName: String): ColumnBuilder = {
+    addScaledDecimal8(columnName, false)
+  }
+
+  def addScaledDecimal8(columnName: String, isEditable: Boolean): ColumnBuilder = {
     columns += (columnName + ":ScaledDecimal8:" + isEditable)
     this
   }
