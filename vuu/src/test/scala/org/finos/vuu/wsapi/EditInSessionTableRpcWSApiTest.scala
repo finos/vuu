bc37f69b29230e2fe0da265f6958970a3d9d6521
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-07-16 11:31:50 +0100

    #2169 add tests for CreateSessionTableRpc (#2211)
    * #2169 add tests

--- vuu/src/test/scala/org/finos/vuu/wsapi/EditInSessionTableRpcWSApiTest.scala
+++ vuu/src/test/scala/org/finos/vuu/wsapi/EditInSessionTableRpcWSApiTest.scala
@@ -5,7 +5,7 @@ import org.finos.vuu.api.{ColumnBuilder, SessionTableDef, TableDef, ViewPortDef}
 import org.finos.vuu.core.AbstractVuuServer
 import org.finos.vuu.core.module.{ModuleFactory, ViewServerModule}
 import org.finos.vuu.core.table.{DataTable, TableContainer}
-import org.finos.vuu.net.{CreateViewPortRequest, CreateViewPortSuccess, RpcRequest, RpcResponseNew}
+import org.finos.vuu.net.{CreateViewPortRequest, CreateViewPortSuccess, GetTableMetaRequest, GetTableMetaResponse, RpcRequest, RpcResponseNew, SelectRowRangeRequest, SelectRowRangeSuccess, SelectRowRequest, SelectRowSuccess}
 import org.finos.vuu.net.rpc.{CreateSessionTableRpcHandler, EndEditSessionRpcHandler, RpcErrorResult, RpcNames, RpcSuccessResult, ViewPortContext}
 import org.finos.vuu.provider.{Provider, ProviderContainer}
 import org.finos.vuu.viewport.{ViewPortRange, ViewPortTable}
@@ -28,11 +28,9 @@ class EditInSessionTableRpcWSApiTest extends WebSocketApiTestBase {
   private val testProviderFactory = new TestProviderFactory
   private val maxCopySize = 10 // configured in CoreServerApiTest
 
-  // TODO 2069 add test scenarios for empty but all columns, selection with all columns, selection with some columns
-  // test with large data, have 10 in base table, 5 in vp, add filter and sort
-  // test vp is sorted, the data copied to session table is also sorted
-  // TODO 2169 do we care about sorting of viewport selection???
-  // add a test for selected rows more than max size
+  // TODO add more tests:
+  // Test when vp is filtered and sorted, the data copied to session table is also filtered and sorted
+  // Test when copying from a given list of columns, only data from those columns are copied
   Feature("[Web Socket API] create a session table and copy data from source table") {
     Scenario("create an empty session table from source table") {
       Given("a view port exist")
@@ -103,7 +101,7 @@ class EditInSessionTableRpcWSApiTest extends WebSocketApiTestBase {
       responseBody.rpcName shouldEqual RpcNames.CreateSessionTableRpc
       val rpcResult = assertAndCastAsInstanceOf[RpcSuccessResult](responseBody.result)
       val sessionTableName = rpcResult.data.asInstanceOf[Map[String, String]]("sessionTable")
-      val sessionTableViewPortId = createViewPortAndVerifyDataSize(sessionTableName, 0)
+      val sessionTableViewPortId = createViewPortAndVerifyDataSize(sessionTableName, 3)
     }
 
     Scenario("create a session table and copy all rows up to max threshold from source table") {
@@ -117,6 +115,65 @@ class EditInSessionTableRpcWSApiTest extends WebSocketApiTestBase {
         params = Map(
           "sessionTableName" -> sessionTableDefName,
           "copyOption" -> "All",
+          "columnsToCopy" -> "*"
+        ))
+      val requestId = vuuClient.send(sessionId, createSessionTableRequest)
+
+      Then("session table is created with max number of rows")
+      val response = vuuClient.awaitForResponse(requestId)
+      val responseBody = assertBodyIsInstanceOf[RpcResponseNew](response)
+      responseBody.rpcName shouldEqual RpcNames.CreateSessionTableRpc
+      val rpcResult = assertAndCastAsInstanceOf[RpcSuccessResult](responseBody.result)
+      val sessionTableName = rpcResult.data.asInstanceOf[Map[String, String]]("sessionTable")
+      val sessionTableViewPortId = createViewPortAndVerifyDataSize(sessionTableName, maxCopySize)
+    }
+
+    Scenario("create a session table from selected rows of source table") {
+      Given("a view port exist and some rows are selected")
+      val viewPortId = createViewPort(tableName1)
+
+      val selectRowRequest1 = SelectRowRequest(viewPortId, "row1", false)
+      val selectRowRequest2 = SelectRowRequest(viewPortId, "row3", true)
+      vuuClient.send(sessionId, selectRowRequest1)
+      vuuClient.awaitForMsgWithBody[SelectRowSuccess]
+      vuuClient.send(sessionId, selectRowRequest2)
+      vuuClient.awaitForMsgWithBody[SelectRowSuccess]
+
+      When("request creating a session table from selected rows")
+      val createSessionTableRequest = RpcRequest(
+        ViewPortContext(viewPortId),
+        RpcNames.CreateSessionTableRpc,
+        params = Map(
+          "sessionTableName" -> sessionTableDefName,
+          "copyOption" -> "Selected",
+          "columnsToCopy" -> "*"
+        ))
+      val requestId = vuuClient.send(sessionId, createSessionTableRequest)
+
+      Then("session table is created")
+      val response = vuuClient.awaitForResponse(requestId)
+      val responseBody = assertBodyIsInstanceOf[RpcResponseNew](response)
+      responseBody.rpcName shouldEqual RpcNames.CreateSessionTableRpc
+      val rpcResult = assertAndCastAsInstanceOf[RpcSuccessResult](responseBody.result)
+      val sessionTableName = rpcResult.data.asInstanceOf[Map[String, String]]("sessionTable")
+      val sessionTableViewPortId = createViewPortAndVerifyDataSize(sessionTableName, 2)
+    }
+
+    Scenario("create a session table and copy selected rows up to max threshold from source table") {
+      Given("a view port exist and some rows are selected")
+      val viewPortId = createViewPort(largeTableName)
+
+      val selectRowRequest = SelectRowRangeRequest(viewPortId, "row1", "row13", false)
+      vuuClient.send(sessionId, selectRowRequest)
+      vuuClient.awaitForMsgWithBody[SelectRowRangeSuccess]
+
+      When("request creating a session table and copy selected rows")
+      val createSessionTableRequest = RpcRequest(
+        ViewPortContext(viewPortId),
+        RpcNames.CreateSessionTableRpc,
+        params = Map(
+          "sessionTableName" -> sessionTableDefName,
+          "copyOption" -> "Selected",
           "columnsToCopy" -> "*"
         ))
       val requestId = vuuClient.send(sessionId, createSessionTableRequest)
@@ -193,16 +250,20 @@ class EditInSessionTableRpcWSApiTest extends WebSocketApiTestBase {
     val providerFactory = (table: DataTable, _: AbstractVuuServer) => testProviderFactory.create(table, dataSource)
     val largeDataSource = new FakeDataSource(ListMap(
       "row1" -> Map("Id" -> "row1", "Name" -> "user1", "Account" -> 1),
-      "row2" -> Map("Id" -> "row2", "Name" -> "user1", "Account" -> 2),
-      "row3" -> Map("Id" -> "row3", "Name" -> "user1", "Account" -> 3),
-      "row4" -> Map("Id" -> "row4", "Name" -> "user1", "Account" -> 4),
-      "row5" -> Map("Id" -> "row5", "Name" -> "user1", "Account" -> 5),
-      "row6" -> Map("Id" -> "row6", "Name" -> "user1", "Account" -> 6),
-      "row7" -> Map("Id" -> "row7", "Name" -> "user1", "Account" -> 7),
-      "row8" -> Map("Id" -> "row8", "Name" -> "user1", "Account" -> 8),
-      "row9" -> Map("Id" -> "row9", "Name" -> "user1", "Account" -> 9),
-      "row10" -> Map("Id" -> "row10", "Name" -> "user1", "Account" -> 10),
-      "row11" -> Map("Id" -> "row11", "Name" -> "user1", "Account" -> 11),
+      "row2" -> Map("Id" -> "row2", "Name" -> "user2", "Account" -> 2),
+      "row3" -> Map("Id" -> "row3", "Name" -> "user3", "Account" -> 3),
+      "row4" -> Map("Id" -> "row4", "Name" -> "user4", "Account" -> 4),
+      "row5" -> Map("Id" -> "row5", "Name" -> "user5", "Account" -> 5),
+      "row6" -> Map("Id" -> "row6", "Name" -> "user6", "Account" -> 6),
+      "row7" -> Map("Id" -> "row7", "Name" -> "user7", "Account" -> 7),
+      "row8" -> Map("Id" -> "row8", "Name" -> "user8", "Account" -> 8),
+      "row9" -> Map("Id" -> "row9", "Name" -> "user9", "Account" -> 9),
+      "row10" -> Map("Id" -> "row10", "Name" -> "user10", "Account" -> 10),
+      "row11" -> Map("Id" -> "row11", "Name" -> "user11", "Account" -> 11),
+      "row12" -> Map("Id" -> "row12", "Name" -> "user12", "Account" -> 12),
+      "row13" -> Map("Id" -> "row13", "Name" -> "user13", "Account" -> 13),
+      "row14" -> Map("Id" -> "row14", "Name" -> "user14", "Account" -> 14),
+      "row15" -> Map("Id" -> "row15", "Name" -> "user15", "Account" -> 15),
     ))
     val largeProviderFactory = (table: DataTable, _: AbstractVuuServer) => testProviderFactory.create(table, largeDataSource)
 
