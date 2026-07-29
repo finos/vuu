b7ae0cf8c9c3269db76c1b300fe74bc90c079d39
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-07-29 09:44:13 +0100

    #2169 update log msg

--- vuu/src/main/scala/org/finos/vuu/net/rpc/EndEditSessionRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/EndEditSessionRpcHandler.scala
@@ -7,19 +7,19 @@ trait EndEditSessionRpcHandler(using val tableContainer: TableContainer) extends
 
   def endEditSession(params: RpcParams): RpcFunctionResult = {
     if (!verifyPermission(params)) {
-      logger.warn(s"Failed to end edit session for table ${params.viewPort.table.name}. No permission.")
+      logger.warn(s"Failed to end edit session in viewport ${params.viewPort.id} in session ${params.ctx.session.sessionId}. No permission.")
       return new RpcFunctionFailure(s"Unable to end edit session. No permission.")
     }
 
     if (!validateDataInTable(params)) {
-      logger.warn(s"Failed to end edit session for table ${params.viewPort.table.name}. Invalid data found.")
+      logger.warn(s"Failed to end edit session in viewport ${params.viewPort.id} in session ${params.ctx.session.sessionId}. Invalid data found.")
       return new RpcFunctionFailure(s"Unable to end edit session. Invalid data found.")
     }
 
     if (submit(params)) {
       RpcFunctionSuccess(None)
     } else {
-      logger.warn(s"Failed to end edit session for table ${params.viewPort.table.name}. Failed to submit.")
+      logger.warn(s"Failed to end edit session in viewport ${params.viewPort.id} in session ${params.ctx.session.sessionId}. Failed to submit.")
       new RpcFunctionFailure(s"Failed to end edit session.")
     }
   }
