bc286f46b2f83003fe07b29660acdb2508ff4add
AB <anand4uall@yahoo.com>
2026-08-03 12:03:38 +0100

    Fixed type error issue

--- vuu-ui/packages/vuu-table/src/useTable.ts
+++ vuu-ui/packages/vuu-table/src/useTable.ts
@@ -112,6 +112,7 @@ export interface TableHookProps
     | "disableFocus"
     | "highlightedIndex"
     | "id"
+    | "isRowSelectable"
     | "navigationStyle"
     | "onConfigChange"
     | "onDragStart"
