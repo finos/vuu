369efe6135c51a54c2d3227e1aed2bdae7fef6b5
stonesmi <8273759+stonesmi@users.noreply.github.com>
2026-08-18 16:46:53 +0100

    params Wip

--- plugin/clickhouse-plugin/src/main/scala/org/finos/vuu/plugin/clickhouse/provider/filter/ClickHouseFilterVisitor.scala
+++ plugin/clickhouse-plugin/src/main/scala/org/finos/vuu/plugin/clickhouse/provider/filter/ClickHouseFilterVisitor.scala
@@ -1,9 +1,14 @@
 package org.finos.vuu.plugin.clickhouse.provider.filter
 
+import org.finos.toolbox.time.TimeUtils.ofEpochNanosecond
+import org.finos.vuu.core.table.DataType
+import org.finos.vuu.core.table.datatype.Scale.{Eight, Four, Six, Two}
+import org.finos.vuu.core.table.datatype.ScaledDecimal
 import org.finos.vuu.grammar.FilterBaseVisitor
 import org.finos.vuu.grammar.FilterParser.*
 import org.finos.vuu.plugin.virtualized.api.VirtualizedSessionTableColumn
 
+import java.time.Instant.ofEpochMilli
 import scala.collection.mutable
 
 case class ClickHouseFilterVisitor(remoteNameMapping: Map[String, VirtualizedSessionTableColumn],
@@ -25,80 +30,63 @@ case class ClickHouseFilterVisitor(remoteNameMapping: Map[String, VirtualizedSes
     visit(ctx.orExpression())
 
   override def visitOperationEq(ctx: OperationEqContext): Unit = {
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" = ")
-    appendScalar(ctx.scalar())
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" = ")
+    appendScalarParam(remoteColumn, ctx.scalar())
   }
 
   override def visitOperationNeq(ctx: OperationNeqContext): Unit = {
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" != ")
-    appendScalar(ctx.scalar())
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" != ")
+    appendScalarParam(remoteColumn, ctx.scalar())
   }
 
-  override def visitOperationGt(ctx: OperationGtContext): Unit =
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" > ").append(ctx.NUMBER().getText)
+  override def visitOperationGt(ctx: OperationGtContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" > ").append(ctx.NUMBER().getText)
+  }
 
-  override def visitOperationGte(ctx: OperationGteContext): Unit =
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" >= ").append(ctx.NUMBER().getText)
+  override def visitOperationGte(ctx: OperationGteContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" >= ").append(ctx.NUMBER().getText)
+  }
 
-  override def visitOperationLt(ctx: OperationLtContext): Unit =
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" < ").append(ctx.NUMBER().getText)
+  override def visitOperationLt(ctx: OperationLtContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" < ").append(ctx.NUMBER().getText)
+  }
 
-  override def visitOperationLte(ctx: OperationLteContext): Unit =
-    stringBuilder.append(getRemoteId(ctx.ID().getText)).append(" <= ").append(ctx.NUMBER().getText)
+  override def visitOperationLte(ctx: OperationLteContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    stringBuilder.append(remoteColumn.remoteName).append(" <= ").append(ctx.NUMBER().getText)
+  }
 
-  override def visitOperationStarts(ctx: OperationStartsContext): Unit =
-    like(getRemoteId(ctx.ID().getText), ctx.STRING().getText, prefix = false, suffix = true)
+  override def visitOperationStarts(ctx: OperationStartsContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    like(remoteColumn.remoteName, ctx.STRING().getText, prefix = false, suffix = true)
+  }
 
-  override def visitOperationEnds(ctx: OperationEndsContext): Unit =
-    like(getRemoteId(ctx.ID().getText), ctx.STRING().getText, prefix = true, suffix = false)
+  override def visitOperationEnds(ctx: OperationEndsContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    like(remoteColumn.remoteName, ctx.STRING().getText, prefix = true, suffix = false)
+  }
 
-  override def visitOperationContains(ctx: OperationContainsContext): Unit =
-    like(getRemoteId(ctx.ID().getText), ctx.STRING().getText, prefix = true, suffix = true)
+  override def visitOperationContains(ctx: OperationContainsContext): Unit = {
+    val remoteColumn = getRemoteColumn(ctx.ID().getText)
+    like(remoteColumn.remoteName, ctx.STRING().getText, prefix = true, suffix = true)
+  }
 
   override def visitOperationIn(ctx: OperationInContext): Unit = {
-    val id = getRemoteId(ctx.ID().getText)
-    val setCtx = ctx.set()
-
-    val nums = setCtx.NUMBER()
-    if (nums != null && !nums.isEmpty) {
-      stringBuilder.append(id).append(" IN (")
-      val it = nums.iterator()
-      if (it.hasNext) stringBuilder.append(it.next().getText)
-      while (it.hasNext) {
-        stringBuilder.append(", ").append(it.next().getText)
-      }
-      stringBuilder.append(")")
-      return
-    }
-
-    val strings = setCtx.STRING()
-    if (strings != null && !strings.isEmpty) {
-      stringBuilder.append(id).append(" IN (")
-      val it = strings.iterator()
-      if (it.hasNext) {
-        stringBuilder.append('\'')
-        appendEscaped(it.next().getText)
-        stringBuilder.append('\'')
-      }
-      while (it.hasNext) {
-        stringBuilder.append(", '")
-        appendEscaped(it.next().getText)
-        stringBuilder.append('\'')
-      }
-      stringBuilder.append(")")
-      return
-    }
-
-    stringBuilder.append("1 = 0")
+    //TODO
   }
 
   // ------------------------------------------------------------
   // Helpers
   // ------------------------------------------------------------
 
-  private def getRemoteId(id: String): String = {
+  private def getRemoteColumn(id: String): VirtualizedSessionTableColumn = {
     remoteNameMapping.getOrElse(id,
-      throw new IllegalArgumentException(s"Mapping missing for filter column: '$id'")).remoteName
+      throw new IllegalArgumentException(s"Mapping missing for filter column: '$id'"))
   }
 
   private def joinChildren(children: java.util.List[_ <: org.antlr.v4.runtime.tree.ParseTree], op: String): Unit = {
@@ -137,32 +125,62 @@ case class ClickHouseFilterVisitor(remoteNameMapping: Map[String, VirtualizedSes
   private def like(id: String, lit: String, prefix: Boolean, suffix: Boolean): Unit = {
     stringBuilder.append(id).append(" LIKE '")
     if (prefix) stringBuilder.append('%')
-    appendEscaped(lit)
+    //TODO
     if (suffix) stringBuilder.append('%')
     stringBuilder.append('\'')
   }
 
-  private def appendScalar(scalar: ScalarContext): Unit = {
-    val s = scalar.STRING()
-    if (s != null) {
-      stringBuilder.append('\'')
-      appendEscaped(s.getText)
-      stringBuilder.append('\'')
-    } else {
-      stringBuilder.append(scalar.getText)
+  private def appendScalarParam(column: VirtualizedSessionTableColumn,
+                                scalarContext: ScalarContext): Unit = {
+    val paramName = s"p_${params.size}"
+    val paramValue = scalarContext.getText
+
+    stringBuilder.append("{")
+    stringBuilder.append(paramName)
+    stringBuilder.append(":")
+
+    column.dataType match {
+      case DataType.StringDataType =>
+        params.put(paramName, paramValue)
+        stringBuilder.append("String")
+      case DataType.CharDataType =>
+        params.put(paramName, paramValue.charAt(0))
+        stringBuilder.append("String")
+      case DataType.IntegerDataType =>
+        params.put(paramName, paramValue.toInt)
+        stringBuilder.append("Int32")
+      case DataType.LongDataType =>
+        params.put(paramName, paramValue.toLong)
+        stringBuilder.append("Int64")
+      case DataType.DoubleDataType =>
+        params.put(paramName, paramValue.toDouble)
+        stringBuilder.append("Float64")
+      case DataType.BooleanDataType =>
+        params.put(paramName, paramValue.toBoolean)
+        stringBuilder.append("Bool")
+      case DataType.EpochTimestampType =>
+        params.put(paramName, ofEpochMilli(paramValue.toLong))
+        stringBuilder.append("DateTime64")
+      case DataType.EpochTimestampNanoType =>
+        params.put(paramName, ofEpochNanosecond(paramValue.toLong))
+        stringBuilder.append("DateTime64")
+      case DataType.ScaledDecimal2Type =>
+        params.put(paramName, ScaledDecimal(paramValue, Two).scaledValue)
+        stringBuilder.append("Int64")
+      case DataType.ScaledDecimal4Type =>
+        params.put(paramName, ScaledDecimal(paramValue, Four).scaledValue)
+        stringBuilder.append("Int64")
+      case DataType.ScaledDecimal6Type =>
+        params.put(paramName, ScaledDecimal(paramValue, Six).scaledValue)
+        stringBuilder.append("Int64")
+      case DataType.ScaledDecimal8Type =>
+        params.put(paramName, ScaledDecimal(paramValue, Eight).scaledValue)
+        stringBuilder.append("Int64")
+      case _ =>
+        throw new IllegalArgumentException(s"Unable to handle column dataType: '${column.dataType}'")
     }
+
+    stringBuilder.append("}")
   }
 
-  private def appendEscaped(s: String): Unit = {
-    if (s != null) {
-      var i = 0
-      val len = s.length
-      while (i < len) {
-        val c = s.charAt(i)
-        if (c == '\'') stringBuilder.append("''")
-        else stringBuilder.append(c)
-        i += 1
-      }
-    }
-  }
 }
