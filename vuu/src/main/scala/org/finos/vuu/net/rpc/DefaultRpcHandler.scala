0b748688b21f93f036e96d57bcf1e9d01d75ebd7
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-08-18 13:31:48 +0100

    #2231 move default rpc handling logic to RpcHandler

--- vuu/src/main/scala/org/finos/vuu/net/rpc/DefaultRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/DefaultRpcHandler.scala
@@ -5,42 +5,8 @@ import com.typesafe.scalalogging.StrictLogging
 import org.finos.vuu.core.module.typeahead.ViewportTypeAheadRpcHandler
 import org.finos.vuu.core.table.TableContainer
 
-import java.util.concurrent.ConcurrentHashMap
-
 class DefaultRpcHandler(implicit tableContainer: TableContainer) extends RpcHandler with StrictLogging {
-
-  private val rpcHandlerMap = new ConcurrentHashMap[Rpc.FunctionName, Rpc.Function]()
 
   private val viewportTypeAheadRpcHandler = new ViewportTypeAheadRpcHandler(tableContainer)
   viewportTypeAheadRpcHandler.register(this)
-
-  /**
-   * Register a handler for a given rpc function
-   *
-   * @param functionName name of the rpc function
-   * @param handlerFunc  handler function that takes RpcParams and return RpcMethodCallResult
-   */
-
-  def registerRpc(functionName: Rpc.FunctionName, handlerFunc: Rpc.Function): Unit = {
-
-    if (rpcHandlerMap.containsKey(functionName)) {
-      throw new IllegalArgumentException(s"Function $functionName already registered")
-    }
-    rpcHandlerMap.put(functionName, handlerFunc)
-  }
-
-  override def processRpcRequest(rpcName: String, params: RpcParams): RpcFunctionResult = {
-    if (rpcHandlerMap.containsKey(rpcName)) {
-      try {
-        val handler = rpcHandlerMap.get(rpcName)
-        handler(params)
-      } catch {
-        case e: Exception =>
-          logger.error(s"Error processing rpc method $rpcName", e)
-          RpcFunctionFailure(1, e.toString, e)
-      }
-    } else {
-      new RpcFunctionFailure(s"Could not find rpcMethodHandler $rpcName")
-    }
-  }
 }
