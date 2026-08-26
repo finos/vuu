b41316796694001efe855dd3dfb61d4ec3d8ade6
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-08-26 22:57:48 +0100

    #2231 make java example extend EditSessionRpcHandler

--- example/main-java/src/main/java/org/finos/vuu/person/rpc/UpdateRecordRpcHandler.java
+++ example/main-java/src/main/java/org/finos/vuu/person/rpc/UpdateRecordRpcHandler.java
@@ -2,14 +2,14 @@ package org.finos.vuu.person.rpc;
 
 import org.finos.vuu.core.table.TableContainer;
 import org.finos.vuu.net.rpc.DefaultRpcHandlerImpl;
-import org.finos.vuu.net.rpc.EditTableRpcHandler;
+import org.finos.vuu.net.rpc.EditSessionRpcHandler;
 import org.finos.vuu.net.rpc.EndEditSessionRpcHandler;
 import org.finos.vuu.net.rpc.RpcFunctionResult;
 import org.finos.vuu.net.rpc.RpcFunctionSuccess;
 import org.finos.vuu.net.rpc.RpcParams;
 
 // An example of Java implementation of RpcHandler
-public class UpdateRecordRpcHandler extends DefaultRpcHandlerImpl implements EditTableRpcHandler, EndEditSessionRpcHandler {
+public class UpdateRecordRpcHandler extends DefaultRpcHandlerImpl implements EditSessionRpcHandler, EndEditSessionRpcHandler {
     private final TableContainer tableContainer;
 
     public UpdateRecordRpcHandler(TableContainer tableContainer) {
