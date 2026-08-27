563b0842037165bda5ba8ec9b4f753909ea065ed
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-08-27 11:49:31 +0100

    #2231 add default implementation to ImportSesssionRpcHandler

--- vuu/src/main/scala/org/finos/vuu/net/rpc/ImportSessionRpcHandler.scala
+++ vuu/src/main/scala/org/finos/vuu/net/rpc/ImportSessionRpcHandler.scala
@@ -1,10 +1,34 @@
 package org.finos.vuu.net.rpc
 
+import org.finos.vuu.core.table.DefaultColumn.MSG
+import org.finos.vuu.core.table.column.ColumnNames.VuuRowNum
+
 // Default implementation of EditTableRpcHandler for import mode
 trait ImportSessionRpcHandler extends EditTableRpcHandler {
+
   def addRow(params: RpcParams): RpcFunctionResult = {
-    // TODO add implementation
-    new RpcFunctionSuccess()
+    params.namedParams.get("data") match {
+      case Some(data: Map[String, Any]) =>
+        data.get(VuuRowNum) match {
+          case Some(rowNum: String) =>
+            data.get(MSG.name) match {
+              case Some(vuuMsg: String) => addRowWithVuuMsg(params)
+              case _ => addRowWithoutVuuMsg(params)
+            }
+          case _ => new RpcFunctionFailure("Unable to add row. Row number missing.")
+        }
+      case _ => new RpcFunctionFailure("Unable to add row. Data missing.")
+    }
+  }
+
+  protected def addRowWithVuuMsg(params: RpcParams): RpcFunctionResult = {
+    // add implementation 
+    new RpcFunctionFailure("")
+  }
+
+  protected def addRowWithoutVuuMsg(params: RpcParams): RpcFunctionResult = {
+    // add implementation 
+    new RpcFunctionFailure("")
   }
 
   def deleteRow(params: RpcParams): RpcFunctionResult = {
