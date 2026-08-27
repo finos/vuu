e9d511fce7591168c6fd0b900094bcd118208afe
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-08-27 22:46:55 +0100

    #2231 fix java example

--- vuu/src/test/scala/org/finos/vuu/wsapi/EditSessionRpcHandlerWSApiTest.scala
+++ vuu/src/test/scala/org/finos/vuu/wsapi/EditSessionRpcHandlerWSApiTest.scala
@@ -4,6 +4,7 @@ import org.finos.vuu.core.module.ViewServerModule
 
 // Test default implementation of RPCs in EditSessionRpcHandler
 class EditSessionRpcHandlerWSApiTest extends WebSocketApiTestBase {
+  // TODO 2231 add tests
 
   override protected def defineModuleWithTestTables(): ViewServerModule = ???
 }
