9d361789a08865e4f4a721fb6a677b8f63c5adb8
tanjjj <34288072+tanjjj@users.noreply.github.com>
2026-08-27 16:04:48 +0100

    #2231 move ViewPortDefHelper to scala

--- vuu/src/main/scala/org/finos/vuu/api/ViewPortDef.scala
+++ vuu/src/main/scala/org/finos/vuu/api/ViewPortDef.scala
@@ -1,7 +1,8 @@
 package org.finos.vuu.api
 
-import org.finos.vuu.core.table.Column
+import org.finos.vuu.core.table.{Column, DataTable, TableContainer}
 import org.finos.vuu.net.rpc.{DefaultRpcHandler, RpcHandler}
+import org.finos.vuu.provider.{Provider, ProviderContainer}
 
 object ViewPortDef {
   def apply(columns: Array[Column], service: RpcHandler): ViewPortDef = {
@@ -12,3 +13,16 @@ object ViewPortDef {
 }
 
 class ViewPortDef(val columns: Array[Column], val service: RpcHandler) {}
+
+object ViewPortDefHelper {
+
+  def defaultDefaultViewPortDefFunc: (DataTable, Provider, ProviderContainer, TableContainer) => ViewPortDef = {
+    (t, _, _, _) =>
+      ViewPortDef.createDefault(t.getTableDef.getColumns)
+  }
+
+  def viewPortDefFunc(service: RpcHandler): (DataTable, Provider, ProviderContainer, TableContainer) => ViewPortDef = {
+    (t, _, _, _) =>
+      ViewPortDef.apply(t.getTableDef.getColumns, service)
+  }
+}
